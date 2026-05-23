0bdd396440a5873116fecd7fc18b0baf7c340e67
zyr001122 <vazseyo@gmail.com>
2026-05-23 17:53:49 +0900

    Update data.js

--- data.js
+++ data.js
@@ -663,5 +663,234 @@ const posts = [
         actorReply: null
       }
     ]
+  },
+  네! 여기 있어요 😊
+
+```js
+  ,{
+    id: 6,
+    date: "2026-01-23T00:08",
+    weiboUrl: "https://weibo.com/5994946077",
+    originalText: "다들 자?",
+    translatedText: "다들 자?",
+    images: [],
+    comments: [
+      {
+        id: 602,
+        username: "周翊然Tz",
+        originalText: "我也去蹭蹭烟花看看了🫡",
+        translatedText: "나 차에서 잠깐 눈 좀 붙이려고 다들 일찍 쉬고 밤새지 마 잘 자😴😴😴",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 601,
+        username: "周翊然Tz",
+        originalText: "🙄刚吃完饭 现在去机场的路上",
+        translatedText: "🙄이제 막 밥 다 먹어서 지금 공항 가는 중이야",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 603,
+        username: "烟竹阮桐粥",
+        originalText: "！！翊然！！！出大事了，我在外面闯祸了，对方说要你的自拍50张，我还价还到500张了！！！快拍！😋😋 @周翊然Tz",
+        translatedText: "！！익연아！！！큰일 났어, 내가 밖에서 사고 쳤는데, 상대방이 너 자카(셀카) 50장이 있어야 한대, 내가 흥정해서 500장으로 늘려놨어！！！！빨리 찍어줘! 😋😋 @周翊然Tz",
+        images: ["images/copa-so.jpg"],
+        actorReply: { originalText: "谢谢你啊", translatedText: "참 고맙다" }
+      },
+      {
+        id: 604,
+        username: "椰椰糕冷",
+        originalText: "每次法拉利和茅妹打架最后谁赢啊哈哈",
+        translatedText: "매번 페라리(라루이)랑 마오타이(마오메이)랑 싸우면 결국 누가 이기는지 궁금해 하하",
+        images: ["images/dogs-taekwondo.jpg"],
+        actorReply: { originalText: "？？？", translatedText: "？？？" }
+      },
+      {
+        id: 605,
+        username: "烟竹阮桐粥",
+        originalText: "这里有个老粉要翻车了，要不要帮忙翻一下？😭 @周翊然Tz",
+        translatedText: "여기 고인물 팬 한 명 타기 직전인데, 뒤집어줄 생각 없어? 😭 @周翊然Tz",
+        images: ["images/dog-nose.jpg"],
+        actorReply: { originalText: "🪏", translatedText: "🪏 (뒤집개)" }
+      },
+      {
+        id: 606,
+        username: "祈川十里春",
+        originalText: "小然vlog剪得怎么样了！",
+        translatedText: "소연 브이로그 편집은 어떻게 돼가!",
+        images: ["images/muscle-meme.jpg"],
+        actorReply: { originalText: "后面的内容很稀疏哈哈哈", translatedText: "뒷부분 내용이 아주 드문드문해 하하하" }
+      },
+      {
+        id: 607,
+        username: "是我的小土豆-Tz",
+        originalText: "然然你终于来了 我已经在被窝里了🛏️",
+        translatedText: "란란 너 드디어 왔구나 나 이미 이불 속이야🛏️",
+        images: ["images/bed-meme.jpg"],
+        actorReply: { originalText: "晚安哈哈哈", translatedText: "잘 자 ㅎㅎㅎ" }
+      },
+      {
+        id: 608,
+        username: "烟竹阮桐粥",
+        originalText: "茅妹现在在干嘛？💩 @周翊然Tz",
+        translatedText: "마오타이 지금 뭐 하는 중? 💩 @周翊然Tz",
+        images: ["images/maotai-tongue.jpg"],
+        actorReply: { originalText: "理毛", translatedText: "그루밍(스스로 청결하게 하기)" }
+      },
+      {
+        id: 609,
+        username: "咬一口脆皮年糕_",
+        originalText: "哥哥在剪辑吗 我想你了",
+        translatedText: "우리 오빠 편집하고 있어? 나 보고 싶어",
+        images: ["images/yiran-hostage.jpg"],
+        actorReply: { originalText: "在的在的", translatedText: "있어 있어" }
+      },
+      {
+        id: 610,
+        username: "是我的小土豆-Tz",
+        originalText: "你新打的耳洞看到了 恢复怎么样～",
+        translatedText: "너 새로 귀걸이 한 거 봤어 회복은 좀 어때～",
+        images: ["images/yiran-sunglasses.jpg"],
+        actorReply: { originalText: "现在都正常了", translatedText: "지금은 다 정상이야" }
+      },
+      {
+        id: 611,
+        username: "桃子泡泡面er",
+        originalText: "宝宝你看 我两天前打的耳洞！！",
+        translatedText: "아가, 이것 봐 나 이틀 전에 뚫은 귀걸이(귀)야！！",
+        images: ["images/earring-new.jpg"],
+        actorReply: { originalText: "好看！", translatedText: "예쁘네！" }
+      },
+      {
+        id: 612,
+        username: "樱花味的cherry",
+        originalText: "哥哥吃法棍了吗？☺️",
+        translatedText: "오빠 바게트 먹었어? ☺️",
+        images: ["images/baguette-girl.jpg"],
+        actorReply: { originalText: "差点把牙磕掉", translatedText: "이 다 부러질 뻔했어" }
+      },
+      {
+        id: 613,
+        username: "家有小狗i开车",
+        originalText: "给我回复我的csgo段位能到S",
+        translatedText: "나한테 답글 달아주면 내 csgo 등급(랭크) S등급까지 간다",
+        images: ["images/s-rank.jpg"],
+        actorReply: { originalText: "我已经S了🫡", translatedText: "난 이미 S야🫡" }
+      },
+      {
+        id: 614,
+        username: "家有小狗i开车",
+        originalText: "周翊然给我回复！",
+        translatedText: "주익연 나한테 답글 달아줘！",
+        images: ["images/wechat-capture.jpg"],
+        actorReply: { originalText: "[수박짤]", translatedText: "(수박 짤로 답장)" }
+      },
+      {
+        id: 615,
+        username: "秃兮",
+        originalText: "然然手势舞拍了多久😏",
+        translatedText: "란란 손댄스 챌린지 얼마나 오랫동안 찍었어😏",
+        images: ["images/dance-challenge.jpg"],
+        actorReply: { originalText: "雨里拍了半个小时左右😂", translatedText: "빗속에서 반 시간(30분) 정도 찍었나 봐😂" }
+      },
+      {
+        id: 616,
+        username: "芝士椰啵脆",
+        originalText: "宝宝我过几天要做个小手术好紧张怎么办",
+        translatedText: "아가 나 며칠 뒤에 작은 수술을 받아야 해서 너무 긴장되는데 어떡하지?",
+        images: ["images/shy-meme.jpg"],
+        actorReply: { originalText: "会没事的 不要紧张 🫡", translatedText: "다 잘 풀릴 거야 긴장하지 마 🫡" }
+      },
+      {
+        id: 617,
+        username: "_周의粥",
+        originalText: "宝宝这件衣服你到底有多喜欢，都破了还穿，是给我们开的\"加油窗口\"吗？不行我要第一个进场🙃",
+        translatedText: "아가 이 옷을 도대체 얼마나 아끼는 거야, 이렇게 찢어졌는데도 안 바꾸고 계속 입는 거 우리 보라고 열어둔 \"응원 창구\"야? 안 되겠다 내가 1등으로 입장해야지🙃",
+        images: ["images/torn-clothes.jpg"],
+        actorReply: { originalText: "衣柜里拿到什么穿什么😂", translatedText: "옷장에 잡히는 대로 입는 거라😂" }
+      },
+      {
+        id: 618,
+        username: "家有小狗i开车",
+        originalText: "oi 哥哥我今天生日！",
+        translatedText: "oi 오빠 나 오늘 생日이야！",
+        images: ["images/d-day-0.jpg"],
+        actorReply: { originalText: "生日快乐！", translatedText: "생일 축하해！" }
+      },
+      {
+        id: 619,
+        username: "咬一口脆皮年糕_",
+        originalText: "哥哥帮我选一个内耳钉",
+        translatedText: "오빠 나 귀창(인너컨츠) 피어싱 하나만 골라줘",
+        images: ["images/earring-pick.jpg"],
+        actorReply: { originalText: "第一个真的好看", translatedText: "첫 번째 거 진짜 예쁘다" }
+      },
+      {
+        id: 620,
+        username: "锦鲤然子_",
+        originalText: "你看！",
+        translatedText: "이것 좀 봐！",
+        images: ["images/childhood-circle.jpg"],
+        actorReply: { originalText: "有意思", translatedText: "재미있네" }
+      },
+      {
+        id: 621,
+        username: "梨涡加一又加一",
+        originalText: "哥哥！！！我刚买！..是勒克莱尔年末在阿布扎比猛刷紫色的那辆车！！",
+        translatedText: "오빠！！！ 나 방금 샀어！.. 연말에 르클레르(샤를)가 아부다비에서 엄청나게 보라색(패스티스트 랩) 찍었던 그 차야！！",
+        images: ["images/racing-kid.jpg"],
+        actorReply: { originalText: "sf25", translatedText: "sf25" }
+      },
+      {
+        id: 622,
+        username: "司徒绾w",
+        originalText: "宝宝猜猜这是什么品种",
+        translatedText: "아가 이거 무슨 품종인지 맞춰봐",
+        images: ["images/black-cat.jpg"],
+        actorReply: { originalText: "哈吉米", translatedText: "하길미(고양이 지칭 밈)" }
+      },
+      {
+        id: 623,
+        username: "Tttt_zzzy3199",
+        originalText: "哥哥晚上一个人看怪奇物语好怕怎么办🤤",
+        translatedText: "오빠 밤에 혼자 기묘한 이야기(怪奇物语) 보면 무서운데 어떡해🤤",
+        images: [],
+        actorReply: { originalText: "那白天看🙄", translatedText: "그럼 낮에 봐🙄" }
+      },
+      {
+        id: 624,
+        username: "婧然Tz-",
+        originalText: "从法国回来第一件事是什么",
+        translatedText: "프랑스에서 돌아와서 제일 먼저 한 일이 뭐야",
+        images: [],
+        actorReply: { originalText: "回家睡觉😂", translatedText: "집에 와서 잠자기😂" }
+      },
+      {
+        id: 625,
+        username: "Kiojydbxkwydnckdyebc",
+        originalText: "吃小龙坎了吗 跟在四川吃的一个味道",
+        translatedText: "샤오롱칸(훠궈 브랜드) 먹었어? 사천에서 먹었던 맛이랑 똑같아",
+        images: [],
+        actorReply: { originalText: "吃了 还不错哈哈哈", translatedText: "먹었어 꽤 괜찮더라 ㅎㅎㅎ" }
+      },
+      {
+        id: 626,
+        username: "要再见翊面",
+        originalText: "翊总手势舞进步了~ 看来是背后偷偷练了",
+        translatedText: "연총(익연 총재) 손댄스 실력이 늘었네~ 보아하니 뒤에서 몰래 연습했나 봐",
+        images: [],
+        actorReply: { originalText: "吃火锅的时候也在练", translatedText: "훠궈 먹을 때도 연습했어" }
+      },
+      {
+        id: 627,
+        username: "Yzraaaaaaaa",
+        originalText: "哥哥最近想打耳洞 打完之后护理有什么tips吗",
+        translatedText: "오빠 최근에 귀 뚫고 싶은데 뚫고 나서 관리 팁 같은 거 있어?",
+        images: [],
+        actorReply: { originalText: "滴眼药水消炎好 具体名字忘了 还有红霉素眼膏真的不错", translatedText: "안약(점안액) 넣어서 소염해주면 좋아 구체적인 이름은 까먹었어 그리고 에리스로마이신(안연고 등 연고류) 연고 진짜 좋아" }
+      }
+    ]
   }
-  ];
+];
