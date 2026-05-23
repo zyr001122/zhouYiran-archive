f78b30c2c9988037484932023c94452b2b6d88a2
zyr001122 <vazseyo@gmail.com>
2026-05-23 14:47:21 +0900

    Update data.js

--- data.js
+++ data.js
@@ -355,19 +355,19 @@ const posts = [
     id: 2,
     date: "2026-04-22T20:30",
     weiboUrl: "https://weibo.com/5994946077/5290585814469338",
-    originalText: "来了 ​​​",
-    translatedText: "왔어",
+    originalText: "周翊然超话 来了 ​​​",
+    translatedText: "주익연 초화 왔어",
     images: [],
     comments: [
       {
         id: 202,
         username: "只吃两碗饭-",
         originalText: "请问这位训狗届最严厉的父亲 茅妹和拉瑞除了坐下和不准吃开发出什么新技能了吗！🙊",
-        translatedText: "반려견 훈련계의 가장 엄격한 아버지에게 질문 마오타이랑 페라리는 앉아랑 먹지 마 말고 새로 배운 개인기 있어?! 🙊",
+        translatedText: "반려견 훈련계의 가장 엄격한 아버님께 여쭙니다 마오메이랑 라루이는 앉아랑 먹지 마 말고 새로 개발한 개인기 있어?! 🙊",
         images: ["images/maotai-hat.jpg"],
         actorReply: {
           originalText: "还有 站起来",
-          translatedText: "그거랑 일어서기"
+          translatedText: "그리고 일어서기"
         }
       },
       {
@@ -385,11 +385,11 @@ const posts = [
         id: 204,
         username: "熹然Z相逢",
         originalText: "翊然，此时此刻看着它，你在想什么呢～",
-        translatedText: "익연, 이 순간에 보면서 무슨 생각 하고 있던 거야?~",
+        translatedText: "익연아 지금 이 순간 이걸 보면서 무슨 생각 하고 있어~",
         images: ["images/black-sportscar.jpg"],
         actorReply: {
           originalText: "在想 弄什么涂装好看",
-          translatedText: "어떤 랩핑 해야 예쁠까"
+          translatedText: "어떤 랩핑 해야 예쁠까 생각 중"
         }
       },
       {
@@ -400,14 +400,14 @@ const posts = [
         images: ["images/ferrari-headset.jpg"],
         actorReply: {
           originalText: "法拉利什么段位哈哈哈哈",
-          translatedText: "페라리는 티어 뭔데 ㅋㅋㅋㅋ"
+          translatedText: "페라리 티어 뭔데 ㅋㅋㅋㅋ"
         }
       },
       {
         id: 206,
         username: "翊分钟恋人z",
         originalText: "给假面骑士 滑雪 开车 打瓦排个名 我好想知道现在假面骑士排第几🤭🤭",
-        translatedText: "가면라이더 스키 운전 발로란트 순위 매겨줘 지금 가면라이더가 몇 등인지 너무 알고 싶어 🤭🤭",
+        translatedText: "가면라이더 스키 운전 발로란트 순위 좀 매겨줘 지금 가면라이더가 몇 등인지 너무 알고 싶어 🤭🤭",
         images: ["images/figure-ski.jpg"],
         actorReply: {
           originalText: "每个季节 有每个季节该做的事情🫡",
@@ -451,7 +451,7 @@ const posts = [
         id: 210,
         username: "BianTaoTi-",
         originalText: "你怎么只喷这个贴纸 你没有别的喷漆图案了吗哈哈哈哈😂",
-        translatedText: "왜 이 스티커만 뿌려 다른 스프레이 없어? ㅋㅋㅋㅋ 😂",
+        translatedText: "너 왜 이 스티커만 뿌려 다른 스프레이 없어? ㅋㅋㅋㅋ 😂",
         images: ["images/valorant-sticker.jpg"],
         actorReply: {
           originalText: "这是给队友点赞用的",
@@ -466,7 +466,7 @@ const posts = [
         images: ["images/alley.jpg", "images/china-film.jpg"],
         actorReply: {
           originalText: "自己洗照片  厉害啊",
-          translatedText: "직접 인화라니 대단하다"
+          translatedText: "사진을 직접 인화한다니 대단하다"
         }
       },
       {
@@ -498,6 +498,8 @@ const posts = [
         translatedText: "오빠 지금 뭐 해 사진 올려서 보고해 줘",
         images: ["images/ribbon-dog.jpg"],
         actorReply: {
+          originalText: "(사진으로 답장)",
+          translatedText: "(사진으로 답장)",
           replyImages: ["images/xianxia-movie.jpg"]
         }
       },
@@ -516,7 +518,7 @@ const posts = [
         id: 216,
         username: "Campbe_眠雪碎眠悸",
         originalText: "最近我皮肤真的不是很好，他们都让我自己补充点维生素，但是我根本不需要那玩意，因为你就是我的维E。",
-        translatedText: "요즘 내 피부가 진짜 안 좋나봐 다들 나보고 비타민 좀 챙겨 먹으래 근데 난 그런 거 전혀 필요 없는데 왜냐면 네가 내 비타민E니까.",
+        translatedText: "요즘 내 피부가 진짜 안 좋나봐 다들 나보고 비타민 좀 챙겨 먹으라는데 난 그런 거 전혀 필요 없어 왜냐면 네가 내 비타민E니까.",
         images: ["images/mint-meme.jpg"],
         actorReply: {
           originalText: "…..",
@@ -538,7 +540,7 @@ const posts = [
         id: 218,
         username: "忙内专家",
         originalText: "猜你想念重庆的火锅了",
-        translatedText: "충칭 훠궈 땡기지",
+        translatedText: "네가 충칭 훠궈 그리워하고 있을 거라고 추측 중",
         images: ["images/hotpot1.jpg", "images/hotpot2.jpg"],
         actorReply: {
           originalText: "又来诱惑我🙂",
@@ -573,6 +575,278 @@ const posts = [
         originalText: "最后分享一张 上班的照片 我先走了下次见！！",
         translatedText: "마지막으로 출근 사진 한 장 공유할게 나 먼저 갈게 다음에 봐!!",
         images: ["images/work-back.jpg"],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 3,
+    date: "2026-03-22T15:30",
+    weiboUrl: "https://weibo.com/5994946077",
+    originalText: "hello ​​​",
+    translatedText: "hello",
+    images: [],
+    comments: [
+      {
+        id: 302,
+        username: "哭泣鱼丸丸丸-",
+        originalText: "Hi！好看吗！小然@周翊然Tz",
+        translatedText: "Hi！예뻐? 소연@周翊然Tz",
+        images: ["images/sunset-3shot.jpg"],
+        actorReply: {
+          originalText: "手机拍的吗",
+          translatedText: "핸드폰으로 찍은 거야"
+        }
+      },
+      {
+        id: 303,
+        username: "烟竹阮桐粥",
+        originalText: "轮家庭地位这一块🤔 @周翊然Tz",
+        translatedText: "집안 서열에 대해서는 말이지🤔 @周翊然Tz",
+        images: ["images/family-3shot.jpg"],
+        actorReply: {
+          originalText: "？？",
+          translatedText: "？？"
+        }
+      },
+      {
+        id: 304,
+        username: "yi翊口小周",
+        originalText: "哥哥 法拉利也来开赛车啦！！",
+        translatedText: "오빠 페라리도 레이싱카 타러 왔어!!",
+        images: ["images/ferrari-racing.jpg"],
+        actorReply: {
+          originalText: "牛………..",
+          translatedText: "대박………."
+        }
+      },
+      {
+        id: 305,
+        username: "樱花味的cherry",
+        originalText: "回重庆吃啥好吃的没有啊？🥰",
+        translatedText: "충칭 돌아가서 맛있는 거 뭐 먹었어?🥰",
+        images: ["images/chongqing-bridge.jpg", "images/chongqing-food.jpg"],
+        actorReply: {
+          originalText: "你这家店怎么很熟悉",
+          translatedText: "너 이 가게 왜 이렇게 낯이 익지"
+        }
+      },
+      {
+        id: 306,
+        username: "r男朋友真的只想开车",
+        originalText: "是不是偷偷去学画画了🙄",
+        translatedText: "혹시 몰래 그림 배우러 다녔어🙄",
+        images: ["images/sunflower-drawing.jpg", "images/sunflower-drawing2.jpg"],
+        actorReply: {
+          originalText: "是不是画得不错😏",
+          translatedText: "나 그림 좀 잘 그리지😏"
+        }
+      },
+      {
+        id: 307,
+        username: "轻轻茉莉不加冰-",
+        originalText: "小然 好奇上次vlog里这个角度是咋拍的！🤔",
+        translatedText: "소연 궁금한 게 지난 브이로그에서 이 각도는 어떻게 찍은 거야!🤔",
+        images: ["images/paris-vlog.jpg"],
+        actorReply: {
+          originalText: "把相机放在地上 拍完再跑回去捡🫡",
+          translatedText: "카메라를 바닥에 두고 찍은 다음에 다시 뛰어가서 주워왔어🫡"
+        }
+      },
+      {
+        id: 308,
+        username: "脆皮灿包",
+        originalText: "来看日落咯@周翊然Tz",
+        translatedText: "일몰 보러 왔어@周翊然Tz",
+        images: ["images/sea-wave.jpg"],
+        actorReply: {
+          originalText: "拍的真好 手机拍的吗",
+          translatedText: "진짜 잘 찍었다 핸드폰으로 찍은 거야"
+        }
+      },
+      {
+        id: 309,
+        username: "然后煮翊碗周",
+        originalText: "然然快看 昨天一块钱转盘转到一只小乌龟🐢 可爱不🥰",
+        translatedText: "란란 빨리 봐 어제 일 위안짜리 뽑기 돌려서 꼬마 거북이 한 마리 뽑았어🐢 귀엽지🥰",
+        images: ["images/turtle.jpg"],
+        actorReply: {
+          originalText: "我小时候也抽到过这个",
+          translatedText: "나도 어릴 때 이거 뽑아본 적 있어"
+        }
+      },
+      {
+        id: 310,
+        username: "nuori纯",
+        originalText: "然然每次给yummy画的妆怎么都是一样的@周翊然Tz",
+        translatedText: "란란은 야미한테 메이크업해 줄 때마다 왜 다 똑같이 해줘@周翊然Tz",
+        images: ["images/apm-bear.jpg"],
+        actorReply: {
+          originalText: "实在想不到其他的了",
+          translatedText: "진짜 다른 게 안 떠올라서 그래"
+        }
+      },
+      {
+        id: 311,
+        username: "翊粒紫然Tz",
+        originalText: "谢谢周哥的翻牌，虽然还没翻，可能是忘了，但是提前谢谢了，周哥总不可能真的不翻吧，肯定不可能，毕竟哥哥超级心软，我怕一会翻我的时候哥哥看不到的我的感谢，这里提前谢谢全宇宙超级无敌第一帅的哥哥‼️你最棒‼️@周翊然Tz",
+        translatedText: "주 형 답글 고마워, 비록 아직 안 달아줬고 까먹었을 수도 있지만, 그래도 미리 고마워, 주 형이 진짜 답글 안 달아줄 리가 없잖아, 절대 그럴 리 없지, 왜냐면 오빠는 엄청 마음이 약하니까, 이따가 나한테 답글 달아줄 때 오빠가 내 감사 인사를 못 볼까 봐 미리 전 우주에서 제일 무敵 최고로 잘생긴 오빠한테 감사 인사를 남겨‼️ 네가 최고야‼️@周翊然Tz",
+        images: ["images/shaving.jpg"],
+        actorReply: {
+          originalText: "来了啊",
+          translatedText: "왔어"
+        }
+      },
+      {
+        id: 312,
+        username: "藤孥Love-翊",
+        originalText: "好玩吗这个迷你版赛车🙃",
+        translatedText: "재밌어 이 미니 버전 레이싱카🙃",
+        images: ["images/racing-assembly.jpg"],
+        actorReply: {
+          originalText: "拼起来要点时间😏",
+          translatedText: "조립하는 데 시간 좀 걸려😏"
+        }
+      },
+      {
+        id: 313,
+        username: "翊口韩式拌饭-Tz",
+        originalText: "哥哥！耳洞打的时候疼不疼啊 我也想打 但怕疼 推荐一个给我😚",
+        translatedText: "오빠！귀 뚫을 때 아파 안 아파 나도 뚫고 싶은데 아플까 봐 무서워 하나만 추천해 줘😚",
+        images: ["images/ear-gun.jpg"],
+        actorReply: {
+          originalText: "你打和我一个位置应该是最不疼的",
+          translatedText: "너도 나랑 같은 위치에 뚫으면 제일 안 아플 거야"
+        }
+      },
+      {
+        id: 314,
+        username: "我那比不直耶的男朋友",
+        originalText: "然然 好奇手里是什么@周翊然Tz",
+        translatedText: "란란 궁금한 게 손에 들고 있는 거 뭐야@周翊然Tz",
+        images: ["images/hand-food.jpg"],
+        actorReply: {
+          originalText: "牛肉干！",
+          translatedText: "육포！"
+        }
+      },
+      {
+        id: 315,
+        username: "JuscHe",
+        originalText: "好看的很😏 @周翊然Tz",
+        translatedText: "엄청 멋지네😏 @周翊然Tz",
+        images: ["images/cart-3shot.jpg"],
+        actorReply: {
+          originalText: "出门就能看f1是吧🙄",
+          translatedText: "집 밖에만 나가면 바로 F1 볼 수 있는 거지🙄"
+        }
+      },
+      {
+        id: 316,
+        username: "周了个粥呀",
+        originalText: "第一次发现你这么有绘画天赋",
+        translatedText: "너한테 이렇게 그림 실력(천부적인 재능)이 있는지 처음 알았어",
+        images: ["images/sunflower-1shot.jpg"],
+        actorReply: {
+          originalText: "真的假的？？",
+          translatedText: "진짜로？？"
+        }
+      },
+      {
+        id: 317,
+        username: "只修兔纸Tz",
+        originalText: "哥哥去重庆吃好吃的了吗❤️",
+        translatedText: "오빠 충칭 가서 맛있는 거 먹었어❤️",
+        images: ["images/chongqing-noodle.jpg"],
+        actorReply: {
+          originalText: "",
+          translatedText: "(사진으로 답장)",
+          replyImages: ["images/jeonbyeong-food.jpg"]
+        }
+      },
+      {
+        id: 318,
+        username: "白月光的月_tz",
+        originalText: "如何🏎️？",
+        translatedText: "레이싱은 어때🏎️？",
+        images: ["images/labubu-racing.jpg"],
+        actorReply: {
+          originalText: "？？ 这是你家吗",
+          translatedText: "？？ 여기 네 집이야"
+        }
+      },
+      {
+        id: 319,
+        username: "YZ翊小楠",
+        originalText: "哥哥你是不是最近偷偷健身了🥱",
+        translatedText: "오빠 너 혹시 최근에 몰래 운동(웨이트)했어🥱",
+        images: ["images/chongqing-v3.jpg"],
+        actorReply: {
+          originalText: "角度问题",
+          translatedText: "각도 때문이야"
+        }
+      },
+      {
+        id: 320,
+        username: "落知梦Tz",
+        originalText: "说，这个视频你拍了多久？",
+        translatedText: "말해봐, 이 영상 너 얼마나 오랫동안 찍었어？",
+        images: ["images/charles-challenge.jpg"],
+        actorReply: {
+          originalText: "一遍！",
+          translatedText: "한 번에！"
+        }
+      },
+      {
+        id: 321,
+        username: "yisiyiiiiii",
+        originalText: "看这是谁",
+        translatedText: "이게 누구인지 봐봐",
+        images: ["images/red-cart.jpg"],
+        actorReply: {
+          originalText: "这个人看起来应该挺会开赛车的",
+          translatedText: "이 사람 보니까 레이싱 엄청 잘하게 생겼네"
+        }
+      },
+      {
+        id: 322,
+        username: "追十七秒的日落",
+        originalText: "哥哥 这次回重庆拍摄悦游好吗！快推荐一些重庆打卡地方👽",
+        translatedText: "오빠 이번에 충칭 돌아가서 내셔널 지오그래픽 트래블러(悦游) 촬영하는 거 좋아！빨리 충칭에 가볼 만한 곳 몇 군데만 추천해 줘👽",
+        images: [],
+        actorReply: {
+          originalText: "大扶梯！ 真的很厉害 我还是第一次去",
+          translatedText: "대형 에스컬레이터(皇冠大扶梯)！ 진짜 엄청나 나도 이번에 처음 가봤어"
+        }
+      },
+      {
+        id: 323,
+        username: "喝口QQ粥-",
+        originalText: "哥哥你尚博勒的评分多少呀🫡",
+        translatedText: "오빠 너 체임버(尚博勒) 점수 몇 점이야🫡",
+        images: ["images/chamber-증사.jpg"],
+        actorReply: {
+          originalText: "400往上😏",
+          translatedText: "400점 이상😏"
+        }
+      },
+      {
+        id: 324,
+        username: "夜雨愀",
+        originalText: "哥哥和偶像拍手势舞是啥感觉🥰 @周翊然Tz",
+        translatedText: "오빠 롤모델(아이돌)이랑 같이 손댄스 챌린지 찍을 때 어떤 기분이었어🥰 @周翊然Tz",
+        images: [],
+        actorReply: {
+          originalText: "爽！！！！",
+          translatedText: "개쩔어！！！！"
+        }
+      },
+      {
+        id: 301,
+        username: "周翊然Tz",
+        originalText: "走啦 下次见各位🫡",
+        translatedText: "갈게 다음에 봐 다들🫡",
+        images: [],
         actorReply: null
       }
     ]
