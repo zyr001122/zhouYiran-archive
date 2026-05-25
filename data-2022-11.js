c7e22ea55b72e54585055dc711721dd42e29ab42
zyr001122 <vazseyo@gmail.com>
2026-05-25 14:19:37 +0900

    Create data-2022-11.js

---
+++ data-2022-11.js
@@ -0,0 +1,308 @@
+const posts_202211 = [
+  {
+    id: 301,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "哈喽哈喽，我来了",
+    translatedText: "안녕안녕, 나 왔어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 302,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "你们怎么都没睡",
+    translatedText: "다들 왜 안 자고 있어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 303,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "随机考考你们",
+    translatedText: "랜덤 퀴즈 낼게",
+    images: [],
+    comments: []
+  },
+  {
+    id: 304,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "你们想知道大结局吗",
+    translatedText: "대결말 알고 싶어?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 305,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "诶我就不说",
+    translatedText: "에이 안 알려줄 거야",
+    images: [],
+    comments: []
+  },
+  {
+    id: 306,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "哎今天任务有点重",
+    translatedText: "오늘 할 거 좀 많아",
+    images: [],
+    comments: []
+  },
+  {
+    id: 307,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "不知道说啥了",
+    translatedText: "뭐 얘기할지 모르겠어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 308,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "好想路透啊",
+    translatedText: "루머 올리고 싶다",
+    images: [],
+    comments: []
+  },
+  {
+    id: 309,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "要不收集表情包",
+    translatedText: "이모티콘 수집이나 할까",
+    images: [],
+    comments: []
+  },
+  {
+    id: 310,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "这个怎么样",
+    translatedText: "이거 어때",
+    images: ["images/2211-sweat-meme.jpg"],
+    comments: []
+  },
+  {
+    id: 311,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "这个呢",
+    translatedText: "이건?",
+    images: ["images/2211-kobongi.jpg"],
+    comments: []
+  },
+  {
+    id: 312,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "最近试了一下拍vlog",
+    translatedText: "요즘 브이로그 찍어봤어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 313,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "拍后感如图",
+    translatedText: "찍고 나서 느낀 점이 짤 같아",
+    images: ["images/2211-nod-meme.jpg"],
+    comments: []
+  },
+  {
+    id: 314,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "",
+    translatedText: "",
+    images: ["images/2211-dog-one-ear.jpg"],
+    comments: []
+  },
+  {
+    id: 315,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "你们的表情包呢？",
+    translatedText: "너네 이모티콘은?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 316,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "这个不错",
+    translatedText: "이거 괜찮다",
+    images: ["images/2211-ultraman.jpg"],
+    comments: []
+  },
+  {
+    id: 317,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "这个跟我的好像",
+    translatedText: "이거 나랑 닮은 것 같아",
+    images: ["images/2211-kuromi.jpg"],
+    comments: []
+  },
+  {
+    id: 318,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "Vlog这个东西一年也只能录一次",
+    translatedText: "브이로그는 일 년에 한 번밖에 못 찍는 것 같아",
+    images: [],
+    comments: []
+  },
+  {
+    id: 319,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "太难了",
+    translatedText: "너무 어려워",
+    images: [],
+    comments: []
+  },
+  {
+    id: 320,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "怎么还这么多人",
+    translatedText: "왜 아직도 이렇게 많은 거야",
+    images: [],
+    comments: []
+  },
+  {
+    id: 321,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "要11点了",
+    translatedText: "11시 다 됐어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 322,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "哎 kpi还差点",
+    translatedText: "아 kpi 아직 좀 부족한데",
+    images: [],
+    comments: []
+  },
+  {
+    id: 323,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "为什么有人说我11级",
+    translatedText: "왜 누가 나 11레벨이라고 하는 거야",
+    images: [],
+    comments: []
+  },
+  {
+    id: 324,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "去哪里看我几级",
+    translatedText: "내 레벨 어디서 확인해?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 325,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "有人截图我10级的吗",
+    translatedText: "나 10레벨 캡처한 사람 있어?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 326,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "好像看到有人让我自己p",
+    translatedText: "누가 나한테 직접 만들어보라고 하는 것 같던데",
+    images: ["images/2211-pig-pink.jpg"],
+    comments: []
+  },
+  {
+    id: 327,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "什么是小粉牌",
+    translatedText: "핑크 뱃지가 뭐야",
+    images: [],
+    comments: []
+  },
+  {
+    id: 328,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "怎么你们今天说的我都听不懂",
+    translatedText: "오늘은 왜 이렇게 하는 말마다 못 알아듣겠어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 329,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "有人说12级有小粉牌",
+    translatedText: "12레벨 되면 핑크 뱃지 생긴다고 하던데",
+    images: [],
+    comments: []
+  },
+  {
+    id: 330,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "但是我的任务不是小粉牌",
+    translatedText: "근데 내 임무는 핑크 뱃지가 아닌데",
+    images: [],
+    comments: []
+  },
+  {
+    id: 331,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "猜错了吧",
+    translatedText: "틀렸지?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 332,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "今天就到这里了",
+    translatedText: "오늘은 여기까지",
+    images: [],
+    comments: []
+  },
+  {
+    id: 333,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "明天要早起",
+    translatedText: "내일 일찍 일어나야 해",
+    images: [],
+    comments: []
+  },
+  {
+    id: 334,
+    date: "2022-11-08",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "早点睡铁子们",
+    translatedText: "일찍 자 얘들아",
+    images: [],
+    comments: []
+  }
+];
