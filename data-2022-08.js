e116511c98fe5b9bef752fb2b908f7aa82f16ab4
zyr001122 <vazseyo@gmail.com>
2026-05-25 14:13:32 +0900

    Create data-2022-08.js

---
+++ data-2022-08.js
@@ -0,0 +1,407 @@
+const posts_202208 = [
+  {
+    id: 101,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "好久不见",
+    translatedText: "오랜만이야",
+    images: [],
+    comments: []
+  },
+  {
+    id: 102,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "又来超话了",
+    translatedText: "슈퍼토픽 또 왔어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 103,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "大家最近有看二十不惑2吗？",
+    translatedText: "다들 요즘 이십불혹2 보고 있어?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 104,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "喜欢殷赏吗",
+    translatedText: "인상 좋아?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 105,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "刚去工作了",
+    translatedText: "방금 일하러 갔다 왔어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 106,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "现在在休息",
+    translatedText: "지금은 쉬는 중",
+    images: [],
+    comments: []
+  },
+  {
+    id: 107,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "我网不好",
+    translatedText: "나 인터넷이 별로야",
+    images: [],
+    comments: []
+  },
+  {
+    id: 108,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "说说话吧",
+    translatedText: "얘기나 하자",
+    images: [],
+    comments: []
+  },
+  {
+    id: 109,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "殷赏的嘴太毒了",
+    translatedText: "인상이 말이 너무 독하더라",
+    images: [],
+    comments: []
+  },
+  {
+    id: 110,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "但是我觉得说得挺好",
+    translatedText: "근데 나는 맞는 말 같던데",
+    images: [],
+    comments: []
+  },
+  {
+    id: 111,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "你们追到第几集了？",
+    translatedText: "몇 화까지 봤어?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 112,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "回来了",
+    translatedText: "다시 왔어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 113,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "考一考你们",
+    translatedText: "퀴즈 낼게",
+    images: [],
+    comments: []
+  },
+  {
+    id: 114,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "最喜欢殷赏哪句台词",
+    translatedText: "인상 대사 중에 제일 좋아하는 거 뭐야",
+    images: [],
+    comments: []
+  },
+  {
+    id: 115,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "最喜欢殷赏哪个造型",
+    translatedText: "인상 스타일 중에 제일 좋아하는 거 뭐야",
+    images: [],
+    comments: []
+  },
+  {
+    id: 116,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "我觉得殷赏发型不好看",
+    translatedText: "나는 인상 헤어스타일 별로인 것 같아",
+    images: [],
+    comments: []
+  },
+  {
+    id: 117,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "不要提泡脚桶",
+    translatedText: "족욕통 얘기는 꺼내지 마",
+    images: [],
+    comments: []
+  },
+  {
+    id: 118,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "我不需要泡脚桶",
+    translatedText: "족욕통 필요 없거든",
+    images: [],
+    comments: []
+  },
+  {
+    id: 119,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "最近青岛挺凉快",
+    translatedText: "요즘 칭다오 꽤 시원해",
+    images: [],
+    comments: []
+  },
+  {
+    id: 120,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "一休息就犯困",
+    translatedText: "쉬면 바로 졸려",
+    images: [],
+    comments: []
+  },
+  {
+    id: 121,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "想快点收工",
+    translatedText: "빨리 퇴근하고 싶다",
+    images: [],
+    comments: []
+  },
+  {
+    id: 122,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "都吃饭了吗",
+    translatedText: "다들 밥은 먹었어?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 123,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "太卡就不回复了",
+    translatedText: "너무 끊기면 답장 못 할 것 같아",
+    images: [],
+    comments: []
+  },
+  {
+    id: 124,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "工作人员跟我说发够25条可以让你们看到惊喜",
+    translatedText: "스태프가 25개 올리면 깜짝 이벤트 볼 수 있다고 했어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 125,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "今晚有没有殷赏？",
+    translatedText: "오늘 밤에 인상 나와?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 126,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "够25条没？",
+    translatedText: "25개 됐어?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 127,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "是不是够25了？",
+    translatedText: "이제 25개 된 거 맞지?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 128,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "我已经不会数了",
+    translatedText: "나도 이제 못 세겠어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 129,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "看到惊喜了吗？",
+    translatedText: "깜짝 이벤트 봤어?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 130,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "我没看见",
+    translatedText: "나는 못 봤는데",
+    images: [],
+    comments: []
+  },
+  {
+    id: 131,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "拜拜",
+    translatedText: "잘 있어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 132,
+    date: "2022-08-26",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "明天见",
+    translatedText: "내일 봐",
+    images: [],
+    comments: []
+  },
+  {
+    id: 133,
+    date: "2022-08-28",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "28号有惊喜 我任务完成了",
+    translatedText: "28일에 깜짝 이벤트 있어 내 임무 완수했다",
+    images: [],
+    comments: []
+  },
+  {
+    id: 134,
+    date: "2022-08-28",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "早上好 我来看彩蛋了",
+    translatedText: "좋은 아침 깜짝 이벤트 보러 왔어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 135,
+    date: "2022-08-28",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "谁做的 怎么是这样",
+    translatedText: "누가 만든 거야 왜 이래",
+    images: [],
+    comments: []
+  },
+  {
+    id: 136,
+    date: "2022-08-28",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "不如没有",
+    translatedText: "없는 게 나을 것 같은데",
+    images: [],
+    comments: []
+  },
+  {
+    id: 137,
+    date: "2022-08-28",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "但还是感谢微博",
+    translatedText: "그래도 웨이보한테 고맙긴 해",
+    images: [],
+    comments: []
+  },
+  {
+    id: 138,
+    date: "2022-08-28",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "昨天谢谢大家",
+    translatedText: "어제 다들 고마워",
+    images: [],
+    comments: []
+  },
+  {
+    id: 139,
+    date: "2022-08-28",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "期待下一次见面",
+    translatedText: "다음에 또 만나길 기대할게",
+    images: [],
+    comments: []
+  },
+  {
+    id: 140,
+    date: "2022-08-28",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "今天起太早有点困",
+    translatedText: "오늘 일찍 일어나서 좀 졸려",
+    images: [],
+    comments: []
+  },
+  {
+    id: 141,
+    date: "2022-08-28",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "你们早上吃的啥",
+    translatedText: "아침에 뭐 먹었어?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 142,
+    date: "2022-08-28",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "参考一下我中午吃",
+    translatedText: "참고해서 점심에 먹을게",
+    images: [],
+    comments: []
+  },
+  {
+    id: 143,
+    date: "2022-08-28",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "希望微博下次弄个帅点的彩蛋",
+    translatedText: "다음엔 웨이보가 좀 더 멋진 이벤트 만들어줬으면 해",
+    images: [],
+    comments: []
+  },
+  {
+    id: 144,
+    date: "2022-08-28",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "我会再来的",
+    translatedText: "또 올게",
+    images: [],
+    comments: []
+  },
+  {
+    id: 145,
+    date: "2022-08-28",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "拜拜 走了",
+    translatedText: "잘 있어 간다",
+    images: [],
+    comments: []
+  }
+];
