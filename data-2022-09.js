a4095960079fa267962bc9af12dedec2d859db71
zyr001122 <vazseyo@gmail.com>
2026-05-25 14:19:20 +0900

    Create data-2022-09.js

---
+++ data-2022-09.js
@@ -0,0 +1,281 @@
+const posts_202209 = [
+  {
+    id: 201,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "来聊一分钟的",
+    translatedText: "1분만 얘기하러 왔어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 202,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "今天起了个大早",
+    translatedText: "오늘 일찍 일어났어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 203,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "现在还有点困",
+    translatedText: "아직 좀 졸려",
+    images: [],
+    comments: []
+  },
+  {
+    id: 204,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "人呢",
+    translatedText: "다들 어딨어?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 205,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "大家在干嘛",
+    translatedText: "뭐 하고 있어?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 206,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "吃了吗",
+    translatedText: "밥은 먹었어?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 207,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "",
+    translatedText: "",
+    images: ["images/2209-pig-lay.jpg"],
+    comments: []
+  },
+  {
+    id: 208,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "都吃啥好吃的了",
+    translatedText: "맛있는 거 뭐 먹었어?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 209,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "有点饿",
+    translatedText: "좀 배고프다",
+    images: [],
+    comments: []
+  },
+  {
+    id: 210,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "",
+    translatedText: "",
+    images: ["images/2209-frog-cry.jpg"],
+    comments: []
+  },
+  {
+    id: 211,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "有什么好吃的推荐吗",
+    translatedText: "맛있는 거 추천해줄 것 있어?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 212,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "不想吃小龙虾😏",
+    translatedText: "새우 요리는 별로야😏",
+    images: [],
+    comments: []
+  },
+  {
+    id: 213,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "嗦粉是有点意思",
+    translatedText: "쏴펀은 좀 재밌는 것 같아",
+    images: [],
+    comments: []
+  },
+  {
+    id: 214,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "大家喝百事可乐了吗",
+    translatedText: "다들 펩시콜라 마셨어?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 215,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "阿宽最好吃😏",
+    translatedText: "아콴이 제일 맛있어😏",
+    images: [],
+    comments: []
+  },
+  {
+    id: 216,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "苕皮味真的正宗",
+    translatedText: "고구마 맛은 진짜 정통이야",
+    images: [],
+    comments: []
+  },
+  {
+    id: 217,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "月饼吃了水果味",
+    translatedText: "월병은 과일 맛으로 먹었어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 218,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "今天感受到大家的热情了",
+    translatedText: "오늘 다들 열정 진짜 느꼈어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 219,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "但是下次不要这样了",
+    translatedText: "근데 다음엔 이러지 마",
+    images: [],
+    comments: []
+  },
+  {
+    id: 220,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "聚集不好",
+    translatedText: "모이는 거 좋지 않아",
+    images: [],
+    comments: []
+  },
+  {
+    id: 221,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "万一受伤更不好",
+    translatedText: "혹시라도 다치면 더 안 좋잖아",
+    images: [],
+    comments: []
+  },
+  {
+    id: 222,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "咱们有机会见面的",
+    translatedText: "언젠간 만날 기회 있을 거야",
+    images: [],
+    comments: []
+  },
+  {
+    id: 223,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "懂了的扣1",
+    translatedText: "이해했으면 1 눌러줘",
+    images: [],
+    comments: []
+  },
+  {
+    id: 224,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "🙏铁子们",
+    translatedText: "🙏얘들아",
+    images: [],
+    comments: []
+  },
+  {
+    id: 225,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "",
+    translatedText: "",
+    images: ["images/2209-dog-muscle.jpg"],
+    comments: []
+  },
+  {
+    id: 226,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "说着说着更饿了",
+    translatedText: "얘기하다 보니 더 배고파졌어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 227,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "别光用我的表情包",
+    translatedText: "내 이모티콘만 쓰지 마",
+    images: [],
+    comments: []
+  },
+  {
+    id: 228,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "你们也发我一点表情包",
+    translatedText: "너네 이모티콘도 보내줘",
+    images: [],
+    comments: []
+  },
+  {
+    id: 229,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "这个不错",
+    translatedText: "이거 괜찮다",
+    images: ["images/2209-heart-hand.jpg"],
+    comments: []
+  },
+  {
+    id: 230,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "再送你们一个",
+    translatedText: "이것도 선물로 줄게",
+    images: ["images/2209-duck-weird.jpg"],
+    comments: []
+  },
+  {
+    id: 231,
+    date: "2022-09-15",
+    weiboUrl: "https://weibo.com/5994946077/5250067201524299",
+    originalText: "走了走了",
+    translatedText: "간다 간다",
+    images: [],
+    comments: []
+  }
+];
