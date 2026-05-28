e4b9ce48a8f0dc0ff0ef2b96d5be1683b3b3a24b
zyr001122 <vazseyo@gmail.com>
2026-05-28 14:27:06 +0900

    Create data-2023-01.js

---
+++ data-2023-01.js
@@ -0,0 +1,173 @@
+const posts_202301 = [
+  {
+    id: 401,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn1Ie4wN",
+    originalText: "我怕三十的鞭炮太响 你们会听不到我的祝福",
+    translatedText: "섣달그믐 폭죽 소리가 너무 클까봐 내 새해 인사가 안 들릴 것 같아서",
+    images: [],
+    comments: []
+  },
+  {
+    id: 402,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn2ooUCi",
+    originalText: "我怕初一的祝福太多 你们会看不到我的祝福",
+    translatedText: "설날에 새해 인사가 너무 많을까봐 내 인사가 안 보일 것 같아서",
+    images: [],
+    comments: []
+  },
+  {
+    id: 403,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn2LrMzu",
+    originalText: "提前祝大家新年快乐",
+    translatedText: "미리 새해 복 많이 받아",
+    images: [],
+    comments: []
+  },
+  {
+    id: 404,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn3iAJhb",
+    originalText: "",
+    translatedText: "",
+    images: ["images/2301-chicken-jump.jpg"],
+    comments: []
+  },
+  {
+    id: 405,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn3Vike3",
+    originalText: "你们今晚有没有跟家人一起吃年夜饭看春晚？",
+    translatedText: "오늘 밤에 가족이랑 같이 새해 저녁 먹고 춘완 봤어?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 406,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn4UsgGY",
+    originalText: "你们晚上都吃了啥 来说说",
+    translatedText: "저녁에 뭐 먹었어 얘기해봐",
+    images: [],
+    comments: []
+  },
+  {
+    id: 407,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn5cfvCR",
+    originalText: "我吃了饺子啊",
+    translatedText: "나는 만두 먹었어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 408,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn5rBfzP",
+    originalText: "除夕最重要的是团圆",
+    translatedText: "섣달그믐엔 가족이 함께하는 게 제일 중요하지",
+    images: [],
+    comments: []
+  },
+  {
+    id: 409,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn5wCDCv",
+    originalText: "",
+    translatedText: "",
+    images: ["images/2301-family-meal.jpg"],
+    comments: []
+  },
+  {
+    id: 410,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn5S7Beg",
+    originalText: "很感慨2022要过去了",
+    translatedText: "2022년이 지나가는 게 너무 감회가 새롭다",
+    images: [],
+    comments: []
+  },
+  {
+    id: 411,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn6EixjU",
+    originalText: "今年依旧很感谢你们的陪伴",
+    translatedText: "올해도 변함없이 함께해줘서 정말 고마워",
+    images: [],
+    comments: []
+  },
+  {
+    id: 412,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn6Lp5kb",
+    originalText: "感激 感恩 感谢",
+    translatedText: "감격 감사 고마워",
+    images: [],
+    comments: []
+  },
+  {
+    id: 413,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn81bmOk",
+    originalText: "23年我们一起加油",
+    translatedText: "23년도 같이 화이팅하자",
+    images: [],
+    comments: []
+  },
+  {
+    id: 414,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn87muH2",
+    originalText: "保持开心保持健康",
+    translatedText: "행복하고 건강하게 지내자",
+    images: [],
+    comments: []
+  },
+  {
+    id: 415,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn8CkPnP",
+    originalText: "成为更好的大人",
+    translatedText: "더 나은 어른이 되자",
+    images: [],
+    comments: []
+  },
+  {
+    id: 416,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn8J9Nsf",
+    originalText: "好了 祝福都收到了吧",
+    translatedText: "자 새해 인사 다 받았지?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 417,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn8ZpTEn",
+    originalText: "守完岁早点睡",
+    translatedText: "새해 맞이 다 했으면 일찍 자",
+    images: [],
+    comments: []
+  },
+  {
+    id: 418,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn94aMK0",
+    originalText: "晚安",
+    translatedText: "잘 자",
+    images: [],
+    comments: []
+  },
+  {
+    id: 419,
+    date: "2023-01-21",
+    weiboUrl: "https://weibo.com/5994946077/Mpn9uqmLd",
+    originalText: "真的走了啊",
+    translatedText: "진짜 간다",
+    images: [],
+    comments: []
+  }
+];
