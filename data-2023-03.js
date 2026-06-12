a37b9d3c3c543ed9241751c7cb5e1d8b494f142c
zyr001122 <vazseyo@gmail.com>
2026-06-12 23:43:58 +0900

    Add files via upload

---
+++ data-2023-03.js
@@ -0,0 +1,182 @@
+const posts_202303 = [
+  {
+    id: 501,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl2Nehbw",
+    originalText: "我来了",
+    translatedText: "나 왔어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 502,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl3fkLrf",
+    originalText: "祝所有女生节日快乐",
+    translatedText: "여성의 날 축하해",
+    images: [],
+    comments: []
+  },
+  {
+    id: 503,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl3v0zjK",
+    originalText: "分享图片",
+    translatedText: "사진 공유",
+    images: ["images/2303-pink-flower.jpg"],
+    comments: []
+  },
+  {
+    id: 504,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl3AitYA",
+    originalText: "开开心心",
+    translatedText: "행복하게",
+    images: [],
+    comments: []
+  },
+  {
+    id: 505,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl3FjQO1",
+    originalText: "快快乐乐",
+    translatedText: "즐겁게",
+    images: [],
+    comments: []
+  },
+  {
+    id: 506,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl3M7J3e",
+    originalText: "健健康康",
+    translatedText: "건강하게",
+    images: [],
+    comments: []
+  },
+  {
+    id: 507,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl5Q4DzJ",
+    originalText: "分享图片",
+    translatedText: "사진 공유",
+    images: ["images/2303-shinchan-turn.jpg"],
+    comments: []
+  },
+  {
+    id: 508,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl5TkDH5",
+    originalText: "自拍",
+    translatedText: "셀카",
+    images: ["images/2303-ultraman-tilt.jpg"],
+    comments: []
+  },
+  {
+    id: 509,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl6ol7LQ",
+    originalText: "分享图片",
+    translatedText: "사진 공유",
+    images: ["images/2303-shinchan-turn.jpg"],
+    comments: []
+  },
+  {
+    id: 510,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl6yq7w3",
+    originalText: "今天天气很好",
+    translatedText: "오늘 날씨 너무 좋다",
+    images: [],
+    comments: []
+  },
+  {
+    id: 511,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl6KfmNi",
+    originalText: "可以出门走走",
+    translatedText: "나가서 좀 걷기 좋아",
+    images: [],
+    comments: []
+  },
+  {
+    id: 512,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl6OE1Hv",
+    originalText: "和朋友们吃顿好的",
+    translatedText: "친구들이랑 맛있는 거 먹기도 좋고",
+    images: [],
+    comments: []
+  },
+  {
+    id: 513,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl7419qz",
+    originalText: "拍拍好看的照片",
+    translatedText: "예쁜 사진도 좀 찍고",
+    images: [],
+    comments: []
+  },
+  {
+    id: 514,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl7clQsG",
+    originalText: "让我看看你们拍的春天",
+    translatedText: "너네가 찍은 봄 보여줘",
+    images: [],
+    comments: []
+  },
+  {
+    id: 515,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl7FigDo",
+    originalText: "分享图片",
+    translatedText: "사진 공유",
+    images: ["images/2303-thumbs-up.jpg"],
+    comments: []
+  },
+  {
+    id: 516,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl86r6xs",
+    originalText: "周末也可以出去玩儿",
+    translatedText: "주말에도 나가서 놀기 좋아",
+    images: [],
+    comments: []
+  },
+  {
+    id: 517,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl9pEooT",
+    originalText: "我准备吃饭了",
+    translatedText: "나 이제 밥 먹으려고",
+    images: [],
+    comments: []
+  },
+  {
+    id: 518,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwl9P9NSa",
+    originalText: "还没想好 你们给点建议？",
+    translatedText: "아직 뭐 먹을지 못 정했어 추천 좀 해줄래?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 519,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/MwlagiDK9",
+    originalText: "知道了👌",
+    translatedText: "알겠어👌",
+    images: [],
+    comments: []
+  },
+  {
+    id: 520,
+    date: "2023-03-08",
+    weiboUrl: "https://weibo.com/5994946077/Mwlan6v07",
+    originalText: "拜拜",
+    translatedText: "잘 있어",
+    images: [],
+    comments: []
+  }
+];
