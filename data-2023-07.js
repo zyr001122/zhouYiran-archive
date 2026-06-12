094a862109d9225462dff3d0f7d8382e74c7a1a4
zyr001122 <vazseyo@gmail.com>
2026-06-13 00:40:44 +0900

    Add files via upload

---
+++ data-2023-07.js
@@ -0,0 +1,30 @@
+const posts_202307 = [
+  {
+    id: 601,
+    date: "2023-07-31",
+    weiboUrl: "https://weibo.com/5994946077/NcqG8tQw8",
+    originalText: "放饭了，大家都在干嘛呢",
+    translatedText: "밥 먹을 시간이야, 다들 뭐 하고 있어",
+    images: [],
+    comments: []
+  },
+  {
+    id: 602,
+    date: "2023-07-31",
+    weiboUrl: "https://weibo.com/5994946077/NcqKDqwzu",
+    originalText: "今天吃外婆菜炒蛋",
+    translatedText: "오늘은 와이포차이 계란볶음 먹었어",
+    note: "와이포차이: 절인 채소 종류 중 하나",
+    images: ["images/2307-egg-rice.jpg"],
+    comments: []
+  },
+  {
+    id: 603,
+    date: "2023-07-31",
+    weiboUrl: "https://weibo.com/5994946077/NcqTQgJP8",
+    originalText: "走了走了 开工了",
+    translatedText: "간다 간다 일 시작해야 해",
+    images: ["images/2307-ultraman-bye.jpg"],
+    comments: []
+  }
+];
