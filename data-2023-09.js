692d8750c2ed7b6d0911d7bab77813dbb14c23bb
zyr001122 <vazseyo@gmail.com>
2026-06-15 00:19:14 +0900

    Add files via upload

---
+++ data-2023-09.js
@@ -0,0 +1,119 @@
+const posts_202309 = [
+  {
+    id: 801,
+    date: "2023-09-08",
+    weiboUrl: "https://weibo.com/5994946077/Nio6RABRb",
+    originalText: "🌧️大家注意安全 都要平平安安的",
+    translatedText: "🌧️다들 안전 조심해 모두 무사히",
+    images: [],
+    comments: []
+  },
+  {
+    id: 802,
+    date: "2023-09-22",
+    weiboUrl: "https://weibo.com/5994946077/NkuYB5Wqu",
+    originalText: "不好意思",
+    translatedText: "미안해",
+    images: [],
+    comments: []
+  },
+  {
+    id: 803,
+    date: "2023-09-22",
+    weiboUrl: "https://weibo.com/5994946077/Nkv01cBYy",
+    originalText: "我来晚了",
+    translatedText: "늦게 왔어",
+    images: ["images/2309-ultraman.jpg"],
+    comments: []
+  },
+  {
+    id: 804,
+    date: "2023-09-22",
+    weiboUrl: "https://weibo.com/5994946077/NkuY8dW64",
+    originalText: "大家吃晚饭了吗",
+    translatedText: "다들 저녁 먹었어?",
+    images: [],
+    comments: []
+  },
+  {
+    id: 805,
+    date: "2023-09-22",
+    weiboUrl: "https://weibo.com/5994946077/Nkv4ZnjbR",
+    originalText: "福建还挺热",
+    translatedText: "푸젠은 아직도 꽤 덥네",
+    images: [],
+    comments: []
+  },
+  {
+    id: 806,
+    date: "2023-09-22",
+    weiboUrl: "https://weibo.com/5128509197/Nkv3X0F2n",
+    originalText: "给你看一家三口 希望你能看到😭",
+    translatedText: "세 식구 그림 보여줄게 봐줬으면 좋겠어😭",
+    images: ["images/2309-cat-drawing.jpg"],
+    comments: [
+      {
+        id: 80601,
+        username: "周翊然Tz",
+        originalText: "你画的吗",
+        translatedText: "네가 그린 거야?",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 807,
+    date: "2023-09-22",
+    weiboUrl: "https://weibo.com/5994946077/Nkv5BzRVq",
+    originalText: "大家还是要注意安全",
+    translatedText: "다들 안전 조심해",
+    images: [],
+    comments: []
+  },
+  {
+    id: 808,
+    date: "2023-09-22",
+    weiboUrl: "https://weibo.com/5994946077/Nkv5K81jo",
+    originalText: "见面的机会还有很多",
+    translatedText: "만날 기회는 아직 많아",
+    images: [],
+    comments: []
+  },
+  {
+    id: 809,
+    date: "2023-09-22",
+    weiboUrl: "https://weibo.com/2874109744/Nkv5K96RW",
+    originalText: "不看看假面腰带吗 宝🥲",
+    translatedText: "가면라이더 벨트 한번 봐봐 자기🥲",
+    images: ["images/2309-ultraman-belt.jpg"],
+    comments: [
+      {
+        id: 80901,
+        username: "周翊然Tz",
+        originalText: "非常棒👍",
+        translatedText: "완전 좋은데👍",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 810,
+    date: "2023-09-22",
+    weiboUrl: "https://weibo.com/5994946077/Nkv6yy2Mz",
+    originalText: "溜了溜了",
+    translatedText: "슬쩍 가볼게",
+    images: [],
+    comments: []
+  },
+  {
+    id: 811,
+    date: "2023-09-22",
+    weiboUrl: "https://weibo.com/5994946077/Nkv6YzjUt",
+    originalText: "拜拜",
+    translatedText: "잘 있어",
+    images: [],
+    comments: []
+  }
+];
