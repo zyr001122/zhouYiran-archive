2cfcdde16821e05954f11217d30cf6484261710a
zyr001122 <vazseyo@gmail.com>
2026-06-15 01:20:34 +0900

    Update data-2023-09.js

--- data-2023-09.js
+++ data-2023-09.js
@@ -4,7 +4,7 @@ const posts_202309 = [
     date: "2023-09-08",
     weiboUrl: "https://weibo.com/5994946077/Nio6RABRb",
     originalText: "🌧️大家注意安全 都要平平安安的",
-    translatedText: "🌧️다들 안전 조심해 모두 무사히",
+    translatedText: "🌧️다들 다치지말고 조심해",
     images: [],
     comments: []
   },
@@ -22,7 +22,7 @@ const posts_202309 = [
     date: "2023-09-22",
     weiboUrl: "https://weibo.com/5994946077/Nkv01cBYy",
     originalText: "我来晚了",
-    translatedText: "늦게 왔어",
+    translatedText: "내가 좀 늦었지",
     images: ["images/2309-ultraman.jpg"],
     comments: []
   },
@@ -40,7 +40,7 @@ const posts_202309 = [
     date: "2023-09-22",
     weiboUrl: "https://weibo.com/5994946077/Nkv4ZnjbR",
     originalText: "福建还挺热",
-    translatedText: "푸젠은 아직도 꽤 덥네",
+    translatedText: "푸젠은 아직도 꽤 더워",
     images: [],
     comments: []
   },
@@ -48,17 +48,19 @@ const posts_202309 = [
     id: 806,
     date: "2023-09-22",
     weiboUrl: "https://weibo.com/5128509197/Nkv3X0F2n",
+    authorName: "饺子不想练琴",
+    authorAvatar: "🥣",
     originalText: "给你看一家三口 希望你能看到😭",
-    translatedText: "세 식구 그림 보여줄게 봐줬으면 좋겠어😭",
+    translatedText: "세 식구 그림 보여줄게 네가 봐줬으면😭",
     images: ["images/2309-cat-drawing.jpg"],
     comments: [
       {
         id: 80601,
-        username: "饺子不想练琴",
-        originalText: "",
-        translatedText: "",
+        username: "周翊然Tz",
+        originalText: "你画的吗",
+        translatedText: "네가 그린 거야?",
         images: [],
-        actorReply: { originalText: "你画的吗", translatedText: "네가 그린 거야?" }
+        actorReply: null
       }
     ]
   },
@@ -67,7 +69,7 @@ const posts_202309 = [
     date: "2023-09-22",
     weiboUrl: "https://weibo.com/5994946077/Nkv5BzRVq",
     originalText: "大家还是要注意安全",
-    translatedText: "다들 안전 조심해",
+    translatedText: "그래도 다들 꼭 조심해야 돼",
     images: [],
     comments: []
   },
@@ -76,7 +78,7 @@ const posts_202309 = [
     date: "2023-09-22",
     weiboUrl: "https://weibo.com/5994946077/Nkv5K81jo",
     originalText: "见面的机会还有很多",
-    translatedText: "만날 기회는 아직 많아",
+    translatedText: "만날 기회는 앞으로도 많으니까",
     images: [],
     comments: []
   },
@@ -84,17 +86,19 @@ const posts_202309 = [
     id: 809,
     date: "2023-09-22",
     weiboUrl: "https://weibo.com/2874109744/Nkv5K96RW",
+    authorName: "邂逅翊夏_",
+    authorAvatar: "🥣",
     originalText: "不看看假面腰带吗 宝🥲",
-    translatedText: "가면라이더 벨트 한번 봐봐 자기🥲",
+    translatedText: "가면라이더 벨트인데 안 봐줄 거야?🥲",
     images: ["images/2309-ultraman-belt.jpg"],
     comments: [
       {
         id: 80901,
-        username: "邂逅翊夏_",
-        originalText: "",
-        translatedText: "",
+        username: "周翊然Tz",
+        originalText: "非常棒👍",
+        translatedText: "완전 좋은데👍",
         images: [],
-        actorReply: { originalText: "非常棒👍", translatedText: "완전 좋은데👍" }
+        actorReply: null
       }
     ]
   },
@@ -103,7 +107,7 @@ const posts_202309 = [
     date: "2023-09-22",
     weiboUrl: "https://weibo.com/5994946077/Nkv6yy2Mz",
     originalText: "溜了溜了",
-    translatedText: "슬쩍 가볼게",
+    translatedText: "ㅌㅌ",
     images: [],
     comments: []
   },
@@ -112,7 +116,7 @@ const posts_202309 = [
     date: "2023-09-22",
     weiboUrl: "https://weibo.com/5994946077/Nkv6YzjUt",
     originalText: "拜拜",
-    translatedText: "잘 있어",
+    translatedText: "빠이빠이",
     images: [],
     comments: []
   }
