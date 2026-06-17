ac2c42d7dee26b5a9416df4ebb0416aaf7991991
zyr001122 <vazseyo@gmail.com>
2026-06-17 16:51:26 +0900

    Add files via upload

---
+++ images/data-2024-01.js
@@ -0,0 +1,208 @@
+const posts_202401 = [
+  {
+    id: 1201,
+    date: "2024-01-13",
+    weiboUrl: "https://weibo.com/5994946077/NBHbmaHet",
+    originalText: "感谢大家，我继续努力💪🏻",
+    translatedText: "다들 고마워, 계속 열심히 할게💪🏻",
+    images: ["images/2401-trophy.jpg"],
+    comments: []
+  },
+  {
+    id: 1202,
+    date: "2024-01-21",
+    weiboUrl: "https://weibo.com/5994946077/NCT9ktEM6",
+    originalText: "感受零下二十五度的崇礼❄️#本周报备#",
+    translatedText: "영하 25도 충리를 느끼는 중❄️#本周报备#",
+    images: [
+      "images/2401-cheonglie1.jpg",
+      "images/2401-cheonglie2.jpg",
+      "images/2401-cheonglie3.jpg",
+      "images/2401-cheonglie4.jpg",
+      "images/2401-cheonglie5.jpg",
+      "images/2401-cheonglie6.jpg"
+    ],
+    comments: []
+  },
+  {
+    id: 1203,
+    date: "2024-01-26",
+    weiboUrl: "https://weibo.com/5994946077/NDDkTyqDy",
+    originalText: "拍摄现场🤫#本周报备#",
+    translatedText: "촬영 현장🤫#本周报备#",
+    images: [
+      "images/2401-set1.jpg",
+      "images/2401-set2.jpg",
+      "images/2401-set3.jpg",
+      "images/2401-set4.jpg",
+      "images/2401-set5.jpg",
+      "images/2401-set6.jpg"
+    ],
+    comments: [
+      {
+        id: 120301,
+        username: "玥玥Tz",
+        originalText: "法拉利怎么也穿红的",
+        translatedText: "페라리는 왜 또 빨간 옷이야",
+        images: [],
+        actorReply: { originalText: "喜庆", translatedText: "경사스러워서" }
+      },
+      {
+        id: 120302,
+        username: "_最惬意",
+        originalText: "这29个未接来电是你打的吗",
+        translatedText: "이 부재중 전화 29개 너가 건 거야?",
+        images: ["images/2401-remote.jpg"],
+        actorReply: { originalText: "", translatedText: "", replyImages: ["images/2401-three-dogs.jpg"] }
+      },
+      {
+        id: 120303,
+        username: "佩佩睡着惹_",
+        originalText: "快看我做的花花，好不好看",
+        translatedText: "내가 만든 꽃 좀 봐줘, 예쁘지 않아?",
+        images: ["images/2401-snow-rose.jpg"],
+        actorReply: { originalText: "👍", translatedText: "👍" }
+      }
+    ]
+  },
+  {
+    id: 1204,
+    date: "2024-01-31",
+    weiboUrl: "https://weibo.com/7254508423/NEn4s8xHs",
+    authorName: "你荔枝亦点啊房",
+    authorAvatar: "🥣",
+    originalText: "嗨网友哥哥 你来啦",
+    translatedText: "안녕 네티즌 오빠 왔구나",
+    images: [],
+    comments: [
+      {
+        id: 120401,
+        username: "周翊然Tz",
+        originalText: "冒个泡",
+        translatedText: "잠깐 인사하러 왔어",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1205,
+    date: "2024-01-31",
+    weiboUrl: "https://weibo.com/7820691555/NEn6hiRYi",
+    authorName: "吉不吉岛普吉岛",
+    authorAvatar: "🥣",
+    originalText: "新年你打算要怎么过🤔",
+    translatedText: "새해 어떻게 보낼 거야🤔",
+    images: ["images/2401-fireworks.jpg"],
+    comments: [
+      {
+        id: 120501,
+        username: "周翊然Tz",
+        originalText: "在剧组过💪🏻",
+        translatedText: "촬영장에서 보낼 거야💪🏻",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1206,
+    date: "2024-01-31",
+    weiboUrl: "https://weibo.com/6822950966/NEn7vwZeX",
+    authorName: "忙内专家",
+    authorAvatar: "🥣",
+    originalText: "小然你看有100个你的造型 可爱吗！🥰🥰🥰",
+    translatedText: "익연아 봐봐 너 코디 100개 있어 귀엽지!🥰🥰🥰",
+    images: ["images/2401-characters.jpg"],
+    comments: [
+      {
+        id: 120601,
+        username: "周翊然Tz",
+        originalText: "🤝🏻👍",
+        translatedText: "🤝🏻👍",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1207,
+    date: "2024-01-31",
+    weiboUrl: "https://weibo.com/7854369909/NEnaXFih3",
+    authorName: "周周让让tz",
+    authorAvatar: "🥣",
+    originalText: "然哥看！所以然哥你要不要翻我一下🫠",
+    translatedText: "익연이형 봐봐! 그래서 나한테 답글 좀 달아줄 거야 안 줄 거야🫠",
+    images: ["images/2401-dragon-puzzle.jpg"],
+    comments: [
+      {
+        id: 120701,
+        username: "周翊然Tz",
+        originalText: "同款小🐉",
+        translatedText: "똑같은 작은 🐉네",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1208,
+    date: "2024-01-31",
+    weiboUrl: "https://weibo.com/5238250823/NEncVFmCA",
+    authorName: "我真的是易燃妻",
+    authorAvatar: "🥣",
+    originalText: "然弟 我过年也不放假，搬砖",
+    translatedText: "익연 동생아 나 새해에도 휴가 없어, 일하는 중",
+    images: ["images/2401-doll.jpg"],
+    comments: [
+      {
+        id: 120801,
+        username: "周翊然Tz",
+        originalText: "我也是💪🏻新年快乐！",
+        translatedText: "나도 그래💪🏻새해 복 많이 받아!",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1209,
+    date: "2024-01-31",
+    weiboUrl: "https://weibo.com/7763695807/NEnfeeWSI",
+    authorName: "小猫说每天都会很开心o",
+    authorAvatar: "🥣",
+    originalText: "新年快乐 在过年之前拿到了驾照🚗 考试的分数也出啦 好像考的还不错捏 🎉在剧组搬砖的小然也要努力💪🏻一起加油",
+    translatedText: "새해 복 많이 받아 설 전에 운전면허증 땄어🚗 시험 점수도 나왔는데 꽤 잘 본 것 같아 🎉촬영장에서 일하는 익연이도 열심히💪🏻 같이 화이팅하자",
+    images: [],
+    comments: [
+      {
+        id: 120901,
+        username: "周翊然Tz",
+        originalText: "加油💪🏻新年快乐",
+        translatedText: "화이팅💪🏻새해 복 많이 받아",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1210,
+    date: "2024-01-31",
+    weiboUrl: "https://weibo.com/6903137085/NEngIybn1",
+    authorName: "是巴拉不是芭芭拉",
+    authorAvatar: "🥣",
+    originalText: "苦 太苦了 没有然哥翻牌的日子太苦了",
+    translatedText: "괴롭다 너무 괴로워 익연이형이 답글 안 달아주는 날들이 너무 괴로워",
+    images: ["images/2401-cold-kid.jpg"],
+    comments: [
+      {
+        id: 121001,
+        username: "周翊然Tz",
+        originalText: "周三快乐",
+        translatedText: "수요일도 즐겁게",
+        images: [],
+        actorReply: null
+      }
+    ]
+  }
+];
