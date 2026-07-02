ccb709fb1893d8bbb0b196155a213ba032df2ad2
zyr001122 <vazseyo@gmail.com>
2026-07-02 21:09:48 +0900

    Add files via upload

---
+++ data-2024-02.js
@@ -0,0 +1,144 @@
+const posts_202402 = [
+  {
+    id: 1301,
+    date: "2024-02-02",
+    weiboUrl: "https://weibo.com/5994946077/NEIFfles9",
+    originalText: "小年夜飞行✈️ #春节搭子##本周报备#",
+    translatedText: "설 전날 밤 비행✈️ #春节搭子##本周报备#",
+    images: [
+      "images/2402-flight1.jpg",
+      "images/2402-flight2.jpg",
+      "images/2402-flight3.jpg",
+      "images/2402-flight4.jpg"
+    ],
+    comments: []
+  },
+  {
+    id: 1302,
+    date: "2024-02-08",
+    weiboUrl: "https://weibo.com/5994946077/NFBinwIea",
+    originalText: "年夜饭吃什么🤔",
+    translatedText: "연야반 뭐 먹지🤔",
+    note: "연야반: 새해 전날 밤에 먹는 밥",
+    images: [],
+    comments: []
+  },
+  {
+    id: 1303,
+    date: "2024-02-09",
+    weiboUrl: "https://weibo.com/5994946077/NFKoYaXz8",
+    originalText: "#本周报备# 新春快乐🎉 祝大家万事兴\"龙\"",
+    translatedText: "#本周报备# 새해 복 많이 받아🎉 다들 만사형통하길",
+    note: "万事兴\"龙\": 만사형통(万事兴隆)에서 隆 대신 龙(용)으로 바꾼 말장난. 용의 해라서!",
+    images: [
+      "images/2402-newyear1.jpg",
+      "images/2402-newyear2.jpg",
+      "images/2402-newyear3.jpg",
+      "images/2402-newyear4.jpg",
+      "images/2402-newyear5.jpg",
+      "images/2402-newyear6.jpg",
+      "images/2402-newyear7.jpg",
+      "images/2402-newyear8.jpg",
+      "images/2402-newyear9.jpg"
+    ],
+    comments: []
+  },
+  {
+    id: 1304,
+    date: "2024-02-19",
+    weiboUrl: "https://weibo.com/5994946077/O1jUJ4o5U",
+    originalText: "一些开工记录 #本周报备#",
+    translatedText: "업무 복귀 기록 #本周报备#",
+    images: [
+      "images/2402-work1.jpg",
+      "images/2402-work2.jpg",
+      "images/2402-work3.jpg",
+      "images/2402-work4.jpg",
+      "images/2402-work5.jpg",
+      "images/2402-work6.jpg",
+      "images/2402-work7.jpg",
+      "images/2402-work8.jpg",
+      "images/2402-work9.jpg"
+    ],
+    comments: []
+  },
+  {
+    id: 1305,
+    date: "2024-02-22",
+    weiboUrl: "https://weibo.com/5994946077/O1LD32j9u",
+    originalText: "下雪了",
+    translatedText: "눈 왔어",
+    images: ["images/2402-snow.jpg"],
+    comments: [
+      {
+        id: 130501,
+        username: "超可爱的小猪宝",
+        originalText: "雪猪来咯",
+        translatedText: "눈 돼지 왔다",
+        images: ["images/2402-snow-pig.jpg"],
+        actorReply: { originalText: "堆雪猪？", translatedText: "눈 돼지 만든 거야?" }
+      },
+      {
+        id: 130502,
+        username: "树深时雾起i",
+        originalText: "你怎么站在房顶上",
+        translatedText: "왜 지붕 위에 서 있어",
+        images: [],
+        actorReply: { originalText: "看雪呢", translatedText: "눈 보는 중이지" }
+      },
+      {
+        id: 130503,
+        username: "半颗小番茄",
+        originalText: "给你看楼下不知名的小丑人😆",
+        translatedText: "아래층 이름 모를 광대 보여줄게😆",
+        images: ["images/2402-clown.jpg"],
+        actorReply: { originalText: "🤔挺man的", translatedText: "🤔꽤 남자다운데" }
+      },
+      {
+        id: 130504,
+        username: "-什么东西y",
+        originalText: "哈哈哈哈哈你看我的",
+        translatedText: "ㅋㅋㅋㅋㅋ내 거 봐봐",
+        images: ["images/2402-hair-acc.jpg"],
+        actorReply: { originalText: "头饰挺别致😏", translatedText: "머리 장식 꽤 독특한데😏" }
+      },
+      {
+        id: 130505,
+        username: "周翊然Tz",
+        originalText: "搬砖去了👋🏻",
+        translatedText: "일하러 갈게👋🏻",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1306,
+    date: "2024-02-28",
+    weiboUrl: "https://weibo.com/5994946077/O2GybmFjJ",
+    originalText: "有雪有雨也有晴 #本周报备##一条plog告别二月#",
+    translatedText: "눈도 오고 비도 오고 맑기도 하고 #本周报备##一条plog告别二月#",
+    images: [
+      "images/2402-plog1.jpg",
+      "images/2402-plog2.jpg",
+      "images/2402-plog3.jpg",
+      "images/2402-plog4.jpg",
+      "images/2402-plog5.jpg",
+      "images/2402-plog6.jpg",
+      "images/2402-plog7.jpg",
+      "images/2402-plog8.jpg",
+      "images/2402-plog9.jpg"
+    ],
+    comments: []
+  },
+  {
+    id: 1307,
+    date: "2024-02-29",
+    weiboUrl: "https://weibo.com/5994946077/O2SHS6k1G",
+    originalText: "最好的都给你-余佳运",
+    translatedText: "최고의 것은 모두 너에게 - 위자윈",
+    youtubeUrl: "https://youtu.be/O7Mzg4zpruA",
+    images: [],
+    comments: []
+  }
+];
