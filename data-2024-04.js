d042b2a7aecf9daccbef86f3aeb8c988b14675ad
zyr001122 <vazseyo@gmail.com>
2026-07-11 21:34:56 +0900

    Add files via upload

---
+++ data-2024-04.js
@@ -0,0 +1,232 @@
+const posts_202404 = [
+  {
+    id: 1501,
+    date: "2024-04-01",
+    weiboUrl: "https://weibo.com/5994946077/O7H8KCpnX",
+    originalText: "归。",
+    translatedText: "돌아왔다.",
+    images: [
+      "images/2404-apr1-1.jpg",
+      "images/2404-apr1-2.jpg",
+      "images/2404-apr1-3.jpg",
+      "images/2404-apr1-4.jpg",
+      "images/2404-apr1-5.jpg",
+      "images/2404-apr1-6.jpg"
+    ],
+    comments: []
+  },
+  {
+    id: 1502,
+    date: "2024-04-15",
+    weiboUrl: "https://weibo.com/5994946077/O9RFCF6qA",
+    originalText: "5️⃣0️⃣0️⃣🎞️",
+    translatedText: "5️⃣0️⃣0️⃣🎞️",
+    images: [
+      "images/2404-apr15-1.jpg",
+      "images/2404-apr15-2.jpg",
+      "images/2404-apr15-3.jpg"
+    ],
+    comments: []
+  },
+  {
+    id: 1503,
+    date: "2024-04-22",
+    weiboUrl: "https://weibo.com/7031139499/OaUyL4ES9",
+    authorName: "第十七秒日落_",
+    authorAvatar: "🥣",
+    originalText: "根据你的小红配色来的 酷不酷吧 你就说 ‼️⁉️‼️⁉️‼️",
+    translatedText: "너의 빨간 배색으로 만든 거야 멋있지 않아? 솔직히 말해봐 ‼️⁉️‼️⁉️‼️",
+    images: ["images/2404-nail.jpg"],
+    comments: [
+      {
+        id: 150301,
+        username: "周翊然Tz",
+        originalText: "超级帅 可以贴点小拉花上去",
+        translatedText: "완전 멋있다 라떼아트 스티커 붙여도 되겠다",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1504,
+    date: "2024-04-22",
+    weiboUrl: "https://weibo.com/6490558147/OaUz7AjiO",
+    authorName: "菜园不吃菜",
+    authorAvatar: "🥣",
+    originalText: "每次发微博你都会给我们分享你看到的风景 这次我也邀请你来看我这边的风景",
+    translatedText: "매번 웨이보 올릴 때마다 네가 본 풍경을 공유해줬잖아 이번엔 나도 내 쪽 풍경으로 초대할게",
+    images: ["images/2404-scenery.jpg"],
+    comments: [
+      {
+        id: 150401,
+        username: "周翊然Tz",
+        originalText: "3 4 我很喜欢 哈哈哈",
+        translatedText: "3번 4번 좋다 ㅋㅋㅋ",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1505,
+    date: "2024-04-22",
+    weiboUrl: "https://weibo.com/7297525643/OaUAk3hXD",
+    authorName: "永远明天见Tz",
+    authorAvatar: "🥣",
+    originalText: "小周老师 这么好看的风景你确定不看？",
+    translatedText: "주 선생님 이렇게 예쁜 풍경 안 볼 거예요?",
+    images: ["images/2404-sunset-cloud.jpg"],
+    comments: [
+      {
+        id: 150501,
+        username: "周翊然Tz",
+        originalText: "这是哪儿 火烧云 绝了",
+        translatedText: "이게 어딘데 노을 구름 미쳤다",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1506,
+    date: "2024-04-22",
+    weiboUrl: "https://weibo.com/5449587991/OaUAAeZJ1",
+    authorName: "是一一哦",
+    authorAvatar: "🥣",
+    originalText: "我觉得吧 也该看看我了",
+    translatedText: "나도 이제 좀 봐줄 때가 된 것 같은데",
+    images: ["images/2404-food2.jpg"],
+    comments: [
+      {
+        id: 150601,
+        username: "周翊然Tz",
+        originalText: "赶去吃饭的我 流口水了",
+        translatedText: "밥 먹으러 가던 중에 침 나왔어",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1507,
+    date: "2024-04-22",
+    weiboUrl: "https://weibo.com/6314250799/OaUB5iNkc",
+    authorName: "AD钙颖",
+    authorAvatar: "🥣",
+    originalText: "东北去都去了 铁锅炖吃了没？",
+    translatedText: "동북까지 갔으면 티에궈둔 먹었어?",
+    images: [],
+    comments: [
+      {
+        id: 150701,
+        username: "周翊然Tz",
+        originalText: "第一天就吃了啊",
+        translatedText: "첫날에 바로 먹었지",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1508,
+    date: "2024-04-22",
+    weiboUrl: "https://weibo.com/6148785485/OaUBYhAQt",
+    authorName: "北潮寒萧时有涵涵_Destin",
+    authorAvatar: "🥣",
+    originalText: "骑摩托好玩儿吗？",
+    translatedText: "오토바이 타는 거 재밌어?",
+    images: ["images/2404-motorcycle.jpg"],
+    comments: [
+      {
+        id: 150801,
+        username: "周翊然Tz",
+        originalText: "哈哈哈 很稳",
+        translatedText: "ㅋㅋㅋ 안정적이던데",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1509,
+    date: "2024-04-22",
+    weiboUrl: "https://weibo.com/7713985841/OaUCwpjfC",
+    authorName: "9九九久9",
+    authorAvatar: "🥣",
+    originalText: "然宝感觉东北怎么样",
+    translatedText: "익연아 동북 어때?",
+    images: [],
+    comments: [
+      {
+        id: 150901,
+        username: "周翊然Tz",
+        originalText: "挺好的啊 就是早晚温差有点大 我的军大衣没带😢",
+        translatedText: "꽤 좋아 근데 일교차가 좀 크네 군용 코트를 안 가져왔어😢",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1510,
+    date: "2024-04-22",
+    weiboUrl: "https://weibo.com/5994946077/OaUDL7mcS",
+    originalText: "去吃饭了 我走了🙇🏻",
+    translatedText: "밥 먹으러 가야 해 간다🙇🏻",
+    images: ["images/2404-apr22-end.jpg"],
+    comments: []
+  },
+  {
+    id: 1511,
+    date: "2024-04-25",
+    weiboUrl: "https://weibo.com/5994946077/ObldydLSv",
+    originalText: "东北 来了 吃了 玩了 跑了🚶🏻‍♂‍➡️",
+    translatedText: "동북 왔다 먹었다 놀았다 떠났다🚶🏻‍♂‍➡️",
+    images: [
+      "images/2404-apr25-1.jpg",
+      "images/2404-apr25-2.jpg",
+      "images/2404-apr25-3.jpg",
+      "images/2404-apr25-4.jpg",
+      "images/2404-apr25-5.jpg",
+      "images/2404-apr25-6.jpg",
+      "images/2404-apr25-7.jpg",
+      "images/2404-apr25-8.jpg",
+      "images/2404-apr25-9.jpg"
+    ],
+    comments: [
+      {
+        id: 151101,
+        username: "Rabbituui",
+        originalText: "东北什么最好吃！！！",
+        translatedText: "동북에서 뭐가 제일 맛있어!!!",
+        images: [],
+        actorReply: { originalText: "铁锅炖大鹅！", translatedText: "티에궈둔 다어(솥뚜껑 거위찜)!" }
+      },
+      {
+        id: 151102,
+        username: "芋泥没烦恼",
+        originalText: "你怎么吃两个冰淇淋",
+        translatedText: "왜 아이스크림을 두 개 먹어",
+        images: [],
+        actorReply: { originalText: "一个用来吃 一个用来拍照", translatedText: "하나는 먹으려고 하나는 사진 찍으려고" }
+      },
+      {
+        id: 151103,
+        username: "要睡个好觉觉-",
+        originalText: "这个冰激凌是什么建筑嘛",
+        translatedText: "이 아이스크림 무슨 건물이야",
+        images: [],
+        actorReply: { originalText: "索菲亚大教堂", translatedText: "소피아 성당" }
+      },
+      {
+        id: 151104,
+        username: "爆冷萌厨_",
+        originalText: "小伙子 啥时候再去吃铁锅炖啊",
+        translatedText: "젊은 친구 티에궈둔 언제 또 먹으러 가?",
+        images: [],
+        actorReply: { originalText: "下次来的时候？？", translatedText: "다음에 올 때??" }
+      }
+    ]
+  }
+];
