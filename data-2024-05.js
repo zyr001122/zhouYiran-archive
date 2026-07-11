12e7fa37a9cd716953a98c24b5ab045e6f57fc23
zyr001122 <vazseyo@gmail.com>
2026-07-11 21:53:30 +0900

    Add files via upload

---
+++ data-2024-05.js
@@ -0,0 +1,251 @@
+const posts_202405 = [
+  {
+    id: 1601,
+    date: "2024-05-08",
+    weiboUrl: "https://weibo.com/5994946077/OdlcJigtV",
+    originalText: "周三 3张🤳#本周报备#",
+    translatedText: "수요일 셀카 3장🤳#本周报备#",
+    images: [
+      "images/2405-may8-1.jpg",
+      "images/2405-may8-2.jpg",
+      "images/2405-may8-3.jpg",
+      "images/2405-may8-4.jpg"
+    ],
+    comments: []
+  },
+  {
+    id: 1602,
+    date: "2024-05-20",
+    weiboUrl: "https://weibo.com/5994946077/Of3NfttS1",
+    originalText: "#本周报备# 分享西瓜。",
+    translatedText: "#本周报备# 수박 공유.",
+    images: [
+      "images/2405-watermelon1.jpg",
+      "images/2405-watermelon2.jpg",
+      "images/2405-watermelon3.jpg",
+      "images/2405-watermelon4.jpg",
+      "images/2405-watermelon5.jpg",
+      "images/2405-watermelon6.jpg",
+      "images/2405-watermelon7.jpg",
+      "images/2405-watermelon8.jpg",
+      "images/2405-watermelon9.jpg"
+    ],
+    comments: []
+  },
+  {
+    id: 1603,
+    date: "2024-05-22",
+    weiboUrl: "https://weibo.com/5994946077/OfrDKxifr",
+    originalText: "hi🌧️",
+    translatedText: "hi🌧️",
+    images: [],
+    comments: []
+  },
+  {
+    id: 1604,
+    date: "2024-05-22",
+    weiboUrl: "https://weibo.com/6457466863/OfrEsA4ES",
+    authorName: "z吃口甜粥z",
+    authorAvatar: "🥣",
+    originalText: "梅州是不是最近都在下雨，那我就给你分享一个我们这的落日吧～",
+    translatedText: "메이저우는 요즘 계속 비 오지? 그럼 우리 쪽 노을 공유해줄게～",
+    images: ["images/2405-sunset.jpg"],
+    comments: [
+      {
+        id: 160401,
+        username: "周翊然Tz",
+        originalText: "一直在下",
+        translatedText: "계속 오고 있어",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1605,
+    date: "2024-05-22",
+    weiboUrl: "https://weibo.com/7360229323/OfrEttiwH",
+    authorName: "地yu吹雪",
+    authorAvatar: "🥣",
+    originalText: "宝宝看看我在北京拍的胶卷！",
+    translatedText: "자기야 내가 베이징에서 찍은 필름 봐봐!",
+    images: ["images/2405-film2.jpg"],
+    comments: [
+      {
+        id: 160501,
+        username: "周翊然Tz",
+        originalText: "很不错👍",
+        translatedText: "꽤 좋은데👍",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1606,
+    date: "2024-05-22",
+    weiboUrl: "https://weibo.com/7884753848/OfrGAE1JM",
+    authorName: "tz_yamii",
+    authorAvatar: "🥣",
+    originalText: "嘿嘿，然然快看我的丑狗🐶",
+    translatedText: "히히, 익연아 내 못생긴 강아지 빨리 봐봐🐶",
+    images: ["images/2405-ugly-dog.jpg"],
+    comments: [
+      {
+        id: 160601,
+        username: "周翊然Tz",
+        originalText: "哈哈哈哈哈好玩",
+        translatedText: "ㅋㅋㅋㅋㅋ웃기네",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1607,
+    date: "2024-05-22",
+    weiboUrl: "https://weibo.com/6587161907/OfrGzvnNw",
+    authorName: "能不能天天吃火锅-",
+    authorAvatar: "🥣",
+    originalText: "日落合集 你有拍日落吗🌄",
+    translatedText: "노을 모음집 너도 노을 찍어?🌄",
+    images: ["images/2405-sunset2.jpg"],
+    comments: [
+      {
+        id: 160701,
+        username: "周翊然Tz",
+        originalText: "每天下雨 没有日落",
+        translatedText: "매일 비 와서 노을이 없어",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1608,
+    date: "2024-05-22",
+    weiboUrl: "https://weibo.com/1783721615/OfrGvEPNG",
+    authorName: "月亮味的葡萄",
+    authorAvatar: "🥣",
+    originalText: "你看假面骑士在干嘛",
+    translatedText: "가면라이더 뭐 하는지 봐봐",
+    images: ["images/2405-kamen2.jpg"],
+    comments: [
+      {
+        id: 160801,
+        username: "周翊然Tz",
+        originalText: "p的挺好",
+        translatedText: "포토샵 꽤 잘했네",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1609,
+    date: "2024-05-22",
+    weiboUrl: "https://weibo.com/5994946077/OfrKbxHA1",
+    originalText: "接着开工了",
+    translatedText: "다시 일 시작할게",
+    images: ["images/2405-may22-end.jpg"],
+    comments: []
+  },
+  {
+    id: 1610,
+    date: "2024-05-25",
+    weiboUrl: "https://weibo.com/5994946077/OfUv18Dlb",
+    originalText: "等🌞#本周报备#",
+    translatedText: "기다리는 중🌞#本周报备#",
+    images: [
+      "images/2405-may25-1.jpg",
+      "images/2405-may25-2.jpg",
+      "images/2405-may25-3.jpg",
+      "images/2405-may25-4.jpg",
+      "images/2405-may25-5.jpg"
+    ],
+    comments: [
+      {
+        id: 161001,
+        username: "方什么美女",
+        originalText: "我都不敢想象周翊然回复我之后 我会变得多么活泼，开朗，乐观，积极，善良，可爱，聪明伶俐，善解人意",
+        translatedText: "주익연이 나한테 답글 달아주면 내가 얼마나 활발해지고 명랑해지고 낙관적이고 적극적이고 착하고 귀엽고 똑똑하고 배려심 있어질지 상상도 못 하겠어",
+        images: [],
+        actorReply: { originalText: "哈哈😏", translatedText: "ㅋㅋ😏" }
+      },
+      {
+        id: 161002,
+        username: "仙女味的饭团团",
+        originalText: "您的粉丝已过期 将被清理 请翻牌激活",
+        translatedText: "당신의 팬 유효기간이 만료되었습니다 곧 삭제될 예정이니 답글로 활성화해주세요",
+        images: [],
+        actorReply: { originalText: "👽👽", translatedText: "👽👽" }
+      },
+      {
+        id: 161003,
+        username: "西红柿年糕Zzz",
+        originalText: "宝宝在看太阳吗🥰",
+        translatedText: "자기야 태양 보는 중이야?🥰",
+        images: [],
+        actorReply: { originalText: "在等🌞", translatedText: "기다리는 중🌞" }
+      },
+      {
+        id: 161004,
+        username: "樱桃菲士",
+        originalText: "这位乘客，这样探出窗外的行为很危险噢",
+        translatedText: "이 승객분, 이렇게 창문 밖으로 내미는 행동은 매우 위험해요",
+        images: [],
+        actorReply: { originalText: "停车拍的👌🏻", translatedText: "정차 중에 찍은 거야👌🏻" }
+      },
+      {
+        id: 161005,
+        username: "不会说话的蛋挞_i",
+        originalText: "刚刚不小心被蜜蜂蛰了...半边脸都肿了还有点发紫...查了一下到底是什么蜂...才发现原来是...看见你的美貌甘拜下蜂...💗💗💗",
+        translatedText: "방금 실수로 벌에 쏘였어... 얼굴 반쪽이 다 부어서 좀 보라색이 됐어... 도대체 무슨 벌인지 찾아봤더니... 알고 보니... 네 미모에 감탄한 벌이었어(甘拜下蜂/甘拜下风 말장난)...💗💗💗",
+        images: [],
+        actorReply: { originalText: "哈哈哈哈哈哈", translatedText: "ㅋㅋㅋㅋㅋㅋ" }
+      },
+      {
+        id: 161006,
+        username: "xixinzz",
+        originalText: "还有13天高考😭",
+        translatedText: "수능까지 13일 남았어😭",
+        images: [],
+        actorReply: { originalText: "高考都加油", translatedText: "수능 보는 사람들 다 화이팅" }
+      },
+      {
+        id: 161007,
+        username: "要睡个好觉觉-",
+        originalText: "小然注意一下你家的电视，我最近和贞子学了几招😋",
+        translatedText: "익연아 집 TV 좀 조심해, 나 요즘 사다코한테 몇 가지 배웠거든😋",
+        images: [],
+        actorReply: { originalText: "😢😢", translatedText: "😢😢" }
+      },
+      {
+        id: 161008,
+        username: "定律小狗_",
+        originalText: "╭◜◝ ͡ ◜◝╮ ( ˃̶͈◡˂̶͈ ) hi! 我是一朵云✨ ╰◟◞ ͜ ◟◞╯ ╭◜◝ ͡◜◝╮ (˃̶͈◡˂̶͈)我想飘到你心里💖 ╰◟◞ ͜◟◞╯ ╭◜◝ ͡ ◜◝╮ ( ˃̶͈◡˂̶͈ ) 顺便~ ╰◟◞ ͜ ◟◞╯ ╭◜◝ ͡◜◝╮ (˃̶͈◡˂̶͈)康康我的评论叭🥰 ╰◟◞ ͜◟◞╯",
+        translatedText: "╭◜◝ ͡ ◜◝╮ ( ˃̶͈◡˂̶͈ ) hi! 나는 구름이야✨ ╰◟◞ ͜ ◟◞╯ ╭◜◝ ͡◜◝╮ (˃̶͈◡˂̶͈) 네 마음속으로 흘러가고 싶어💖 ╰◟◞ ͜◟◞╯ ╭◜◝ ͡ ◜◝╮ ( ˃̶͈◡˂̶͈ ) 그리고~ ╰◟◞ ͜ ◟◞╯ ╭◜◝ ͡◜◝╮ (˃̶͈◡˂̶͈) 내 댓글 봐줘🥰 ╰◟◞ ͜◟◞╯",
+        images: [],
+        actorReply: { originalText: "🌧️🌧️", translatedText: "🌧️🌧️" }
+      }
+    ]
+  },
+  {
+    id: 1611,
+    date: "2024-05-30",
+    weiboUrl: "https://weibo.com/5994946077/OgIqWz5gx",
+    originalText: "今日晚餐🈶",
+    translatedText: "오늘 저녁🈶",
+    images: ["images/2405-dinner1.jpg"],
+    comments: []
+  },
+  {
+    id: 1612,
+    date: "2024-05-31",
+    weiboUrl: "https://weibo.com/5994946077/OgQzGpSfL",
+    originalText: "晚餐✅ #本周报备#",
+    translatedText: "저녁✅ #本周报备#",
+    images: ["images/2405-dinner2.jpg"],
+    comments: []
+  }
+];
