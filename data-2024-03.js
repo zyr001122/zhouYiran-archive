90b733efb2486d531e2dcb9e0fc1b5c7a0be1b5f
zyr001122 <vazseyo@gmail.com>
2026-07-11 21:29:42 +0900

    Update data-2024-03.js

--- data-2024-03.js
+++ data-2024-03.js
@@ -51,7 +51,7 @@ const posts_202403 = [
     date: "2024-03-22",
     weiboUrl: "https://weibo.com/5994946077/O6cW29av6",
     originalText: "来了👽",
-    translatedText: "왔어👽",
+    translatedText: "나 왔어👽",
     images: [],
     comments: []
   },
@@ -62,14 +62,14 @@ const posts_202403 = [
     authorName: "贝贝爱喝白开水z_",
     authorAvatar: "🥣",
     originalText: "昨天报名了英语四级考试 小然可不可以祝我一次过🥹🥹🥹",
-    translatedText: "어제 영어 4급 시험 신청했어 익연아 한 번에 붙게 빌어줄 수 있어?🥹🥹🥹",
+    translatedText: "어제 영어 4급 시험 신청했거든 익연이가 나 한 번에 합격할 수 있게 응원해 줄 수 있어?🥹🥹🥹",
     images: ["images/2403-fan1.jpg", "images/2403-fan2.jpg"],
     comments: [
       {
         id: 140301,
         username: "周翊然Tz",
         originalText: "加油 时王保佑你😏",
-        translatedText: "화이팅 시왕이 지켜줄 거야😏",
+        translatedText: "화이팅 지오(가면라이더)가 지켜줄 거야😏",
         images: [],
         actorReply: null
       }
@@ -82,14 +82,14 @@ const posts_202403 = [
     authorName: "is木头1019",
     authorAvatar: "🥣",
     originalText: "希望周翊然每天都要开开心心！我也会好好学习，每天都认真生活，努力去见你的！很期待你今后的作品与活动！最后一点点小愿望周翊然能祝我18岁成年快乐！",
-    translatedText: "주익연이 매일 행복하길 바라! 나도 열심히 공부하고 매일 성실하게 살아서 꼭 만나러 갈게! 앞으로의 작품이랑 활동 엄청 기대돼! 마지막으로 작은 소원 하나, 주익연이 내 18살 성인 생일 축하해줬으면!",
+    translatedText: "익연이가 매일매일 행복하길 ! 나도 열심히 공부하고, 매일 성실하게 살면서 널 만나러 가기 위해 노력할게! 앞으로의 작품이랑 활동도 기대할게! 마지막으로 작은 소원이 있다면 익연이가 내 18살 성인이 된 첫 생일을 축하해 줬으면!",
     images: ["images/2403-dog.jpg"],
     comments: [
       {
         id: 140401,
         username: "周翊然Tz",
         originalText: "成年快乐 小狗超可爱😏",
-        translatedText: "성인 축하해 강아지 너무 귀엽다😏",
+        translatedText: "성인 된 거 축하해 강아지 너무 귀엽다😏",
         images: [],
         actorReply: null
       }
@@ -102,7 +102,7 @@ const posts_202403 = [
     authorName: "小狗吐司tusi",
     authorAvatar: "🥣",
     originalText: "嘿，bro 理理我呢",
-    translatedText: "야, 브로 나 좀 봐줘",
+    translatedText: "헤이, 브로 나 좀 봐줘",
     images: ["images/2403-bro.jpg"],
     comments: [
       {
@@ -122,14 +122,14 @@ const posts_202403 = [
     authorName: "1ike吃水果的维C",
     authorAvatar: "🥣",
     originalText: "我只是一个路过的假面骑士 ， 我没有梦想 但是我可以守护别人的梦想！帅不？帝骑",
-    translatedText: "저는 그냥 지나가는 가면라이더예요, 꿈은 없지만 다른 사람의 꿈을 지킬 수 있어요! 멋지지 않아? 제로원",
+    translatedText: "나는 그저 지나가던 가면라이더다. 나에게 꿈은 없지만, 다른 사람의 꿈을 지켜줄 수는 있지! 멋지지 않아? 디케이드",
     images: ["images/2403-kamen.jpg"],
     comments: [
       {
         id: 140601,
         username: "周翊然Tz",
         originalText: "可以可以 台词都来了是吧",
-        translatedText: "오케이 오케이 대사까지 준비했네",
+        translatedText: "ㅇㅋㅇㅋ 명대사까지 준비했네",
         images: [],
         actorReply: null
       }
@@ -142,14 +142,14 @@ const posts_202403 = [
     authorName: "-垂耳一只兔_",
     authorAvatar: "🥣",
     originalText: "看看？",
-    translatedText: "봐봐?",
+    translatedText: "봤어?",
     images: ["images/2403-car.jpg"],
     comments: [
       {
         id: 140701,
         username: "周翊然Tz",
         originalText: "911turbos 不错不错",
-        translatedText: "911터보S 좋은데 좋아",
+        translatedText: "911 turbo s 아주 좋지 좋아",
         images: [],
         actorReply: null
       }
@@ -162,14 +162,14 @@ const posts_202403 = [
     authorName: "螺蛳粉不加豆角",
     authorAvatar: "🥣",
     originalText: "然哥 今天我请假出来吃了麻辣烫 你晚餐吃了啥",
-    translatedText: "익연이형 오늘 조퇴하고 마라탕 먹었어 저녁 뭐 먹었어?",
+    translatedText: "익연 오빠 나 오늘 조퇴하고 마라탕 먹었어 오빠는 저녁 뭐 먹었어?",
     images: ["images/2403-malatang.jpg"],
     comments: [
       {
         id: 140801,
         username: "周翊然Tz",
         originalText: "吃了水煮玉米",
-        translatedText: "삶은 옥수수 먹었어",
+        translatedText: "옥수수 삶은 거 먹었어",
         images: [],
         actorReply: null
       }
@@ -182,14 +182,14 @@ const posts_202403 = [
     authorName: "Y-Congee",
     authorAvatar: "🥣",
     originalText: "看我嘟小狗然然",
-    translatedText: "내 통통한 강아지 익연이 봐봐",
+    translatedText: "혀 내밀고 있는 우리 댕댕이 익연이도 봐봐",
     images: ["images/2403-chubby-dog.jpg"],
     comments: [
       {
         id: 140901,
         username: "周翊然Tz",
         originalText: "自带腮红😏",
-        translatedText: "자체 볼터치 장착😏",
+        translatedText: "볼터치도 했네😏",
         images: [],
         actorReply: null
       }
@@ -202,14 +202,14 @@ const posts_202403 = [
     authorName: "Yun_kookie",
     authorAvatar: "🥣",
     originalText: "嗨小然～最近的生活有你在变得更丰富之外我还有一个新的兴趣 就是拍底片啦🎞️很喜欢底片拍出来的感觉，有种复古感，跟把当时的回忆以特别的方式保存起来。在等待底片洗出来的过程都不知道成果会是什么，就会很期待很兴奋！等到洗出来就会有非常多惊喜，带我回到当下😋 与你分享我的底片相机看到的世界✨ 我们下个月22号见，努力一个月拍完一卷",
-    translatedText: "안녕 익연아~ 요즘 네가 있어서 삶이 더 풍요로워진 것 외에 새로운 취미가 생겼어 바로 필름 사진 찍기야🎞️ 필름으로 찍은 느낌이 너무 좋아, 복고 감성이 있고 그 순간의 기억을 특별한 방식으로 남기는 것 같아서. 필름 현상 기다리는 동안 결과가 어떻게 나올지 몰라서 엄청 설레고 기대돼! 현상되면 깜짝 놀랄 게 많아서 그 순간으로 돌아가게 해줘😋 필름 카메라로 담은 세상을 너와 공유할게✨ 다음 달 22일에 보자, 한 달 동안 열심히 한 롤 다 찍을게",
+    translatedText: "하이 익연아~ 요즘 네 덕분에 일상이 풍요로워진 것 외에도 새로운 취미가 하나 더 생겼어 바로 필름 카메라 사진 찍기야 🎞️ 필름 사진이 주는 특유의 레트로한 감성과, 그 시절의 추억을 특별한 방식으로 기록해 두는 느낌이 정말 좋더라고. 필름을 인화하기 전까지는 어떤 결과물이 나올지 모르니까 기다리는 내내 엄청 설레고 기대돼! 인화된 사진을 받으면 서프라이즈 선물을 받은 것처럼 그 순간의 기억으로 나를 데려다주거든 😋 필름 카메라로 바라본 내 세상을 너와 공유할게 ✨ 우리 다음 달 22일에 만나자, 한 달 동안 열심히 찍어서 한 롤 다 채워볼게",
     images: ["images/2403-film.jpg"],
     comments: [
       {
         id: 141001,
         username: "周翊然Tz",
         originalText: "超好看",
-        translatedText: "너무 예쁘다",
+        translatedText: "진짜 예쁘다",
         images: [],
         actorReply: null
       }
@@ -222,14 +222,14 @@ const posts_202403 = [
     authorName: "一只静音",
     authorAvatar: "🥣",
     originalText: "然哥你此刻在干嘛？",
-    translatedText: "익연이형 지금 뭐 해?",
+    translatedText: "익연 오빠 지금 뭐 해?",
     images: [],
     comments: [
       {
         id: 141101,
         username: "周翊然Tz",
         originalText: "坐车😏",
-        translatedText: "차 타는 중😏",
+        translatedText: "차 타고 이동 중😏",
         images: [],
         actorReply: null
       }
@@ -242,14 +242,14 @@ const posts_202403 = [
     authorName: "假意或真心-",
     authorAvatar: "🥣",
     originalText: "小然你看！三月的日出🌅和樱花🌸，还有好好在心理咨询行业深耕学习的我🙋🏻‍♀️",
-    translatedText: "익연아 봐봐! 3월의 일출🌅이랑 벚꽃🌸, 그리고 심리상담 분야에서 열심히 공부하는 나🙋🏻‍♀️",
+    translatedText: "익연아 봐봐! 3월의 일출🌅이랑 벚꽃🌸, 그리고 심리상담 업계에서 열심히 노력하는 나🙋🏻‍♀️",
     images: ["images/2403-cherry.jpg"],
     comments: [
       {
         id: 141201,
         username: "周翊然Tz",
         originalText: "樱花超美",
-        translatedText: "벚꽃 너무 예쁘다",
+        translatedText: "벚꽃 엄청 예쁘다",
         images: [],
         actorReply: null
       }
@@ -262,7 +262,7 @@ const posts_202403 = [
     authorName: "轮到这季幸福了Tz",
     authorAvatar: "🥣",
     originalText: "我们这边感觉最近天气无常，你那边呢？要照顾好自己别感冒啦💝",
-    translatedText: "우리 쪽은 요즘 날씨가 너무 변덕스러워, 거긴 어때? 감기 걸리지 않게 잘 챙겨💝",
+    translatedText: "우리 동네는 요즘 날씨가 엄청 변덕스러운데, 네가 있는 곳은 어때? 감기 걸리지 않게 몸조리 잘해💝",
     images: ["images/2403-weather.jpg"],
     comments: [
       {
@@ -282,14 +282,14 @@ const posts_202403 = [
     authorName: "吃好睡好天下第一好",
     authorAvatar: "🥣",
     originalText: "小周小周快回我 我要当全世界最开朗的小女孩",
-    translatedText: "익연아 익연아 빨리 답해줘 나 세상에서 제일 밝은 여자애가 될 거야",
+    translatedText: "익연 익연 빨리 답장해줘 나 세상에서 제일 밝고 씩씩한 소녀가 되고 싶단 말이야",
     images: ["images/2403-food1.jpg"],
     comments: [
       {
         id: 141401,
         username: "周翊然Tz",
         originalText: "你吃的啥",
-        translatedText: "뭐 먹은 거야",
+        translatedText: "뭐 먹고 있는 거야?",
         images: [],
         actorReply: null
       }
@@ -302,7 +302,7 @@ const posts_202403 = [
     authorName: "ehhee_tz",
     authorAvatar: "🥣",
     originalText: "今天带着小然去吃了好吃的",
-    translatedText: "오늘 익연이 데리고 맛있는 거 먹으러 갔어",
+    translatedText: "오늘 익연이 데리고 맛있는 거 먹으러 갔다 왔어",
     images: ["images/2403-fried-rice.jpg"],
     comments: [
       {
@@ -322,7 +322,7 @@ const posts_202403 = [
     authorName: "我不吃饭abb",
     authorAvatar: "🥣",
     originalText: "啊啊啊啊啊老公你看这个狗的尾巴酷不酷",
-    translatedText: "아아아아아 자기야 이 강아지 꼬리 멋있지 않아?",
+    translatedText: "아ㅏㅏㅏㅏ 자기야 강아지 꼬리 멋있지 않아?",
     images: ["images/2403-dog-tail.jpg"],
     comments: [
       {
@@ -341,15 +341,15 @@ const posts_202403 = [
     weiboUrl: "https://weibo.com/7558984077/O6d07fyjF",
     authorName: "珠珠堡堡z",
     authorAvatar: "🥣",
-    originalText: "Hello 然然！最近真的好累好焦虑啊 학업的原因 还有对未来的一切都很害怕 嘿嘿嘿 好在我是一个乐观的女孩子！嗯..其中一大部分的乐观是来源于你——然然！在《斗罗大陆2》和《当我飞奔向你》两部剧的紧张拍摄和宣传中 工作占据了你全部的时间 每个时间点都卡得刚刚好 这些工作让你的身体超负荷 经常吃不下饭 但你从没说过一句累和苦。遇到问题 更多的是自我消化 很少向别人提起心里发生的事 不给身边的人增加烦恼和负担。之前你用成熟、稳重、慢热来形容自己 后面你想把成熟换成幼稚 要做一个快乐的小孩 天天开心！所以说 怎么会不苦呢 我的小孩只是不说而已你的火不是偶然 是一步一个脚印踏出来的！你心里的这股劲儿真的太吸引人了！你那么好那么努力 所以我更要努力 然后来见你！我要你扶摇直上 未来的路 一定会一路顺风 事业蒸蒸日上。给然然看下我桌面的小熊 我朋友说丑萌丑萌的 哈哈哈哈哈哈哈 然后最近的日常也就是学习看书📖这些啦 还有收藏然然的照片！最后我的大明星周翊然 天天开心！你往前走 我们一直在！我一定会努力来见你的！",
-    translatedText: "안녕 익연아! 요즘 진짜 너무 지치고 불안해 학업 때문에, 그리고 미래가 너무 무서워 히히히 그래도 다행히 나는 긍정적인 여자애야! 음.. 그 긍정의 많은 부분이 너한테서 와 — 익연아! 두라기2랑 널 향해 달려간다 두 드라마 촬영과 홍보로 바쁜 와중에 일이 네 시간을 전부 차지했는데 타이밍마다 딱딱 맞아떨어졌어. 그 일들이 네 몸을 혹사시키고 밥도 자주 못 먹었는데 한 번도 힘들다 괴롭다는 말을 안 했어. 문제가 생기면 혼자 삭이고 마음속에 있는 일을 다른 사람한테 거의 얘기 안 해서 주변 사람들한테 걱정 부담 안 주려고 하잖아. 예전에 성숙하고 듬직하고 낯가린다고 자신을 표현했는데 나중에 성숙을 유치함으로 바꾸고 싶다고 했잖아 행복한 아이로 매일 즐겁게! 그러니까 어떻게 안 힘들겠어 내 아이는 그냥 말을 안 할 뿐이지 네 인기는 우연이 아니야 한 걸음 한 걸음 밟아온 거야! 네 마음속 그 열정이 진짜 너무 매력적이야! 네가 그렇게 좋고 그렇게 열심히 하니까 나도 더 열심히 해서 만나러 갈게! 네가 승승장구하길 바라 앞으로의 길이 순탄하고 일도 날로 번창하길. 익연이한테 내 바탕화면 곰돌이 보여줄게 친구가 못생기게 귀엽다고 했어 ㅋㅋㅋㅋㅋㅋ 요즘 일상은 공부하고 책 읽기📖 그리고 익연이 사진 모으기! 마지막으로 내 대스타 주익연 매일 행복해! 네가 앞으로 나아가면 우리는 항상 여기 있어! 꼭 열심히 해서 만나러 갈게!",
+    originalText: "Hello 然然！最近真的好累好焦虑啊 学业的原因 还有对未来的一切都很害怕 嘿嘿嘿 好在我是一个乐观的女孩子！嗯..其中一大部分的乐观是来源于你——然然！在《斗罗大陆2》和《当我飞奔向你》两部剧的紧张拍摄和宣传中 工作占据了你全部的时间 每个时间点都卡得刚刚好 这些工作让你的身体超负荷 经常吃不下饭 但你从没说过一句累和苦。遇到问题 更多的是自我消化 很少向别人提起心里发生的事 不给身边的人增加烦恼和负担。之前你用成熟、稳重、慢热来形容自己 后面你想把成熟换成幼稚 要做一个快乐的小孩 天天开心！所以说 怎么会不苦呢 我的小孩只是不说而已你的火不是偶然 是一步一个脚印踏出来的！你心里的这股劲儿真的太吸引人了！你那么好那么努力 所以我更要努力 然后来见你！我要你扶摇直上 未来的路 一定会一路顺风 事业蒸蒸日上。给然然看下我桌面的小熊 我朋友说丑萌丑萌的 哈哈哈哈哈哈哈 然后最近的日常也就是学习看书📖这些啦 还有收藏然然的照片！最后我的大明星周翊然 天天开心！你往前走 我们一直在！我一定会努力来见你的！",
+    translatedText: "안녕 익연아! 요즘 학업 문제 때문에 정말 너무 지치고 불안해. 미래에 마주할 모든 것들이 두렵기도 하고... 헤헤, 그래도 다행히 난 긍정적인 편이야! 음.. 사실 내 긍정적인 에너지의 큰 부분은 너한테서 나와, 익연아! 드라마 《투라대륙2》와 《당아비분향니》 두 작품의 빡빡한 촬영과 홍보 스케줄 속에서 일이 네 시간의 전부를 차지했을 때, 모든 타임라인이 딱 맞아떨어지느라 네 몸은 이미 과부하 상태였잖아. 밥도 제대로 못 먹으면서 힘들다거나 괴롭다는 말 한마디 안 하더라. 문제가 생기면 다른 사람한테 털어놓아 짐을 얹어주기보단, 혼자 속으로 삼키고 소화해 내는 편이잖아. 예전엔 스스로를 성숙하고, 묵직하고, 낯을 가린다고 표현하더니 나중에는 '성숙'이라는 단어를 '유치함'으로 바꾸고 싶다고, 그저 매일매일 행복한 어린아이가 되고 싶다고 했지. 그러니 삶이 어떻게 안 힘들었겠어. 내 가수가 그저 힘들다고 말을 안 할 뿐이지... 네가 이렇게 큰 사랑을 받는 건 절대 우연이 아니라 한 걸음 한 걸음 성실하게 발을 내디뎌온 결과야! 마음속에 품은 그 뚝심과 열정이 사람을 참 끌리게 만들어. 네가 그렇게 멋지고 열심히 하니까 나도 더 자극받아서 열심히 노력해서 널 보러 갈 거야! 네가 거침없이 높이 올라가기를, 네 앞날이 늘 순탄하고 사업도 날로 번창하기를 바랄게. 내 책상 위에 있는 곰돌이 인형 사진 보여줄게. 친구들이 못생겼는데 귀엽대 ㅋㅋㅋㅋㅋㅋㅋ 그리고 요즘 내 일상은 책 읽고 공부하기📖 그리고 익연이 사진 수집하기야! 마지막으로 나의 대스타 주익연, 매일 행복해야 해! 네가 앞으로 나아갈 때 우린 항상 그 자리에 있을게! 꼭 열심히 노력해서 만나러 갈게!",
     images: ["images/2403-bear.jpg"],
     comments: [
       {
         id: 141701,
         username: "周翊然Tz",
         originalText: "谢谢你！ 不用过分焦虑 好事儿都会来的 剩下的只要自己努力 问心无愧就好😘",
-        translatedText: "고마워! 너무 불안해하지 마 좋은 일은 다 올 거야 나머지는 그냥 스스로 열심히 하고 양심에 부끄럽지 않으면 돼😘",
+        translatedText: "고마워! 너무 불안해하지 마 좋은 일들은 다 찾아오기 마련이니까 나머지는 그냥 스스로 열심히 하고 양심에 부끄럽지만 않으면 돼😘",
         images: [],
         actorReply: null
       }
@@ -360,7 +360,7 @@ const posts_202403 = [
     date: "2024-03-22",
     weiboUrl: "https://weibo.com/5994946077/O6cW29av6",
     originalText: "大家都幸福地度过余下的三月吧 天天开心 赶飞机去了 下次见👋🏻",
-    translatedText: "다들 남은 3월도 행복하게 보내 매일 즐겁게 비행기 타러 가야 해 다음에 봐👋🏻",
+    translatedText: "다들 남은 3월도 행복하게 매일 즐겁게 보내 난 비행기 타러 가야 해 다음에 봐👋🏻",
     images: [],
     comments: []
   },
@@ -369,7 +369,7 @@ const posts_202403 = [
     date: "2024-03-28",
     weiboUrl: "https://weibo.com/5994946077/O78LhdLrG",
     originalText: "吃了火锅看了海",
-    translatedText: "훠궈 먹고 바다 봤어",
+    translatedText: "훠궈 먹고 바다 구경",
     images: [
       "images/2403-mar28-1.jpg",
       "images/2403-mar28-2.jpg",
