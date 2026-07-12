60be50f6d6feaf0960779ee3fa42c09dda355a0e
zyr001122 <vazseyo@gmail.com>
2026-07-12 13:51:46 +0900

    Add files via upload

---
+++ data-2024-07.js
@@ -0,0 +1,447 @@
+const posts_202407 = [
+  {
+    id: 1801,
+    date: "2024-07-22",
+    weiboUrl: "",
+    originalText: "下班了！",
+    translatedText: "퇴근했어!",
+    images: [],
+    comments: []
+  },
+  {
+    id: 1802,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "椿榭Tz",
+    authorAvatar: "🥣",
+    originalText: "你觉得咋样？",
+    translatedText: "어떤 것 같아?",
+    images: ["images/2407-weather.jpg"],
+    comments: [
+      {
+        id: 180201,
+        username: "周翊然Tz",
+        originalText: "热",
+        translatedText: "더워",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1803,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "舟上雨入愿",
+    authorAvatar: "🥣",
+    originalText: "然然回国吃的第一顿大餐是什么👽",
+    translatedText: "익연아 귀국하고 첫 번째로 먹은 거 뭐야👽",
+    images: [],
+    comments: [
+      {
+        id: 180301,
+        username: "周翊然Tz",
+        originalText: "肉",
+        translatedText: "고기",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1804,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "啵啵粥TZ",
+    authorAvatar: "🥣",
+    originalText: "哈哈哈，然然给欣赏一下我最近吃的一些美食，都巨好吃。还有可爱的小狗🐶，还有好看的天，你看那个云☁️是爱心❤️形状哒！好美😍然然最近有吃啥好吃的嘛，可以分享一下吗！！！翻我翻我翻我翻我翻我翻我翻我翻我！！！！！！！！",
+    translatedText: "ㅋㅋㅋ 익연아 내가 요즘 먹은 맛있는 거 감상해봐, 다 엄청 맛있어. 귀여운 강아지🐶도 있고 예쁜 하늘도 있어, 저 구름☁️ 하트❤️ 모양이잖아! 너무 예뻐😍 익연아 요즘 맛있는 거 먹었어? 공유해줄 수 있어!!! 답글 달아줘 달아줘 달아줘 달아줘 달아줘 달아줘 달아줘 달아줘!!!!!!!",
+    images: ["images/2407-food.jpg"],
+    comments: [
+      {
+        id: 180401,
+        username: "周翊然Tz",
+        originalText: "图4好像蜡笔小新",
+        translatedText: "4번 사진 짱구 같은데",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1805,
+    date: "2024-07-22",
+    weiboUrl: "https://weibo.com/7872504973/OoLvc8mAq",
+    authorName: "周_什么然",
+    authorAvatar: "🥣",
+    originalText: "宝宝 我给你看我拍的月亮 是不是很好看呀",
+    translatedText: "자기야 내가 찍은 달 보여줄게 예쁘지 않아?",
+    images: ["images/2407-moon2.jpg"],
+    comments: [
+      {
+        id: 180501,
+        username: "周翊然Tz",
+        originalText: "复",
+        translatedText: "답",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1806,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "翊杯冻柠茶",
+    authorAvatar: "🥣",
+    originalText: "月初去了青城山玩 给你看看山 在青城山遇见了翼然亭 想拍下来给你看看 吃了串串 冒菜 还有奶茶！！ 我猜你回北京肯定去吃了重庆火锅🍲吧！看 重庆涨水嘞！今天涨水涨得好高",
+    translatedText: "이달 초에 칭청산 놀러 갔어 산 보여줄게 칭청산에서 이란정을 만났어 사진 찍어서 보여주고 싶었어 촨촨이랑 마오차이랑 버블티도 먹었어!! 베이징 돌아가면 분명 충칭 훠궈🍲 먹었겠지! 봐봐 충칭에 물이 불어났어! 오늘 엄청 많이 불었어",
+    images: ["images/2407-mountain.jpg"],
+    comments: [
+      {
+        id: 180601,
+        username: "周翊然Tz",
+        originalText: "注意安全",
+        translatedText: "안전 조심해",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1807,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "冻柠乐luftmensch",
+    authorAvatar: "🥣",
+    originalText: "本来心情不好 立刻跑上天台看晚霞 心情就变好了 分享给你 如果你能回我就更好啦🫠",
+    translatedText: "기분이 안 좋았는데 바로 옥상 뛰어올라가서 노을 봤더니 기분이 좋아졌어 너한테도 공유할게 네가 답장해주면 더 좋겠지만🫠",
+    images: ["images/2407-sunset3.jpg"],
+    comments: [
+      {
+        id: 180701,
+        username: "周翊然Tz",
+        originalText: "祝你天天开心",
+        translatedText: "매일 행복하길 바라",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1808,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "慕子希溜鼻涕",
+    authorAvatar: "🥣",
+    originalText: "请问钓到鱼了吗？但是我看这样子不像钓到鱼的样子！",
+    translatedText: "혹시 물고기 잡혔어? 근데 보니까 잡힌 것 같진 않아!",
+    images: ["images/2407-fishing.jpg"],
+    comments: [
+      {
+        id: 180801,
+        username: "周翊然Tz",
+        originalText: "emmm",
+        translatedText: "음mm",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1809,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "一一只耳机",
+    authorAvatar: "🥣",
+    originalText: "哈喽然然，真的好快，喜欢你已经一年零五天啦，最开始入坑是去年你横店晚上下班时开窗收粉丝信，还提醒注意安全，当时就觉得你人真的好好，这一年其实是高三奋斗的日子，有时迷茫、动力不足的时候，想想你或者和同学讨论你的事就会被治愈好多😊😊 也是真的幸运昨天刚过完18岁生日，今天就可以和你分享这份开心了。对了有时看视频就觉得你是一个超有上进心的演员，相比之下我自制力就有些不行，以后还要多多想你学习呀，下面是和你分享一些最近生活，也希望你能在忙碌的同时放松几分☺️最后可以回复嘛🥰",
+    translatedText: "안녕 익연아, 진짜 빠르다, 좋아한 지 벌써 1년 5일이야, 처음 입덕한 건 작년에 네가 헝뎬에서 밤에 퇴근할 때 창문 열고 팬 편지 받으면서 안전 조심하라고 했을 때인데, 그때 진짜 좋은 사람이다 싶었어. 이 1년이 사실 고3 열심히 한 시간이었는데, 가끔 막막하고 의욕 없을 때 너 생각하거나 친구들이랑 너 얘기 하면 많이 힐링됐어😊😊 어제 18살 생일을 맞이한 것도 정말 운이 좋아서, 오늘 이 기쁨을 너랑 나눌 수 있게 됐어. 그리고 가끔 영상 보면 너는 정말 향상심 넘치는 배우인 것 같아, 그에 비해 나는 자제력이 좀 부족해서, 앞으로 너 많이 본받아야겠어. 아래는 요즘 일상 공유하는 거고, 바쁜 와중에도 좀 쉬었으면 좋겠어☺️ 마지막으로 답장해줄 수 있어?🥰",
+    images: ["images/2407-birthday.jpg"],
+    comments: [
+      {
+        id: 180901,
+        username: "周翊然Tz",
+        originalText: "生日快乐",
+        translatedText: "생일 축하해",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1810,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "要睡个好觉觉-",
+    authorAvatar: "🥣",
+    originalText: "小然累了就坐坐吧🙋🏻‍♀️🙋🏻‍♀️",
+    translatedText: "익연아 힘들면 좀 앉아봐🙋🏻‍♀️🙋🏻‍♀️",
+    images: ["images/2407-chair.jpg"],
+    comments: [
+      {
+        id: 181001,
+        username: "周翊然Tz",
+        originalText: "谢谢你😂",
+        translatedText: "고마워😂",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1811,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "倾雨眠_Tzy",
+    authorAvatar: "🥣",
+    originalText: "必须给你下单",
+    translatedText: "무조건 주문해줘야지",
+    images: ["images/2407-order.jpg"],
+    comments: [
+      {
+        id: 181101,
+        username: "周翊然Tz",
+        originalText: "🤔?",
+        translatedText: "🤔?",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1812,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "阿昭哪去了",
+    authorAvatar: "🥣",
+    originalText: "小周给你看我画的小法带小夹子 今天钓到的是什么鱼呀？最近有开到心爱的赛车吗？已经喜欢你一年多啦 很高兴遇见你 天天开心周翊然",
+    translatedText: "익연아 내가 그린 페라리 헤어클립 봐봐 오늘 무슨 물고기 잡혔어? 요즘 좋아하는 레이싱카 탔어? 좋아한 지 벌써 1년 넘었어 만나서 반가워 매일 행복해 주익연",
+    images: ["images/2407-drawing2.jpg"],
+    comments: [
+      {
+        id: 181201,
+        username: "周翊然Tz",
+        originalText: "也祝你天天开心",
+        translatedText: "나도 매일 행복하길 바라",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1813,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "小狗最近还发呆吗",
+    authorAvatar: "🥣",
+    originalText: "宝宝你真的有白头发吗",
+    translatedText: "자기야 진짜 흰머리 있어?",
+    images: [],
+    comments: [
+      {
+        id: 181301,
+        username: "周翊然Tz",
+        originalText: "😂拔了",
+        translatedText: "😂뽑았어",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1814,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "芝芝鱼饼-ToTox",
+    authorAvatar: "🥣",
+    originalText: "感觉小周用的是老年机，咋回复的这么慢啊？",
+    translatedText: "익연이 구식 폰 쓰는 것 같아, 왜 이렇게 답장이 느려?",
+    images: [],
+    comments: [
+      {
+        id: 181401,
+        username: "周翊然Tz",
+        originalText: "我用智能机",
+        translatedText: "스마트폰 쓰는데",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1815,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "腻歪歪ran",
+    authorAvatar: "🥣",
+    originalText: "可以回我一个问号吗？",
+    translatedText: "물음표 하나만 답해줄 수 있어?",
+    images: [],
+    comments: [
+      {
+        id: 181501,
+        username: "周翊然Tz",
+        originalText: "🤔",
+        translatedText: "🤔",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1816,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "鱿鱼小小卷",
+    authorAvatar: "🥣",
+    originalText: "分享一下我最近看到的夕阳和小动物们！！！！ 第二张舍友说呆呆的很像我哈哈哈哈哈哈 还有出去偶遇的烟花 最近三刷了小飞奔！还是很好看！！😏 最后是一只四个月大的柯基 巨可爱！！！ 最近还在准备教资笔试 有点摆烂了哈哈哈哈 好啦 分享给你 希望然然天天开心喔😘",
+    translatedText: "요즘 봤던 노을이랑 작은 동물들 공유할게!!!! 2번째 사진은 룸메가 멍하니 있는 게 나 닮았다고 했어ㅋㅋㅋㅋㅋㅋ 나가다가 우연히 만난 불꽃놀이도 있고 최근에 소비奔 세 번 봤어! 아직도 너무 좋아!!😏 마지막은 생후 4개월 코기 엄청 귀여워!!! 요즘 교원자격증 필기시험 준비 중인데 좀 놀고 있어ㅋㅋㅋㅋ 좋아 공유해줄게 익연이 매일 행복하길😘",
+    images: ["images/2407-collage2.jpg"],
+    comments: [
+      {
+        id: 181601,
+        username: "周翊然Tz",
+        originalText: "很可爱 考试加油",
+        translatedText: "너무 귀엽다 시험 화이팅",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1817,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "Aphasianan",
+    authorAvatar: "🥣",
+    originalText: "喜欢开赛车扣1",
+    translatedText: "레이싱카 좋아하면 1 눌러줘",
+    images: [],
+    comments: [
+      {
+        id: 181701,
+        username: "周翊然Tz",
+        originalText: "11111",
+        translatedText: "11111",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1818,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "耶耶真的不想上班",
+    authorAvatar: "🥣",
+    originalText: "也可以回复我一个问号吗❔",
+    translatedText: "나한테도 물음표 하나 답해줄 수 있어?❔",
+    images: [],
+    comments: [
+      {
+        id: 181801,
+        username: "周翊然Tz",
+        originalText: "🧐",
+        translatedText: "🧐",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1819,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "赤木桐紫-Tz",
+    authorAvatar: "🥣",
+    originalText: "被理想大学录取啦",
+    translatedText: "목표 대학에 합격했어",
+    images: ["images/2407-acceptance.jpg"],
+    comments: [
+      {
+        id: 181901,
+        username: "周翊然Tz",
+        originalText: "恭喜",
+        translatedText: "축하해",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1820,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "柚子元元堡",
+    authorAvatar: "🥣",
+    originalText: "我说15的月亮你说__________🤭🤭🤭 来看圆圆的月亮啦小然",
+    translatedText: "내가 15일의 달이라고 하면 넌__________🤭🤭🤭 둥근 달 보러 와 익연아",
+    images: ["images/2407-moon.jpg"],
+    comments: [
+      {
+        id: 182001,
+        username: "周翊然Tz",
+        originalText: "圆？",
+        translatedText: "둥글어?",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1821,
+    date: "2024-07-22",
+    weiboUrl: "",
+    authorName: "翊閃翊閃亮晶晶yu__",
+    authorAvatar: "🥣",
+    originalText: "小然 炎炎夏日吃冰吗😜🍧",
+    translatedText: "익연아 무더운 여름에 아이스크림 먹어?😜🍧",
+    images: [],
+    comments: [
+      {
+        id: 182101,
+        username: "周翊然Tz",
+        originalText: "看着不错👍",
+        translatedText: "맛있어 보이는데👍",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1822,
+    date: "2024-07-22",
+    weiboUrl: "",
+    originalText: "饿了 吃饭去了",
+    translatedText: "배고파 밥 먹으러 간다",
+    images: [],
+    comments: []
+  },
+  {
+    id: 1823,
+    date: "2024-07-22",
+    weiboUrl: "",
+    originalText: "88",
+    translatedText: "88",
+    images: [],
+    comments: []
+  },
+  {
+    id: 1824,
+    date: "2024-07-23",
+    weiboUrl: "",
+    originalText: "下班了🙌",
+    translatedText: "퇴근했어🙌",
+    images: [],
+    comments: []
+  },
+  {
+    id: 1825,
+    date: "2024-07-23",
+    weiboUrl: "",
+    originalText: "今日偶遇明盛同学",
+    translatedText: "오늘 밍성 동생과 우연히 만났어",
+    images: ["images/2407-mingsheng.jpg"],
+    comments: []
+  }
+];
