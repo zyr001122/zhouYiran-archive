17dc77adeaa60908546579903c805308086376c7
zyr001122 <vazseyo@gmail.com>
2026-07-12 14:33:10 +0900

    Add files via upload

---
+++ data-2024-10.js
@@ -0,0 +1,455 @@
+const posts_202410 = [
+  {
+    id: 2101,
+    date: "2024-10-07",
+    weiboUrl: "https://weibo.com/5994946077/OAtsiCvyt",
+    originalText: "吃饭了吗各位",
+    translatedText: "다들 밥 먹었어?",
+    images: [],
+    comments: [
+      {
+        id: 210101,
+        username: "拾七ooo",
+        originalText: "吃了🥰（至尊版泡面",
+        translatedText: "먹었어🥰(프리미엄 라면",
+        images: ["images/2410-ramen2.jpg"],
+        actorReply: { originalText: "你和那位火鸡面旗鼓相当", translatedText: "저 불닭 팬이랑 막상막하네" }
+      },
+      {
+        id: 210102,
+        username: "综上所述你最萌",
+        originalText: "我自己煮的火鸡面！！辣到飞起",
+        translatedText: "내가 직접 끓인 불닭볶음면!! 너무 매워서 날아갈 것 같아",
+        images: ["images/2410-buldak.jpg"],
+        actorReply: { originalText: "火鸡面真有火鸡是第一次见", translatedText: "불닭면에 진짜 닭이 있는 건 처음 봐" }
+      },
+      {
+        id: 210103,
+        username: "桃丸香芋冻_",
+        originalText: "猜这是什么😁",
+        translatedText: "이거 뭔지 맞춰봐😁",
+        images: ["images/2410-fish.jpg"],
+        actorReply: { originalText: "水煮鱼？", translatedText: "수이주위?" }
+      },
+      {
+        id: 210104,
+        username: "Yirrrraan",
+        originalText: "宝宝你手这是受伤了吗 🥺🥺🥺🥺🥺🥺😫😭",
+        translatedText: "자기야 손 다친 거야?🥺🥺🥺🥺🥺🥺😫😭",
+        images: ["images/2410-hand.jpg"],
+        actorReply: { originalText: "这是血浆侵进去了 很难洗", translatedText: "혈장이 스며든 거야 잘 안 지워져" }
+      },
+      {
+        id: 210105,
+        username: "美少女壮士_lucky",
+        originalText: "哥哥请你吃火锅，吃吗？",
+        translatedText: "오빠 훠궈 사줄게, 먹을래?",
+        images: ["images/2410-hotpot7.jpg"],
+        actorReply: { originalText: "超真实扮家家酒？？", translatedText: "엄청 진짜 같은 소꿉놀이??" }
+      },
+      {
+        id: 210106,
+        username: "·限定莓极光·",
+        originalText: "开车哥你想念开车了吗🚗",
+        translatedText: "운전오빠 레이싱 그리워?🚗",
+        images: [],
+        actorReply: { originalText: "非常想念 做梦都在开车", translatedText: "엄청 그리워 꿈에서도 운전하고 있어" }
+      },
+      {
+        id: 210107,
+        username: "Moonear__",
+        originalText: "明天去迪士尼玩，有推荐的项目吗",
+        translatedText: "내일 디즈니랜드 가는데, 추천 놀이기구 있어?",
+        images: [],
+        actorReply: { originalText: "最后的烟花很不错！", translatedText: "마지막 불꽃놀이 진짜 좋아!" }
+      },
+      {
+        id: 210108,
+        username: "我是屁屁大王-",
+        originalText: "我们这里下雨啦然然！",
+        translatedText: "우리 여기 비 온다 익연아!",
+        images: ["images/2410-rain.jpg"],
+        actorReply: { originalText: "我这也在下雨！", translatedText: "여기도 비 와!" }
+      },
+      {
+        id: 210109,
+        username: "小孙菜奈",
+        originalText: "铁锅炖～然然 你想不想😍",
+        translatedText: "티에궈둔～ 익연아 먹고 싶지 않아?😍",
+        images: ["images/2410-goose.jpg"],
+        actorReply: { originalText: "是大鹅吗😒", translatedText: "거위야?😒" }
+      },
+      {
+        id: 210110,
+        username: "周翊然Tz",
+        originalText: "最近有些日夜颠倒",
+        translatedText: "요즘 낮밤이 좀 뒤바뀌었어",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 210111,
+        username: "周翊然Tz",
+        originalText: "放饭了 这会儿在改妆",
+        translatedText: "밥 나왔어 지금 분장 고치는 중",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 210112,
+        username: "周翊然Tz",
+        originalText: "假期结束 各位心情怎么样",
+        translatedText: "연휴 끝났는데 다들 기분 어때?",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 210113,
+        username: "周翊然Tz",
+        originalText: "我吃了这个",
+        translatedText: "나는 이거 먹었어",
+        images: ["images/2410-food-yiran.jpg"],
+        actorReply: null
+      },
+      {
+        id: 210114,
+        username: "周翊然Tz",
+        originalText: "跟大家叨叨几句我就要去拍戏了",
+        translatedText: "다들한테 몇 마디 하고 나서 촬영하러 가야 해",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 210115,
+        username: "周翊然Tz",
+        originalText: "给大家认个错 本周报备欠大家一期",
+        translatedText: "다들한테 사과할게 이번 주 주간보고 한 회 빚졌어",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 210116,
+        username: "周翊然Tz",
+        originalText: "看了眼评论区发现不该问大家吃了没 馋死我了🙄",
+        translatedText: "댓글창 봤는데 밥 먹었냐고 물어보는 게 아니었어 너무 먹고 싶어 죽겠어🙄",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 210117,
+        username: "周翊然Tz",
+        originalText: "我走了 上班去了 最近降温了 大家要注意保暖 换季感冒也要预防 拜拜！",
+        translatedText: "갈게 일하러 간다 요즘 기온 많이 내려갔으니까 다들 따뜻하게 입고 환절기 감기도 조심해 잘 있어!",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 2102,
+    date: "2024-10-18",
+    weiboUrl: "https://weibo.com/5994946077/OC1amgv2s",
+    originalText: "这次真的真的下班了😴",
+    translatedText: "이번엔 진짜 진짜 퇴근했어😴",
+    images: [],
+    comments: []
+  },
+  {
+    id: 2103,
+    date: "2024-10-22",
+    weiboUrl: "https://weibo.com/5994946077/OCLOgrB26",
+    originalText: "收工下班 我来了",
+    translatedText: "촬영 끝내고 퇴근 왔어",
+    images: [],
+    comments: [
+      {
+        id: 210301,
+        username: "周翊然Tz",
+        originalText: "明天早班 我走了",
+        translatedText: "내일 아침 일찍 출근이라 갈게",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 210302,
+        username: "周翊然Tz",
+        originalText: "大家都早点休息🌃",
+        translatedText: "다들 일찍 자🌃",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 210303,
+        username: "周翊然Tz",
+        originalText: "晚安💤",
+        translatedText: "잘 자💤",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 2104,
+    date: "2024-10-22",
+    weiboUrl: "https://weibo.com/7801093950/OCLOIlY4i",
+    authorName: "养只小狗叫拉瑞_",
+    authorAvatar: "🥣",
+    originalText: "想被回复",
+    translatedText: "답글 받고 싶어",
+    images: [],
+    comments: [
+      {
+        id: 210401,
+        username: "周翊然Tz",
+        originalText: "行",
+        translatedText: "알겠어",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 2105,
+    date: "2024-10-22",
+    weiboUrl: "https://weibo.com/7540057458/OCLOJvHqZ",
+    authorName: "螺蛳粉不加豆角",
+    authorAvatar: "🥣",
+    originalText: "周哥晚上好呀 你吃饭了吗 给你看看我这几天吃的🥰",
+    translatedText: "익연이형 좋은 저녁이야 밥 먹었어? 요즘 내가 먹은 거 보여줄게🥰",
+    images: ["images/2410-food2.jpg"],
+    comments: [
+      {
+        id: 210501,
+        username: "周翊然Tz",
+        originalText: "天冷了多吃点",
+        translatedText: "날씨 추워졌으니 많이 먹어",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 2106,
+    date: "2024-10-22",
+    weiboUrl: "https://weibo.com/5746329419/OCLPj2MGY",
+    authorName: "肥肥Tz",
+    authorAvatar: "🥣",
+    originalText: "📸关于普陀山和东极岛的记忆 🌅印象特别深的是东极岛的落日一会一个颜色 随手一拍都感觉是宫崎骏先生的动漫走进了现实里 真的超级好看啊！ ⛰️普陀山上拜了千年古寺普济寺 不小心弄丢了自己准备的香火钱 破财消灾 🌳在一棵大树下偶遇正在乘凉的和尚 🙏见南海观音许愿你平安喜乐",
+    translatedText: "📸 보타산이랑 동지도의 기억 🌅 동지도 노을이 특히 인상 깊었어 순간순간 색이 바뀌는데 아무렇게나 찍어도 미야자키 하야오 애니메이션이 현실로 들어온 것 같았어 진짜 너무 예뻐! ⛰️ 보타산에서 천년 고찰 푸지사에 참배했는데 준비한 향화 돈을 잃어버렸어 재물이 나가면 액운이 사라진다고 🌳 큰 나무 아래에서 더위 피하는 스님을 우연히 만났어 🙏 남해관음을 뵙고 네가 평안하고 행복하길 빌었어",
+    images: ["images/2410-putuoshan.jpg"],
+    comments: [
+      {
+        id: 210601,
+        username: "周翊然Tz",
+        originalText: "谢谢 你为自己许愿了吗",
+        translatedText: "고마워 너 자신을 위해서도 빌었어?",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 2107,
+    date: "2024-10-22",
+    weiboUrl: "https://weibo.com/5401920743/OCLR5fNw9",
+    authorName: "腮帮子鼓了Tz",
+    authorAvatar: "🥣",
+    originalText: "终于来了！然然看，实习空闲去逛展看见的，一眼就觉得是你和法拉利的坐骑",
+    translatedText: "드디어 왔어! 익연아 봐봐, 인턴 쉬는 시간에 전시 구경 갔다가 봤는데, 딱 보자마자 너랑 페라리 탈 것 같다는 생각이 들었어",
+    note: "원문은 동영상",
+    images: [],
+    comments: [
+      {
+        id: 210701,
+        username: "周翊然Tz",
+        originalText: "法拉利应该会喜欢👍",
+        translatedText: "페라리가 좋아할 것 같은데👍",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 2108,
+    date: "2024-10-22",
+    weiboUrl: "https://weibo.com/5475036907/OCLRDwjtW",
+    authorName: "晶晶晶晶陀",
+    authorAvatar: "🥣",
+    originalText: "然子哥！给我考研加油吧！每天学到头秃，只有五十多天就要考啦！",
+    translatedText: "익연이형! 대학원 시험 응원해줘! 매일 공부하느라 대머리 될 것 같아, 50일 조금 넘게 남았어!",
+    note: "원문은 동영상",
+    images: [],
+    comments: [
+      {
+        id: 210801,
+        username: "周翊然Tz",
+        originalText: "加油💪🏻 等你的好消息",
+        translatedText: "화이팅💪🏻 좋은 소식 기다릴게",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 2109,
+    date: "2024-10-22",
+    weiboUrl: "https://weibo.com/7281693084/OCLRZDL87",
+    authorName: "-桃气泡泡啵",
+    authorAvatar: "🥣",
+    originalText: "哥哥我喜欢你十年了 可以回我一次吗",
+    translatedText: "오빠 좋아한 지 10년 됐어 한 번만 답해줄 수 있어?",
+    images: [],
+    comments: [
+      {
+        id: 210901,
+        username: "周翊然Tz",
+        originalText: "啊？",
+        translatedText: "어?",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 2110,
+    date: "2024-10-22",
+    weiboUrl: "https://weibo.com/5870403807/OCLRYwd66",
+    authorName: "_菲菲菲儿_",
+    authorAvatar: "🥣",
+    originalText: "hi 然然 最近的哈尔滨马上要迎来冬天 很遗憾上次你来哈尔滨的时候没能见面 那时正在备考研究生 幸运的是最终如愿以偿 来日方长 下次见 顺便和你分享一下这里的秋天 注意身体 一切顺利",
+    translatedText: "hi 익연아 요즘 하얼빈은 곧 겨울이 찾아올 것 같아 지난번에 하얼빈 왔을 때 못 만난 게 아쉬워 그때 대학원 시험 준비 중이었거든 다행히 결국 원하는 대로 됐어 앞날은 길어 다음에 만나 겸사겸사 이곳의 가을 공유할게 몸 잘 챙기고 다 잘 되길",
+    images: ["images/2410-harbin.jpg"],
+    comments: [
+      {
+        id: 211001,
+        username: "周翊然Tz",
+        originalText: "恭喜你得偿所愿！",
+        translatedText: "원하던 대로 됐다니 축하해!",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 2111,
+    date: "2024-10-22",
+    weiboUrl: "https://weibo.com/7377518906/OCLSw81Qp",
+    authorName: "唯然a",
+    authorAvatar: "🥣",
+    originalText: "然然看我今天做的手工 抽不抽象",
+    translatedText: "익연아 내가 오늘 만든 공예품 봐봐 추상적이야 아니야",
+    images: ["images/2410-craft.jpg"],
+    comments: [
+      {
+        id: 211101,
+        username: "周翊然Tz",
+        originalText: "不抽象 很漂亮",
+        translatedText: "추상적이지 않아 예쁘다",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 2112,
+    date: "2024-10-22",
+    weiboUrl: "https://weibo.com/7882429354/OCLSH58e8",
+    authorName: "翊起飞Tz",
+    authorAvatar: "🥣",
+    originalText: "天冷了要不给我们小法也安排一双，手套呗😁😁",
+    translatedText: "날씨 추워졌는데 우리 꼬마 페라리한테도 장갑 하나 사줘😁😁",
+    images: ["images/2410-gloves.jpg"],
+    comments: [
+      {
+        id: 211201,
+        username: "周翊然Tz",
+        originalText: "有推荐吗🤔",
+        translatedText: "추천 있어?🤔",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 2113,
+    date: "2024-10-22",
+    weiboUrl: "https://weibo.com/6941291812/OCLTMtwbT",
+    authorName: "肯提_Kentii1",
+    authorAvatar: "🥣",
+    originalText: "我这下雨了 然后我用上了自己画的伞",
+    translatedText: "여기 비 왔어 그래서 내가 직접 그린 우산 꺼냈어",
+    note: "원문은 동영상",
+    images: [],
+    comments: [
+      {
+        id: 211301,
+        username: "周翊然Tz",
+        originalText: "你的雨天是蓝色的",
+        translatedText: "네 비 오는 날은 파란색이네",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 2114,
+    date: "2024-10-22",
+    weiboUrl: "https://weibo.com/1779470577/OCLU31sRQ",
+    authorName: "嘟饱爱周粥",
+    authorAvatar: "🥣",
+    originalText: "分享今天阳朔的风景给你",
+    translatedText: "오늘 양숴 풍경 공유할게",
+    images: ["images/2410-yangshuo.jpg"],
+    comments: [
+      {
+        id: 211401,
+        username: "周翊然Tz",
+        originalText: "真是个好地方",
+        translatedText: "진짜 좋은 곳이네",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 2115,
+    date: "2024-10-22",
+    weiboUrl: "https://weibo.com/7832334429/OCLUJBAdi",
+    authorName: "Mennnxg",
+    authorAvatar: "🥣",
+    originalText: "下次见！",
+    translatedText: "다음에 봐!",
+    images: [],
+    comments: [
+      {
+        id: 211501,
+        username: "周翊然Tz",
+        originalText: "下次见",
+        translatedText: "다음에 봐",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 2116,
+    date: "2024-10-22",
+    weiboUrl: "https://weibo.com/7744515295/OCLUB3FDU",
+    authorName: "褪色主义",
+    authorAvatar: "🥣",
+    originalText: "今天画的色彩咋样🤫🤫 还有39天联考 可以祝我联考顺利嘛 回我开心一辈子",
+    translatedText: "오늘 그린 색감 어때?🤫🤫 연합고사까지 39일 남았어 연합고사 잘 보게 빌어줄 수 있어? 답장해주면 평생 행복할게",
+    note: "원문은 동영상",
+    images: [],
+    comments: [
+      {
+        id: 211601,
+        username: "周翊然Tz",
+        originalText: "祝你高分",
+        translatedText: "고득점 기원할게",
+        images: [],
+        actorReply: null
+      }
+    ]
+  }
+];
