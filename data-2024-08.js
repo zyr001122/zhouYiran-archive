c6d655863d3686381a7a10860876e5f00e50aada
zyr001122 <vazseyo@gmail.com>
2026-07-12 14:04:55 +0900

    Add files via upload

---
+++ data-2024-08.js
@@ -0,0 +1,241 @@
+const posts_202408 = [
+  {
+    id: 1901,
+    date: "2024-08-22",
+    weiboUrl: "",
+    originalText: "来了",
+    translatedText: "왔어",
+    images: [],
+    comments: [
+      {
+        id: 190101,
+        username: "周翊然Tz",
+        originalText: "走了啊👋🏻",
+        translatedText: "갈게👋🏻",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1902,
+    date: "2024-08-22",
+    weiboUrl: "",
+    authorName: "倾雨眠_Tzy",
+    authorAvatar: "🥣",
+    originalText: "你谈吉他也模仿一下",
+    translatedText: "기타 치는 거 따라해봐",
+    images: ["images/2408-guitar.jpg"],
+    comments: [
+      {
+        id: 190201,
+        username: "周翊然Tz",
+        originalText: "😯?",
+        translatedText: "😯?",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1903,
+    date: "2024-08-22",
+    weiboUrl: "",
+    authorName: "多云骤雨晴",
+    authorAvatar: "🥣",
+    originalText: "海上日出🌄 请你看",
+    translatedText: "바다 위 일출🌄 봐줘",
+    images: [],
+    comments: [
+      {
+        id: 190301,
+        username: "周翊然Tz",
+        originalText: "很壮观 谢谢",
+        translatedText: "장관이다 고마워",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1904,
+    date: "2024-08-22",
+    weiboUrl: "",
+    authorName: "语来啦呀",
+    authorAvatar: "🥣",
+    originalText: "可以可以 怎么这么慢 这次是智能机吧?🤔🤔🤔",
+    translatedText: "됐어 됐어 왜 이렇게 느려 이번엔 스마트폰이지?🤔🤔🤔",
+    images: [],
+    comments: [
+      {
+        id: 190401,
+        username: "周翊然Tz",
+        originalText: "因为太热了",
+        translatedText: "너무 더워서",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1905,
+    date: "2024-08-22",
+    weiboUrl: "",
+    authorName: "拾柒暮_Tz",
+    authorAvatar: "🥣",
+    originalText: "走过路过不要错过～欢迎然然来看英国风光（上个月错过空降了😭） p1飞机上的美丽风光 下面都是伦敦和 에든버러의 관광지 p10昨天去看柯南电影，但没拍到基德，求安慰😭😭😭",
+    translatedText: "지나가다 들러~ 익연아 영국 풍경 보러 와 (지난달 깜짝 등장 놓쳤어😭) p1 비행기에서 찍은 아름다운 풍경 아래는 런던이랑 에든버러 관광지들 p10 어제 코난 영화 보러 갔는데 키드 못 찍었어, 위로해줘😭😭😭",
+    images: ["images/2408-uk.jpg"],
+    comments: [
+      {
+        id: 190501,
+        username: "周翊然Tz",
+        originalText: "但你看过了世界",
+        translatedText: "그래도 세상을 봤잖아",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1906,
+    date: "2024-08-22",
+    weiboUrl: "",
+    authorName: "小呆洱",
+    authorAvatar: "🥣",
+    originalText: "周翊然不回我我只能大醉一场了🥹",
+    translatedText: "주익연이 답장 안 해주면 그냥 취해버릴 수밖에🥹",
+    images: ["images/2408-drink.jpg"],
+    comments: [
+      {
+        id: 190601,
+        username: "周翊然Tz",
+        originalText: "大醉的应该是蚊子",
+        translatedText: "취해야 하는 건 모기지",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1907,
+    date: "2024-08-22",
+    weiboUrl: "",
+    authorName: "周实悦",
+    authorAvatar: "🥣",
+    originalText: "爬山路上拍的 你能看清路上的字吗",
+    translatedText: "등산하다가 찍었어 길에 있는 글씨 읽혀?",
+    images: ["images/2408-hiking.jpg"],
+    comments: [
+      {
+        id: 190701,
+        username: "周翊然Tz",
+        originalText: "全是套路",
+        translatedText: "다 뻔한 수법이네",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1908,
+    date: "2024-08-22",
+    weiboUrl: "",
+    authorName: "先干饭吧Tz",
+    authorAvatar: "🥣",
+    originalText: "可以翻我吗🤔",
+    translatedText: "나한테 답글 달아줄 수 있어?🤔",
+    note: "원문은 동영상",
+    images: [],
+    comments: [
+      {
+        id: 190801,
+        username: "周翊然Tz",
+        originalText: "🤜🏻🙏🏻",
+        translatedText: "🤜🏻🙏🏻",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1909,
+    date: "2024-08-22",
+    weiboUrl: "",
+    authorName: "7ovo_r",
+    authorAvatar: "🥣",
+    originalText: "看我画的宇航员！",
+    translatedText: "내가 그린 우주비행사 봐봐!",
+    images: [],
+    comments: [
+      {
+        id: 190901,
+        username: "周翊然Tz",
+        originalText: "抽象派",
+        translatedText: "추상파네",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1910,
+    date: "2024-08-22",
+    weiboUrl: "",
+    authorName: "记得萌啊",
+    authorAvatar: "🥣",
+    originalText: "我觉得我拍的云超好看，你觉得呢～",
+    translatedText: "내가 찍은 구름 너무 예쁜 것 같아, 어때～",
+    images: ["images/2408-cloud.jpg"],
+    comments: [
+      {
+        id: 191001,
+        username: "吃瓜罗伯特",
+        originalText: "吃瓜罗伯特觉得你拍的云朵像是周翊然在天空中画画，超级有艺术感的！点赞！",
+        translatedText: "이 구름 사진은 마치 주익연이 하늘에 그림을 그리는 것 같아요, 엄청 예술적이에요! 좋아요!",
+        images: [],
+        actorReply: { originalText: "😯?", translatedText: "😯?" }
+      }
+    ]
+  },
+  {
+    id: 1911,
+    date: "2024-08-22",
+    weiboUrl: "",
+    authorName: "Panorama_zjm",
+    authorAvatar: "🥣",
+    originalText: "Hi👋🏻亲爱的小然 转眼间8月的尾声已悄然而至 而离你的破壳日也越来越近啦～不知道浙江的8月有没有很热呢？（不过重庆今天42度 嗯 有空常回家看看）听说你的8月那是过的多姿多彩 又开上车了开车哥🏎️（我浅浅的听说了一下 比赛那天赛道60度高温呢🔥但是开车哥是那么的爱开车 赴汤蹈火在所不惜）还拿了个小组冠军是吧😏我就知道 我们翊然不管做什么都会成功🥹你超级棒知道吗👊🏻 然后 我来说说我的8月吧 我8月初去了考瑞亚🇰🇷去看了那些我们所没见过的世界🌍这个世界真的很美好 我去坐了落日飞车（我想你一定会喜欢 所以每天都有往你的私信里发照片 小周老师记得查收啊）我也拍了很多很多好看的照片 吃到了各种各样的美食 把他剪成了vlog 想和你分享的东西有很多 ～ 我觉得每一张照片都像一段故事 他包含了拍摄者的情绪与美好的记忆 在我的镜头下 这段回忆都是难忘且美好的 所以 我想把我的快乐分享给你 也想带着你一起去看看那些没见过的世界～带着我们的勇气与力量一直坚定不移地走下去 最后 祝小然天天开心 新戏开机大吉 也期待早日和我们慕清晏见面‼️",
+    translatedText: "Hi👋🏻 사랑하는 익연아 눈 깜짝할 새 8월의 끝자락이 다가왔어 그리고 네 생일도 점점 가까워지고 있네～ 저장의 8월이 많이 더운지 모르겠어 (근데 충칭은 오늘 42도야 응 시간 나면 집에 자주 와) 네 8월이 정말 다채롭게 지나갔다고 들었어 또 레이싱카 탔구나 운전오빠🏎️ (살짝 들었는데 경기 날 트랙이 60도였다고🔥 근데 운전오빠는 레이싱카를 그렇게 사랑하니까 불 속이라도 뛰어드는 거지) 조 우승도 했다며😏 그럴 줄 알았어 우리 익연이는 뭘 해도 성공할 거야🥹 진짜 대단한 거 알지?👊🏻 그리고 내 8월 얘기도 해볼게 8월 초에 한국🇰🇷 갔다 왔어 우리가 본 적 없는 세상을 보러🌍 이 세상은 정말 아름다워 선셋 롤러코스터도 탔어 (네가 분명 좋아할 것 같아서 매일 DM으로 사진 보냈는데 작은 주 선생님 꼭 확인해줘) 예쁜 사진도 엄청 많이 찍고 다양한 음식도 먹고 브이로그로 편집했어 너랑 공유하고 싶은 게 정말 많아～ 사진 한 장 한 장이 하나의 이야기 같아 찍는 사람의 감정과 아름다운 기억이 담겨있어 내 카메라 앵글 안에서 이 기억들은 모두 잊지 못할 소중한 것들이야 그래서 내 행복을 너와 나누고 싶고 너도 데리고 한 번도 본 적 없는 세상을 같이 보러 가고 싶어～ 우리의 용기와 힘을 갖고 흔들림 없이 계속 나아가자 마지막으로 익연이 매일 행복하길 새 드라마 촬영 시작 대길 그리고 우리 무칭옌을 빨리 만날 날을 기대해‼️",
+    images: ["images/2408-korea.jpg"],
+    comments: [
+      {
+        id: 191101,
+        username: "周翊然Tz",
+        originalText: "你的这一段故事很美好 也祝你天天开心",
+        translatedText: "네 이 이야기는 정말 아름다워 나도 매일 행복하길 바라",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 1912,
+    date: "2024-08-22",
+    weiboUrl: "",
+    authorName: "Tikiii_",
+    authorAvatar: "🥣",
+    originalText: "然然，重庆的流星雨我替你看过啦",
+    translatedText: "익연아, 충칭 유성우 나 대신 봤어",
+    images: ["images/2408-meteor.jpg"],
+    comments: [
+      {
+        id: 191201,
+        username: "周翊然Tz",
+        originalText: "许愿了吗",
+        translatedText: "소원 빌었어?",
+        images: [],
+        actorReply: null
+      }
+    ]
+  }
+];
