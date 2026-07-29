4e49682750822f2148e6684846a0a1dc4136481b
zyr001122 <vazseyo@gmail.com>
2026-07-29 20:18:58 +0900

    Update data-2025-01.js

--- data-2025-01.js
+++ data-2025-01.js
@@ -4,15 +4,15 @@ const posts_202501 = [
     date: "2025-01-22",
     weiboUrl: null,
     authorName: "周翊然Tz",
-    originalText: "周翊然超话 晚上好",
-    translatedText: "周翊然超话 다들 좋은 저녁이에요",
+    originalText: "晚上好",
+    translatedText: "저녁 잘 보내고 있지",
     images: [],
     comments: [
       {
         id: 25010101,
         username: "周翊然Tz",
         originalText: "我要走了各位 假期里做了自己喜欢的事 然后时隔那么长时间 也能再一次陪在家人身边过年 我很开心 希望大家也能开开心心的 和家人们一起迎接新年！",
-        translatedText: "나 이제 가볼게 다들. 휴가 동안 좋아하는 일도 하고, 오랜만에 다시 가족들 곁에서 설을 보낼 수 있어서 정말 좋았어. 다들 즐겁게 가족들이랑 새해 맞이하길 바라!",
+        translatedText: "나 이제 가볼게 얘들아 휴가 기간동안 좋아하는 일도 하고, 오랜만에 가족들 곁에서 같이 설 보낼 수 있어서 너무 좋았어 다들 즐겁게 가족들이랑 좋은 설날 보내!",
         images: [],
         actorReply: null
       },
@@ -20,7 +20,7 @@ const posts_202501 = [
         id: 25010102,
         username: "周翊然Tz",
         originalText: "大家最近有没有什么好听的歌分享一下",
-        translatedText: "요즘 다들 괜찮은 노래 있으면 하나 공유해줘",
+        translatedText: "얘들아 요즘 괜찮은 노래 있으면 공유해줘",
         images: [],
         actorReply: null
       },
@@ -29,35 +29,32 @@ const posts_202501 = [
         username: "魚肉蛋黄酥o_",
         originalText: "你好",
         translatedText: "안녕",
-        images: [],
+        images: ["images/2501-hiru.jpg"],
         actorReply: {
           originalText: "你咋啦",
-          translatedText: "왜 그래"
+          translatedText: "왜 그래 뭔 일이야"
         }
       },
       {
         id: 25010105,
         username: "慕翊婷",
-        originalText: "晚上好呀 开完赛车感觉如何呀",
-        translatedText: "좋은 저녁이야~ 카트레이싱 끝내고 기분 어때?",
-        images: [
-          "images/2501-doge-1.jpg",
-          "images/2501-doge-2.jpg"
-        ],
+        originalText: "晚上好呀 开完赛车感觉如何呀[doge][doge]",
+        translatedText: "좋은 저녁이넹 레이싱 하니까 기분 어땠어?[doge][doge]",
+        images: [],
         actorReply: {
           originalText: "不错",
-          translatedText: "괜찮았어"
+          translatedText: "좋았지"
         }
       },
       {
         id: 25010107,
         username: "十八点Singer_",
         originalText: "棒棒的汪汪已经开始新年旺旺模式咯",
-        translatedText: "멋진 우리 강아지 벌써 새해 왕왕 모드 시작했네",
+        translatedText: "멋쟁이 우리 강아지 벌써 새해 대박 모드 시작했네",
         images: [],
         actorReply: {
           originalText: "什么鬼东西 归我了",
-          translatedText: "이게 뭐야ㅋㅋ 내 거 됐네"
+          translatedText: "이게 뭔데ㅋㅋㅋㅋ 내가 가짐"
         }
       },
       {
@@ -70,42 +67,42 @@ const posts_202501 = [
         ],
         actorReply: {
           originalText: "哈哈哈哈哈哈哈 我真的哈哈哈哈",
-          translatedText: "ㅋㅋㅋㅋㅋㅋㅋ 진짜 개웃기다ㅋㅋㅋㅋ"
+          translatedText: "ㅋㅋㅋㅋㅋㅋㅋ 진짜 미치겠다ㅋㅋㅋㅋ"
         }
       },
       {
         id: 25010111,
         username: "附中刘亦菲TZ-",
         originalText: "然子",
-        translatedText: "란쯔~",
+        translatedText: "란쯔",
         images: [
           "images/2501-lie-flat-sticker.jpg"
         ],
         actorReply: {
           originalText: "咳咳",
-          translatedText: "흠흠"
+          translatedText: "크흠"
         }
       },
       {
         id: 25010113,
         username: "1点大王",
         originalText: "我也想开赛车",
-        translatedText: "나도 카트레이싱 하고 싶다",
+        translatedText: "나도 레이싱 하고 싶다",
         images: [],
         actorReply: {
           originalText: "走",
-          translatedText: "가자"
+          translatedText: "ㄱㄱ"
         }
       },
       {
         id: 25010115,
         username: "祈川十里春",
         originalText: "吃完饭了没",
-        translatedText: "밥 다 먹었어?",
+        translatedText: "밥은 다 먹었어?",
         images: [],
         actorReply: {
           originalText: "我也吃了火锅",
-          translatedText: "나도 훠궈 먹었어"
+          translatedText: "나도 훠궈 먹었는데"
         }
       },
       {
@@ -123,7 +120,7 @@ const posts_202501 = [
         id: 25010119,
         username: "麦麦_Tz",
         originalText: "然然评价一下",
-        translatedText: "然然아 평가 좀 해줘",
+        translatedText: "익연아 평가 좀 해줘",
         images: [
           "images/2501-fan-drawing.jpg"
         ],
@@ -135,19 +132,19 @@ const posts_202501 = [
       {
         id: 25010121,
         username: "十八点Singer_",
-        originalText: "走吧小狗 周翊然是不会注意到我们的",
-        translatedText: "가자 강아지들아, 周翊然은 우릴 신경도 안 써줄 거야",
+        originalText: "走吧小狗 周翊然是不会注意到我们的👩🏻‍🦯       🦮🐕‍🦺 ",
+        translatedText: "가자 멍멍아, 주익연은 우리 안중에도 없을 거야👩🏻‍🦯       🦮🐕‍🦺 ",
         images: [],
         actorReply: {
-          originalText: "images/2501-tz-sticker-1.jpg",
-          translatedText: "images/2501-tz-sticker-1.jpg"
+          originalText: "images/2501-dogrun.gif",
+          translatedText: "images/22501-dogrun-1.gif"
         }
       },
       {
         id: 25010123,
         username: "我衹是喜歡一頭可愛豬又有什麼錯",
         originalText: "看看这是啥",
-        translatedText: "이게 뭔지 한번 봐봐",
+        translatedText: "이거 뭐게",
         images: [
           "images/2501-thinking-photo.jpg"
         ],
@@ -160,13 +157,13 @@ const posts_202501 = [
         id: 25010125,
         username: "liu豆豆子_",
         originalText: "我的新年战壳",
-        translatedText: "내 새해맞이 폰 케이스",
+        translatedText: "내 새해맞이 전투용 폰케이스",
         images: [
           "images/2501-phonecase.jpg"
         ],
         actorReply: {
           originalText: "金色的很好看",
-          translatedText: "골드색 예쁘네"
+          translatedText: "골드색 예쁘다"
         }
       },
       {
@@ -174,10 +171,10 @@ const posts_202501 = [
         username: "拜托了哥哥酱",
         originalText: "我的心好痛",
         translatedText: "마음이 너무 아파",
-        images: [],
+        images: ["2501-quuu"],
         actorReply: {
-          originalText: "images/2501-tz-sticker-2.jpg",
-          translatedText: "images/2501-tz-sticker-2.jpg"
+          originalText: "images/2501-quuu-reply.jpg",
+          translatedText: "images/2501-quuu-reply.jpg"
         }
       },
       {
@@ -187,15 +184,15 @@ const posts_202501 = [
         translatedText: "오빠 무슨 고민 있어?",
         images: [],
         actorReply: {
-          originalText: "images/2501-tz-sticker-3.jpg",
-          translatedText: "images/2501-tz-sticker-3.jpg"
+          originalText: "images/2501-tz-sticker-3.gif",
+          translatedText: "images/2501-tz-sticker-3.gif"
         }
       },
       {
         id: 25010131,
         username: "我产是世界第yi_",
         originalText: "许愿新的一年开车哥能天天开赛车",
-        translatedText: "새해엔 드라이빙카 오빠가 매일 카트레이싱 하게 해달라고 소원 빌게",
+        translatedText: "새해엔 우리 레이서 오빠가 매일레이싱 탈 수 있게 해주세요",
         images: [],
         actorReply: {
           originalText: "images/2501-tz-sticker-4.jpg",
@@ -206,11 +203,11 @@ const posts_202501 = [
         id: 25010133,
         username: "猫卷眠眠然",
         originalText: "不翻我 我直接皇化",
-        translatedText: "나 안 넘겨주면 바로 폭주할 거야",
+        translatedText: "나 답글 안 달아주면 바로 흑화한다",
         images: [],
         actorReply: {
           originalText: "哈哈哈哈哈哈哈哈哈什么鬼",
-          translatedText: "ㅋㅋㅋㅋㅋㅋㅋㅋㅋ 뭐야 이게"
+          translatedText: "ㅋㅋㅋㅋㅋㅋㅋㅋㅋ이건 뭔데 미치겠넼"
         }
       },
       {
@@ -223,18 +220,16 @@ const posts_202501 = [
         ],
         actorReply: {
           originalText: "我猜他是奶龙",
-          translatedText: "쟤 나이룽(奶龙) 아닐까"
+          translatedText: "쟤 나이룽 같은데"
         }
       },
       {
         id: 25010137,
         username: "陪我淋一场雨·",
         originalText: "这个壁纸眼熟吗",
-        translatedText: "이 배경화면 낯익지 않아?",
+        translatedText: "배경화면 낯익지 않아?",
         images: [
-          "images/2501-wallpaper-1.jpg",
-          "images/2501-wallpaper-2.jpg"
-        ],
+          "images/2501-wallpaper-1.jpg"],
         actorReply: {
           originalText: "模仿我",
           translatedText: "나 따라했네"
@@ -244,53 +239,53 @@ const posts_202501 = [
         id: 25010139,
         username: "我就是周六栗子日",
         originalText: "周翊然会翻我这件事假面骑士、假面骑士v3、假面骑士X、假面骑士亚马逊、假面骑士强人、假面骑士sky rider、假面骑士超一号、假面骑士暗日、假面骑士空我、假面骑士亚极陀、假面骑士龙骑、假面骑士555、假面骑士剑、假面骑士响鬼、假面骑士the first等诸多假面骑士都觉得很赞",
-        translatedText: "周翊然이 내 이 글도 넘겨준 적 있어. 가면라이더, 가면라이더 V3, 가면라이더 X, 가면라이더 아마존, 가면라이더 스트롱거, 가면라이더 스카이라이더, 가면라이더 슈퍼원, 가면라이더 블랙, 가면라이더 쿠우가, 가면라이더 아기토, 가면라이더 류우키, 가면라이더 555, 가면라이더 켄, 가면라이더 히비키, 가면라이더 더 퍼스트 등 수많은 가면라이더 팬들도 다 멋지다고 생각할 거야",
+        translatedText: "주익연이 내 댓글에 답글 달아줄 거라는 사실은 가면라이더, 가면라이더 V3, 가면라이더 X, 가면라이더 아마존, 가면라이더 스트롱거, 가면라이더 스카이라이더, 가면라이더 슈퍼원, 가면라이더 블랙, 가면라이더 쿠우가, 가면라이더 아기토, 가면라이더 류우키, 가면라이더 555, 가면라이더 켄, 가면라이더 히비키, 가면라이더 더 퍼스트 등 수많은 가면라이더들도 인정하는 바임",
         images: [],
         actorReply: {
           originalText: "给555的",
-          translatedText: "555용으로 준 거야"
+          translatedText: "555한테 주는 답"
         }
       },
       {
         id: 25010141,
         username: "-融雪初岛-",
         originalText: "问问小然年夜饭都吃什么 提前祝你小年快乐！",
-        translatedText: "小然한테 물어볼게, 연야반 뭐 먹었어? 미리 소년 축하해!",
+        translatedText: "익연이한테 물어볼게, 연야반 뭐 먹을 거야? 미리 설날 잘 보내!",
         images: [],
         actorReply: {
           originalText: "小年快乐！ 到时候拍给你们看看",
-          translatedText: "소년 축하해! 그때 되면 찍어서 보여줄게"
+          translatedText: "설날 잘 보내! 그때 사진 찍어서 보여줄게"
         }
       },
       {
         id: 25010143,
         username: "Moonear__",
         originalText: "支持我过年做这个美甲吗💅",
-        translatedText: "설날에 이 네일아트 해도 괜찮을까?💅",
+        translatedText: "설날에 이 네일아트 하는 거 찬성?💅",
         images: [
           "images/2501-nail.jpg"
         ],
         actorReply: {
           originalText: "哈哈哈哈哈哈哈哈 这做了跑步很快吧",
-          translatedText: "ㅋㅋㅋㅋㅋㅋㅋㅋ 이거 하면 달리기 빨라지겠다"
+          translatedText: "ㅋㅋㅋㅋㅋㅋㅋㅋ 이거 하면 달리기 엄청 빨라지겠다"
         }
       },
       {
         id: 25010145,
         username: "遇見小翊",
         originalText: "然总，看我家猫会后空翻法拉利会吗？？？",
-        translatedText: "然총, 우리 집 고양이가 페라리 뒤로 공중제비 도는 거 볼래???",
+        translatedText: "란사장님, 우리 집 고양이 백덤블링 할 줄 아는데 페라리도 할 줄 알아?",
         images: [],
         actorReply: {
           originalText: "你发给我看看",
-          translatedText: "나한테 보내봐"
+          translatedText: "영상 보여줘"
         }
       },
       {
         id: 25010147,
         username: "然而醒_",
         originalText: "今年茅台有没有资格带回家过年啊",
-        translatedText: "올해는 마오타이 집에 가져갈 자격 있어?",
+        translatedText: "올해는 마오타이 집에 데려가서 설 보낼 수 있어?",
         images: [],
         actorReply: {
           originalText: "必须的",
@@ -301,22 +296,22 @@ const posts_202501 = [
         id: 25010149,
         username: "小周喜欢过秋天",
         originalText: "今天是什么顺序翻牌？能翻到我不！",
-        translatedText: "오늘은 무슨 순서로 넘기는 거야? 나도 넘겨줄 수 있어?",
-        images: [],
+        translatedText: "오늘은 무슨 순서로 답글 달아주는 거야? 나도 받을 수 없을까!",
+        images: ["2501-tz-sticker-5"],
         actorReply: {
-          originalText: "images/2501-tz-sticker-5.jpg",
-          translatedText: "images/2501-tz-sticker-5.jpg"
+          originalText: "images/2501-tz-sticker-8.gif",
+          translatedText: "images/2501-tz-sticker-5.gif"
         }
       },
       {
         id: 25010151,
         username: "只想看小渝_",
         originalText: "请问这位赛车手，能否让我坐你的副驾驶",
-        translatedText: "저기 레이서님, 조수석에 태워주실 수 있나요",
+        translatedText: "저기 레이서님, 조수석에 태워주실 수 있을까여",
         images: [],
         actorReply: {
           originalText: "赛车都没有副驾驶",
-          translatedText: "카트레이싱카엔 조수석이 없어"
+          translatedText: "레이싱카엔 조수석 없어"
         }
       }
     ]
