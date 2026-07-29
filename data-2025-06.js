6cc351b9490c43e1a20c22b827bb666cdadb60b9
zyr001122 <vazseyo@gmail.com>
2026-07-30 02:05:56 +0900

    Add files via upload

---
+++ data-2025-06.js
@@ -0,0 +1,35 @@
+const posts_202506 = [
+  {
+    id: 250601,
+    date: "2025-06-22",
+    weiboUrl: "https://weibo.com/5994946077/PxJXr1rMn",
+    authorName: "周翊然Tz",
+    originalText: "Hi 我来了 是不是在看焕羽了",
+    translatedText: "Hi 나 왔어 다들 焕羽 보고 있는 거 아니야?",
+    images: [],
+    comments: [
+      {
+        id: 25060101,
+        username: "vm_1122",
+        originalText: "看来拖鞋有点不合脚[偷乐]",
+        translatedText: "슬리퍼가 좀 안 맞나 보네[偷乐]",
+        images: ["images/2506-slippers.jpg"],
+        actorReply: {
+          originalText: "明盛不要面子的吗？",
+          translatedText: "명성(明盛)은 체면 안 차리나?"
+        }
+      },
+      {
+        id: 25060102,
+        username: "只吃两碗饭-",
+        originalText: "老公你知道法拉利在焕羽第几集出现吗？！🤨",
+        translatedText: "자기야 페라리가 焕羽 몇 화에 나오는지 알아?!🤨",
+        images: ["images/2506-ferrari-episode.jpg"],
+        actorReply: {
+          originalText: "下周就来！",
+          translatedText: "다음 주에 나와!"
+        }
+      }
+    ]
+  }
+];
