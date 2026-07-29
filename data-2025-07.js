862dcb20ab209c841c4fef9cbc946db69b5a78cd
zyr001122 <vazseyo@gmail.com>
2026-07-30 02:26:25 +0900

    Add files via upload

---
+++ data-2025-07.js
@@ -0,0 +1,257 @@
+const posts_202507 = [
+  {
+    id: 250701,
+    date: "2025-07-22",
+    weiboUrl: "https://weibo.com/5994946077/PCiQLxJgc",
+    authorName: "周翊然Tz",
+    originalText: "hello",
+    translatedText: "hello",
+    images: [],
+    comments: [
+      {
+        id: 25070102,
+        username: "周翊然Tz",
+        originalText: "最近喜欢听的歌",
+        translatedText: "요즘 즐겨 듣는 노래",
+        images: ["images/2507-song.jpg"],
+        actorReply: null
+      },
+      {
+        id: 25070103,
+        username: "周翊然Tz",
+        originalText: "小玩一下 哈哈哈哈哈",
+        translatedText: "그냥 좀 해봤어 ㅋㅋㅋㅋㅋ",
+        images: ["images/2507-playing-around.jpg"],
+        actorReply: null
+      },
+      {
+        id: 25070104,
+        username: "Dear-喇巴粥",
+        originalText: "周翊然准备好迎接来自法总的宠爱了吗？",
+        translatedText: "주익연 파라(法) 총재님의 사랑 받을 준비 됐어?",
+        images: ["images/2507-fa-pampering.jpg"],
+        actorReply: {
+          originalText: "感觉我家的那两位已被玩坏了[doge]",
+          translatedText: "우리 집 그 둘 이미 갖고 놀다가 망가진 것 같아[doge]"
+        }
+      },
+      {
+        id: 25070105,
+        username: "熙熙果冻-Tz",
+        originalText: "看到小财神要说什么",
+        translatedText: "재물신 보면 뭐라고 해야 하나",
+        images: ["images/2507-money-god.jpg"],
+        actorReply: {
+          originalText: "说啥啊",
+          translatedText: "뭘 말해"
+        }
+      },
+      {
+        id: 25070106,
+        username: "摇号吖",
+        originalText: "",
+        translatedText: "",
+        images: ["images/2507-mention-only.jpg"],
+        actorReply: {
+          originalText: "谢谢你的喜欢啊 我也很荣幸能够在精神上给到你帮助 希望你未来一切顺利 天天开心[心]",
+          translatedText: "좋아해줘서 고마워 나도 정신적으로 너한테 도움이 될 수 있어서 영광이야 앞으로 다 잘 되고 매일매일 행복하길 바라[心]"
+        }
+      },
+      {
+        id: 25070107,
+        username: "蛋蛋大王_Eeeeg",
+        originalText: "让我看看这个小然然在干嘛 来这么晚！",
+        translatedText: "이 작은 익연이 뭐 하고 있나 좀 보자 왜 이렇게 늦게 왔어!",
+        images: ["images/2507-so-late.jpg"],
+        actorReply: {
+          originalText: "我才放饭",
+          translatedText: "나 이제야 밥 먹으러 가"
+        }
+      },
+      {
+        id: 25070108,
+        username: "七十七小狗",
+        originalText: "哥哥你说实话 自己笑了没[doge]",
+        translatedText: "오빠 솔직히 말해봐 자기가 봐도 웃겨?[doge]",
+        images: ["images/2507-drawing.jpg"],
+        actorReply: {
+          originalText: "画得不好吗",
+          translatedText: "그림 잘 못 그렸어?"
+        }
+      },
+      {
+        id: 25070109,
+        username: "樱花味的cherry",
+        originalText: "画的时候什么想法？",
+        translatedText: "그릴 때 무슨 생각했어?",
+        images: ["images/2507-drawing2.jpg"],
+        actorReply: {
+          originalText: "凭着感觉走[doge]",
+          translatedText: "그냥 느낌 가는 대로[doge]"
+        }
+      },
+      {
+        id: 25070110,
+        username: "咬一口脆皮年糕_",
+        originalText: "停停停 先看看这个 喜欢不",
+        translatedText: "잠깐만 잠깐만 이거 먼저 봐봐, 마음에 들어?",
+        images: ["images/2507-like-this.jpg"],
+        actorReply: {
+          originalText: "好看！",
+          translatedText: "예쁘다!"
+        }
+      },
+      {
+        id: 25070111,
+        username: "十六TZ",
+        originalText: "宝宝哪个好看",
+        translatedText: "자기야 어느 게 더 예뻐?",
+        images: ["images/2507-which-prettier.jpg"],
+        actorReply: {
+          originalText: "都很好看",
+          translatedText: "다 예뻐"
+        }
+      },
+      {
+        id: 25070112,
+        username: "沐兮永远爱周翊然",
+        originalText: "欧巴 求巧克力馒头教程🙈",
+        translatedText: "오빠 초콜릿 만터우 레시피 알려줘🙈",
+        images: ["images/2507-choco-mantou.jpg"],
+        actorReply: {
+          originalText: "哈哈哈哈哈哈哈哈哈哈哈哈哈哈",
+          translatedText: "ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ"
+        }
+      },
+      {
+        id: 25070113,
+        username: "Kaimi_liao",
+        originalText: "能不能让我当幸福的人❤️ 翻我好吗",
+        translatedText: "나도 행복한 사람 되게 해주면 안 돼?❤️ 답글 좀 달아줘",
+        images: ["images/2507-flip-me.jpg"],
+        actorReply: {
+          originalText: "好看 用什么画的啊",
+          translatedText: "예쁘다 뭘로 그린 거야?"
+        }
+      },
+      {
+        id: 25070114,
+        username: "吃块小cake_",
+        originalText: "哥哥你不觉得这六个巧克力馒头出锅的时候很像六块腹肌吗",
+        translatedText: "오빠 이 초콜릿 만터우 6개 쪄놓으니까 식스팩 같지 않아?",
+        images: ["images/2507-choco-mantou.jpg"],
+        actorReply: {
+          originalText: "?",
+          translatedText: "?"
+        }
+      },
+      {
+        id: 25070115,
+        username: "小圆肚子啦啦啦",
+        originalText: "哥，想看你拍的彩虹🌈给我带来好运",
+        translatedText: "오빠, 오빠가 찍은 무지개🌈 보고 싶어 나한테 행운 좀 가져다주게",
+        images: ["images/2507-rainbow-request.jpg"],
+        actorReply: {
+          originalText: "其实我发过了",
+          translatedText: "사실 나 이미 올린 적 있어"
+        }
+      },
+      {
+        id: 25070116,
+        username: "偷吃一口饭饭",
+        originalText: "老公你看这是什么",
+        translatedText: "자기야 이거 봐봐 이게 뭐야?",
+        images: ["images/2507-look-this.jpg"],
+        actorReply: {
+          originalText: "开赛车的人啊",
+          translatedText: "레이싱 하는 사람이지"
+        }
+      },
+      {
+        id: 25070117,
+        username: "然然的大梦想家",
+        originalText: "帅不帅[送花花]",
+        translatedText: "멋있지 않아?[送花花]",
+        images: ["images/2507-handsome.jpg"],
+        actorReply: {
+          originalText: "有点意思",
+          translatedText: "좀 재밌네"
+        }
+      },
+      {
+        id: 25070118,
+        username: "甜粥软乎乎Tz",
+        originalText: "这是哪个小朋友送的呀[并不简单]",
+        translatedText: "이거 어느 꼬마가 준 거야?[并不简单]",
+        images: ["images/2507-gift-from-kid.jpg"],
+        actorReply: {
+          originalText: "这是我给小朋友的回礼 ！",
+          translatedText: "이건 내가 그 꼬마한테 준 답례품이야!"
+        }
+      },
+      {
+        id: 25070119,
+        username: "Cocooo---",
+        originalText: "开车哥能不能帮我考科目二[期待][期待][期待]",
+        translatedText: "레이서 오빠 나 2종 시험 도와줄 수 있어?[期待][期待][期待]",
+        images: ["images/2507-driving-test.jpg"],
+        actorReply: {
+          originalText: "这不很简单吗",
+          translatedText: "이거 완전 쉬운 거 아니야?"
+        }
+      },
+      {
+        id: 25070120,
+        username: "眠宀宀",
+        originalText: "看来今天片场网不错 但是就是回不了我[失望]",
+        translatedText: "오늘 촬영장 와이파이 괜찮은가 본데 근데 나한테는 답을 안 해주네[失望]",
+        images: [],
+        actorReply: {
+          originalText: "能啊 能回",
+          translatedText: "돼 답장 가능해"
+        }
+      },
+      {
+        id: 25070121,
+        username: "蛋蛋大王_Eeeeg",
+        originalText: "老公火鸡面吃不？",
+        translatedText: "자기야 불닭볶음면 먹을 거야?",
+        images: ["images/2507-ramen.jpg"],
+        actorReply: {
+          originalText: "不了不了",
+          translatedText: "안 먹어 안 먹어"
+        }
+      },
+      {
+        id: 25070122,
+        username: "那就九月见吧",
+        originalText: "哥哥选一个回答我",
+        translatedText: "오빠 하나 골라서 대답해줘",
+        images: ["images/2507-pick-one.jpg"],
+        actorReply: {
+          originalText: "那肯定得赛车啊",
+          translatedText: "그야 당연히 레이싱이지"
+        }
+      },
+      {
+        id: 25070123,
+        username: "我的小狗会吐舌",
+        originalText: "酵父你相信光吗",
+        translatedText: "형 빛을 믿어?",
+        images: [],
+        actorReply: {
+          originalText: "抢天光算吗",
+          translatedText: "새벽에 먼저 일어나는 것도 믿는 거로 쳐줄래?"
+        }
+      },
+      {
+        id: 25070101,
+        username: "周翊然Tz",
+        originalText: "我要走了  大家最近天气热 一定要注意不要中暑了 我去改妆了 下次见 ！",
+        translatedText: "나 이제 갈게 요즘 날씨 더우니까 다들 더위 먹지 않게 조심해 나 분장 고치러 가야 해서 다음에 봐!",
+        images: [],
+        actorReply: null
+      }
+    ]
+  }
+];
