4d0ef5a4da816197c4023903065cada50d54aac0
zyr001122 <vazseyo@gmail.com>
2026-07-29 10:26:10 +0900

    Add files via upload

---
+++ data-2024-11.js
@@ -0,0 +1,15 @@
+const posts_202411 = [
+  {
+    id: 241101,
+    date: "2024-11-22",
+    weiboUrl: "https://weibo.com/5994946077/P1wtptzys",
+    originalText: "#本周报备# 见面",
+    translatedText: "#本周报备# 만남",
+    images: [
+      "images/2411-nov22-1.jpg",
+      "images/2411-nov22-2.jpg",
+      "images/2411-nov22-3.jpg"
+    ],
+    comments: []
+  }
+];
