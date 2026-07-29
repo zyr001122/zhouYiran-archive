f949ccebeb094cfa1917a3a8bc49c9462588b300
zyr001122 <vazseyo@gmail.com>
2026-07-29 15:09:31 +0900

    Update data-2024-09.js

--- data-2024-09.js
+++ data-2024-09.js
@@ -4,48 +4,48 @@ const posts_202409 = [
     date: "2024-09-22",
     weiboUrl: "https://weibo.com/5994946077/Oyd4jrTNy",
     originalText: "来了各位",
-    translatedText: "다들 왔어",
+    translatedText: "얘들아 나 왔어",
     images: [],
     comments: [
       {
         id: 200101,
         username: "你蝶太困了",
         originalText: "诶哟那你今天要几点下班",
-        translatedText: "어머 그럼 오늘 몇 시에 퇴근해",
+        translatedText: "아이구 그럼 오늘 몇 시에 퇴근해?",
         images: [],
-        actorReply: { originalText: "你睡后🤔", translatedText: "너 자고 나서🤔" }
+        actorReply: { originalText: "你睡后[思考]", translatedText: "너 자러가면[思考]" }
       },
       {
         id: 200102,
         username: "帽奢",
         originalText: "吃饭了吗",
-        translatedText: "밥 먹었어?",
+        translatedText: "밥은 먹었어?",
         images: [],
-        actorReply: { originalText: "吃了一包辣条", translatedText: "라조우 한 봉지 먹었어" }
+        actorReply: { originalText: "吃了一包辣条", translatedText: "라티아오 한 봉지 먹었어" }
       },
       {
         id: 200103,
         username: "翊条魚干",
-        originalText: "宝宝看小跑车。😭",
-        translatedText: "자기야 작은 레이싱카 봐봐😭",
+        originalText: "宝宝看小跑车。[泪]",
+        translatedText: "익연아 미니 레이싱카 봐봐[泪]",
         images: ["images/2409-racing.jpg"],
-        actorReply: { originalText: "这颜色一看就是法拉利", translatedText: "이 색깔 한눈에 봐도 페라리네" }
+        actorReply: { originalText: "这颜色一看就是法拉利", translatedText: "이거 딱 색깔만 봐도 페라리네" }
       },
       {
         id: 200104,
         username: "源屿子瑜",
         originalText: "宝宝！！！！！！今天去拿茶百道哦海报了！！！还跟你打卡了！！！！！",
-        translatedText: "자기야!!!!!! 오늘 차백도 포스터 받으러 갔어!!! 너랑 같이 인증도 했어!!!!!",
+        translatedText: "익연아!!!!!! 오늘 차백도 포스터 받으러 갔어!!! 너랑 같이 인증샷도 찍었엉!!!!!",
         images: ["images/2409-chabaidao.jpg"],
-        actorReply: { originalText: "周边能送我吗", translatedText: "굿즈 나한테 보내줄 수 있어?" }
+        actorReply: { originalText: "周边能送我吗", translatedText: "굿즈는 나 주면 안 돼?" }
       },
       {
         id: 200105,
         username: "谷光_依然29",
         originalText: "你不休息 我不睡了 明天不上學！",
-        translatedText: "네가 안 쉬면 나도 안 자 내일 학교 안 가!",
+        translatedText: "네가 안 쉬면 나도 안 자 내일 학교 안 갈 거야!",
         images: [],
-        actorReply: { originalText: "可别 睡觉是一件非常幸福的事", translatedText: "그러지 마 자는 건 엄청 행복한 일이야" }
+        actorReply: { originalText: "可别 睡觉是一件非常幸福的事", translatedText: "그러지 마 잘 수 있는 건 엄청 행복한 일이라고" }
       },
       {
         id: 200106,
@@ -61,21 +61,21 @@ const posts_202409 = [
         originalText: "带你兜风",
         translatedText: "드라이브 시켜줄게",
         images: ["images/2409-car-drive.jpg"],
-        actorReply: { originalText: "多少马力", translatedText: "몇 마력이야" }
+        actorReply: { originalText: "多少马力", translatedText: "몇 마력짜리야" }
       },
       {
         id: 200108,
         username: "是我的小土豆-Tz",
         originalText: "今天练完车的美景",
-        translatedText: "오늘 차 연습하고 나서 찍은 야경",
+        translatedText: "오늘 운전 연습 끝나고 찍은 풍경",
         images: ["images/2409-night-view.jpg"],
-        actorReply: { originalText: "赛车吗", translatedText: "레이싱카야?" }
+        actorReply: { originalText: "赛车吗", translatedText: "레이싱카?" }
       },
       {
         id: 200109,
         username: "周翊然Tz",
-        originalText: "才拍完一个组现在去另一个组🧐",
-        translatedText: "방금 한 팀 촬영 끝내고 이제 다른 팀으로 가는 중🧐",
+        originalText: "才拍完一个组现在去另一个组[并不简单]",
+        translatedText: "방금 한 팀 촬영 끝내고 이제 다른 팀으로 가는 중[并不简单]",
         images: [],
         actorReply: null
       },
@@ -83,7 +83,7 @@ const posts_202409 = [
         id: 200110,
         username: "周翊然Tz",
         originalText: "新的狗子是个妹妹 叫茅台 有人问我我没说 就是感觉……",
-        translatedText: "새 강아지는 여자애야 이름은 마오타이 누가 물어봤는데 말 안 했어 그냥 느낌이……",
+        translatedText: "새 강아지는 여자애야 이름은 마오타이 누가 물어봤는데 말 안 했어 그냥 뭔가 느낌이……",
         images: [],
         actorReply: null
       },
@@ -91,7 +91,7 @@ const posts_202409 = [
         id: 200111,
         username: "周翊然Tz",
         originalText: "为啥叫茅台 这是一个无厘头的想法",
-        translatedText: "왜 마오타이냐고 그냥 뜬금없이 생각난 이름이야",
+        translatedText: "왜 마오타이냐고? 그냥 뜬금없이 생각난 이름이야",
         images: [],
         actorReply: null
       },
@@ -99,7 +99,7 @@ const posts_202409 = [
         id: 200112,
         username: "周翊然Tz",
         originalText: "上班去了各位 大家早点睡觉！",
-        translatedText: "다들 일하러 갈게 일찍 자!",
+        translatedText: "나 일하러 갈게 다들 일찍 자!",
         images: [],
         actorReply: null
       }
