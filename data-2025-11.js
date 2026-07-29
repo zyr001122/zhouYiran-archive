508dd866c3540b28981ffdaa77f78afc60430348
zyr001122 <vazseyo@gmail.com>
2026-07-30 04:20:29 +0900

    Add files via upload

---
+++ data-2025-11.js
@@ -0,0 +1,213 @@
+const posts_202511 = [
+  {
+    id: 251101,
+    date: "2025-11-22",
+    weiboUrl: "https://weibo.com/5994946077/Qf2M0uKrh",
+    authorName: "周翊然Tz",
+    originalText: "我又来了",
+    translatedText: "나 또 왔어",
+    note: "익연이 생일 (25세)",
+    images: [],
+    comments: [
+      {
+        id: 25110101,
+        username: "是我的小土豆-Tz",
+        originalText: "然然 我今天也过生日！🎂想要你的祝福～",
+        translatedText: "익연아 나도 오늘 생일이야! 🎂 네 축하 받고 싶어~",
+        images: ["images/2511-birthday-too.jpg"],
+        actorReply: {
+          originalText: "生日快乐！",
+          translatedText: "생일 축하해!"
+        }
+      },
+      {
+        id: 25110102,
+        username: "樱花味的cherry",
+        originalText: "哥哥生日快乐！每天都快乐！我的愿望就是你许下的愿望都能实现！要一直一直幸福！",
+        translatedText: "오빠 생일 축하해! 매일매일 행복해! 내 소원은 오빠가 빈 소원이 다 이루어지는 거야! 계속계속 행복해야 해!",
+        images: ["images/2511-my-wish-1.jpg", "images/2511-my-wish-2.jpg", "images/2511-my-wish-3.jpg"],
+        actorReply: {
+          originalText: "❤️",
+          translatedText: "❤️"
+        }
+      },
+      {
+        id: 25110103,
+        username: "心动壹年",
+        originalText: "今天去鸡鸣寺替你祈福了！25岁一定要顺利！",
+        translatedText: "오늘 지밍쓰(鸡鸣寺) 가서 너 대신 기도하고 왔어! 25살 꼭 순조롭게 보내!",
+        images: ["images/2511-temple-prayer.jpg"],
+        actorReply: {
+          originalText: "也记得给自己祈福一下！",
+          translatedText: "너도 자신을 위해 기도하는 거 잊지 마!"
+        }
+      },
+      {
+        id: 25110104,
+        username: "粥牛牛不会流鼻涕",
+        originalText: "宝宝你前几天去了新加坡 能不能推荐给我你认为最好吃的美食！🥺",
+        translatedText: "자기야 너 며칠 전에 싱가포르 갔었잖아 네가 생각하기에 제일 맛있었던 음식 하나 추천해줄 수 있어?🥺",
+        images: ["images/2511-singapore-food.jpg"],
+        actorReply: {
+          originalText: "其实我觉得那个茶还挺好喝的 肉骨茶？",
+          translatedText: "사실 그 차 꽤 맛있었던 것 같아 박쿠테(肉骨茶)?"
+        }
+      },
+      {
+        id: 25110105,
+        username: "想吃荔枝冰_",
+        originalText: "希望有机会我们赛道上见好吗",
+        translatedText: "기회 되면 우리 서킷에서 만나자, 어때?",
+        images: ["images/2511-see-you-track.jpg"],
+        actorReply: {
+          originalText: "来真的？",
+          translatedText: "진심이야?"
+        }
+      },
+      {
+        id: 25110106,
+        username: "小圆肚子啦啦啦",
+        originalText: "老公其实我想说，你画的帽子像袜子😏",
+        translatedText: "자기야 사실 하고 싶은 말이 있는데, 네가 그린 모자 양말 닮았어😏",
+        images: ["images/2511-hat-like-socks.jpg"],
+        actorReply: {
+          originalText: "？",
+          translatedText: "?"
+        }
+      },
+      {
+        id: 25110107,
+        username: "-许愿Tz",
+        originalText: "宝宝我可以给别人看面相 一看你就是当官的料 你猜猜是什么官呀？",
+        translatedText: "자기야 나 관상 좀 볼 줄 아는데, 딱 보니까 너는 관직에 어울리는 상이야. 무슨 관직일지 맞혀볼래?",
+        images: ["images/2511-official-face.jpg"],
+        actorReply: {
+          originalText: "新郎官？",
+          translatedText: "신랑관(新郎官)?"
+        },
+        note: "'官'으로 라임 맞춘 말장난 - 신랑관은 '신랑'을 뜻함"
+      },
+      {
+        id: 25110108,
+        username: "魚肉蛋黄酥o_",
+        originalText: "来来来 给你找的耳朵痛感图，你参考一下吧，其实耳垂不是很疼的！！！看见谢燕来戴耳钉的造型我觉得超级好看！",
+        translatedText: "자 이거 봐봐, 귀 아픈 정도 보여주는 그림 찾아왔어, 참고해봐. 사실 귓불은 그렇게 안 아파!!! 谢燕来가 귀걸이 한 스타일 보니까 완전 예쁘더라!",
+        images: ["images/2511-ear-pain-chart.jpg"],
+        actorReply: {
+          originalText: "谢谢您",
+          translatedText: "고마워요"
+        }
+      },
+      {
+        id: 25110109,
+        username: "-翊雨TZ-",
+        originalText: "然然 你能不能把笑笑送给我",
+        translatedText: "익연아 '샤오샤오' 나한테 줄 수 있어?",
+        images: ["images/2511-give-me-xiaoxiao.jpg"],
+        actorReply: {
+          originalText: "肯定不行啊",
+          translatedText: "당연히 안 되지"
+        }
+      },
+      {
+        id: 25110110,
+        username: "扁桃体发炎不发言-",
+        originalText: "这次在新加坡和印尼有吃到好吃的吗，这个鱼看着不错呀 是辣的吗",
+        translatedText: "이번에 싱가포르랑 인도네시아에서 맛있는 거 먹었어? 이 생선 맛있어 보이는데 매운 맛이야?",
+        images: ["images/2511-spicy-fish.jpg"],
+        actorReply: {
+          originalText: "其实都是那种咖喱味多一点",
+          translatedText: "사실 다 카레 맛이 좀 강한 편이야"
+        }
+      },
+      {
+        id: 25110111,
+        username: "扁桃体发炎不发言-",
+        originalText: "哥哥 这个车车好玩吗！",
+        translatedText: "오빠 이 자동차 재밌어?!",
+        images: ["images/2511-race-car.jpg"],
+        actorReply: {
+          originalText: "速度太慢了",
+          translatedText: "속도가 너무 느려"
+        }
+      },
+      {
+        id: 25110112,
+        username: "争争翊上Tz",
+        originalText: "老公 你怎么冒鼻涕泡了🥶",
+        translatedText: "자기야 너 왜 콧물 방울 났어🥶",
+        images: ["images/2511-runny-nose.jpg"],
+        actorReply: {
+          originalText: "冬天到了",
+          translatedText: "겨울이 왔네"
+        }
+      },
+      {
+        id: 25110113,
+        username: "翊点点喜欢_楠",
+        originalText: "哥哥给你看我的耳洞",
+        translatedText: "오빠 내 귀 뚫은 거 보여줄게",
+        images: ["images/2511-ear-piercing-1.jpg", "images/2511-ear-piercing-2.jpg"],
+        actorReply: {
+          originalText: "打的时候疼不",
+          translatedText: "뚫을 때 아팠어?"
+        }
+      },
+      {
+        id: 25110114,
+        username: "翊碗烤冷面",
+        originalText: "哥哥认真的吗",
+        translatedText: "오빠 진심이야?",
+        images: ["images/2511-are-you-serious.jpg"],
+        actorReply: {
+          originalText: "咋了？",
+          translatedText: "왜?"
+        }
+      },
+      {
+        id: 25110115,
+        username: "周翊然Tz",
+        originalText: "此时此刻去完成25岁的第一件事了",
+        translatedText: "지금 이 순간 25살의 첫 번째 일을 하러 간다",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 25110116,
+        username: "甜粥软乎乎Tz",
+        originalText: "新做的好看吗",
+        translatedText: "새로 한 거 예쁘지 않아?",
+        images: ["images/2511-new-look.jpg"],
+        actorReply: {
+          originalText: "好看",
+          translatedText: "예뻐"
+        }
+      },
+      {
+        id: 25110117,
+        username: "周翊然Tz",
+        originalText: "感受到了满满的爱 来自全世界的 非常的开心 也很感谢大家 我们明年见！",
+        translatedText: "가득한 사랑을 느꼈어, 전 세계에서 보내준 사랑. 정말 행복하고 다들 너무 고마워. 우리 내년에 봐!",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 25110118,
+        username: "周翊然Tz",
+        originalText: "我走啦 各位 冬天记得保暖 生日很开心 ❤️❤️❤️",
+        translatedText: "나 갈게 다들 겨울에 따뜻하게 입어 생일 정말 행복했어 ❤️❤️❤️",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 25110119,
+        username: "周翊然Tz",
+        originalText: "",
+        translatedText: "",
+        note: "텍스트 없이 사진만 올린 댓글",
+        images: ["images/2511-birthday-photo.jpg"],
+        actorReply: null
+      }
+    ]
+  }
+];
