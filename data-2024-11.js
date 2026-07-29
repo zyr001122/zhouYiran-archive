5f2b431fa0d3d88f266aec6a76752528aaaf1cce
zyr001122 <vazseyo@gmail.com>
2026-07-29 18:51:53 +0900

    Update data-2024-11.js

--- data-2024-11.js
+++ data-2024-11.js
@@ -4,7 +4,7 @@ const posts_202411 = [
     date: "2024-11-22",
     weiboUrl: "https://weibo.com/5994946077/P1wtptzys",
     originalText: "#本周报备# 见面",
-    translatedText: "#本周报备# 만남",
+    translatedText: "만남",
     images: [
       "images/2411-nov22-1.jpg",
       "images/2411-nov22-2.jpg",
