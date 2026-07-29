0f70794cfdb0ae2b45085ef2510a87118975d42d
zyr001122 <vazseyo@gmail.com>
2026-07-29 14:00:02 +0900

    Update data-2024-06.js

--- data-2024-06.js
+++ data-2024-06.js
@@ -31,7 +31,7 @@ const posts_202406 = [
     date: "2024-06-05",
     weiboUrl: "https://weibo.com/5994946077/OhA8paJX5",
     originalText: "阴雨天☔️ #本周报备#",
-    translatedText: "흐리고 비 오는 날☔️ #本周报备#",
+    translatedText: "비 오고 흐림☔️",
     images: [
       "images/2406-jun5-1.jpg",
       "images/2406-jun5-2.jpg",
@@ -53,8 +53,8 @@ const posts_202406 = [
     id: 1706,
     date: "2024-06-14",
     weiboUrl: "https://weibo.com/5994946077/OiZ7z7hGF",
-    originalText: "出发🛫 #本周报备##Hello时装周#",
-    translatedText: "출발🛫 #本周报备##Hello时装周#",
+    originalText: "出发🛫 #本周报备#",
+    translatedText: "출발🛫",
     images: [
       "images/2406-milan1.jpg",
       "images/2406-milan2.jpg",
@@ -66,9 +66,9 @@ const posts_202406 = [
         id: 170601,
         username: "Rabbituui",
         originalText: "起落平安宝宝",
-        translatedText: "이착륙 무사히 해 자기야",
+        translatedText: "이착륙 무사히 해 익연아",
         images: [],
-        actorReply: { originalText: "好嘞 但我延误了", translatedText: "알겠어 근데 지연됐어" }
+        actorReply: { originalText: "好嘞 但我延误了", translatedText: "응응 근데 지연됐어" }
       },
       {
         id: 170602,
@@ -81,18 +81,18 @@ const posts_202406 = [
       {
         id: 170603,
         username: "翊个大学生",
-        originalText: "在米兰多拍vlog啊宝 想看😏",
-        translatedText: "밀라노에서 브이로그 많이 찍어와 자기야 보고 싶어😏",
+        originalText: "在米兰多拍vlog啊宝 想看[doge]",
+        translatedText: "밀라노가서 브이로그 많이 찍어와 익연아 보고 싶어[doge]",
         images: [],
-        actorReply: { originalText: "设备我带了一包", translatedText: "장비 가방 하나 가득 챙겨왔어" }
+        actorReply: { originalText: "设备我带了一包", translatedText: "장비 가방 하나 두둑하게 챙겨왔어" }
       },
       {
         id: 170604,
         username: "海豚不是豚骨拉面",
         originalText: "喜欢这一款，但是博主喜欢假面骑士，怎么办？",
-        translatedText: "이거 마음에 드는데, 블로거가 가면라이더 좋아하면 어떡하지?",
+        translatedText: "이 분 딱 제 취향인데,이 사람 가면라이더 좋아하면 어떡하죠?",
         images: [],
-        actorReply: { originalText: "一起加入", translatedText: "같이 입문해" }
+        actorReply: { originalText: "一起加入", translatedText: "같이 입덕해" }
       }
     ]
   },
@@ -101,14 +101,14 @@ const posts_202406 = [
     date: "2024-06-15",
     weiboUrl: "https://weibo.com/5994946077/Oj6Qefj85",
     originalText: "平安🛬",
-    translatedText: "무사히 도착🛬",
+    translatedText: "무사 도착🛬",
     images: ["images/2406-milan-land.jpg"],
     comments: [
       {
         id: 170701,
         username: "先干饭吧Tz",
-        originalText: "😵😵😵😵😵",
-        translatedText: "😵😵😵😵😵",
+        originalText: "[躺平][躺平][躺平][躺平][躺平]",
+        translatedText: "[躺平][躺平][躺平][躺平][躺平]",
         images: ["images/2406-fan-meme.jpg"],
         actorReply: { originalText: "什么梗？", translatedText: "무슨 밈이야?" }
       },
@@ -116,7 +116,7 @@ const posts_202406 = [
         id: 170702,
         username: "勤天星轨集",
         originalText: "周老大 平安 来赏花 我妈妈做的 厉害吧❤️",
-        translatedText: "익연이형 무사히 도착 꽃 보러 와 우리 엄마가 만든 거야 대단하지❤️",
+        translatedText: "익연 오빠 무사히 도착 했구나 꽃 보러 와 우리 엄마가 만든 거야 대단하지❤️",
         images: ["images/2406-flower.jpg"],
         actorReply: { originalText: "🌸", translatedText: "🌸" }
       },
@@ -139,16 +139,16 @@ const posts_202406 = [
       {
         id: 170705,
         username: "渔小然-o-",
-        originalText: "你IP怎么跑到挪威去了😄",
-        translatedText: "아이피가 왜 노르웨이로 간 거야😄",
+        originalText: "你IP怎么跑到挪威去了[哈哈]",
+        translatedText: "아이피가 왜 노르웨이로 간 거야[哈哈]",
         images: [],
-        actorReply: { originalText: "🤔", translatedText: "🤔" }
+        actorReply: { originalText: "[疑问]", translatedText: "[疑问]" }
       },
       {
         id: 170706,
         username: "早起我恨你你",
-        originalText: "玩的开心😭😭 还有保护好你的行李箱！☺️",
-        translatedText: "즐겁게 놀아😭😭 그리고 캐리어 잘 챙겨!☺️",
+        originalText: "玩的开心[来][来] 还有保护好你的行李箱！[兔子]",
+        translatedText: "재밌게 놀고 와[来][来] 그리고 캐리어 잘 챙겨![兔子]",
         images: [],
         actorReply: { originalText: "它也平安", translatedText: "캐리어도 무사해" }
       }
@@ -170,14 +170,14 @@ const posts_202406 = [
     authorName: "陈澄澄2025",
     authorAvatar: "🥣",
     originalText: "然然最近的一顿饭的是吃什么呢🤔好不好吃！",
-    translatedText: "익연아 요즘 밥 뭐 먹었어🤔 맛있었어!",
+    translatedText: "익연 요즘은 밥 뭐 먹어🤔 맛있는 거 먹었어 안 먹었어!",
     images: [],
     comments: [
       {
         id: 170801,
         username: "周翊然Tz",
         originalText: "剧组盒饭",
-        translatedText: "촬영장 도시락",
+        translatedText: "촬영장 도시락 먹었어",
         images: [],
         actorReply: null
       }
@@ -197,7 +197,8 @@ const posts_202406 = [
         id: 170901,
         username: "周翊然Tz",
         originalText: "烤苕皮？",
-        translatedText: "고구마 껍질 구이?",
+        translatedText: "카오샤오피?",
+        note:"카오샤오피 : 고구마 전분 구이 (충칭 음식)",
         images: [],
         actorReply: null
       }
@@ -210,7 +211,7 @@ const posts_202406 = [
     authorName: "Moonear__",
     authorAvatar: "🥣",
     originalText: "晚饭时间那就分享一下晚饭吧，你的🍚呢？",
-    translatedText: "저녁 시간이니까 저녁 공유할게, 너는🍚?",
+    translatedText: "저녁 시간이니까 저녁 밥 공유할게, 너는🍚 먹었어?",
     images: ["images/2406-dinner3.jpg"],
     comments: [
       {
@@ -230,7 +231,7 @@ const posts_202406 = [
     authorName: "高冷一只萱o",
     authorAvatar: "🥣",
     originalText: "小然 今天是俺生日u 啥表示",
-    translatedText: "익연아 오늘 내 생일이야 뭔가 해줄 거야",
+    translatedText: "익연아 오늘 내 생일인데 뭐 해줄 거야",
     images: [],
     comments: [
       {
@@ -249,8 +250,8 @@ const posts_202406 = [
     weiboUrl: "https://weibo.com/7644233135/OkcjUtFGP",
     authorName: "我翊直在然然",
     authorAvatar: "🥣",
-    originalText: "哈喽小然 最近很很开心耶 天天开心哦 看到了米兰之旅的vlog啦哦哈哈哈哈 那个像柠檬一样的果是石头吗😏😏😏",
-    translatedText: "안녕 익연아 요즘 너무 즐거워 매일매일 행복해 밀라노 여행 브이로그 봤어 ㅋㅋㅋㅋ 레몬처럼 생긴 그거 돌이야?😏😏😏",
+    originalText: "哈喽小然 最近很很开心耶 天天开心哦 看到了米兰之旅的vlog啦哦哈哈哈哈 那个像柠檬一样的果是石头吗[doge][doge][doge]",
+    translatedText: "안녕 익연아 요즘 너무 즐거워 매일매일 행복해 밀라노 여행 브이로그 봤어 ㅋㅋㅋㅋ 레몬처럼 생긴 그거 돌이야?[doge][doge]",
     images: [],
     comments: [
       {
@@ -317,7 +318,7 @@ const posts_202406 = [
         id: 171501,
         username: "周翊然Tz",
         originalText: "会玩",
-        translatedText: "잘 노네",
+        translatedText: "좀 치는데",
         images: [],
         actorReply: null
       }
@@ -337,7 +338,7 @@ const posts_202406 = [
         id: 171601,
         username: "周翊然Tz",
         originalText: "要晴天了",
-        translatedText: "맑아지겠네",
+        translatedText: "맑아지려나보다",
         images: [],
         actorReply: null
       }
@@ -350,7 +351,7 @@ const posts_202406 = [
     authorName: "cookiehh",
     authorAvatar: "🥣",
     originalText: "最近广东有在慢慢放晴诶🌤️ 看到蓝天感觉心情都有变好了 刷到这个假面骑士百人图你能认对几个捏 在麦麦看到这个笑盒 是不是看到感觉心情都变好了哈哈哈哈 在片场临时在信封上画了一个你和法拉利生日会的造型有些潦草不知道你喜不喜欢噻 成为高三生的第一个双休 马上要去集训了好紧张 和好朋友一起来了梅州吃了好吃的拍了好看的好玩的 还带着小猫出去耍了 最后分享一张最近看到的绝美日落 快杀青了吧注意防晒好好吃饭！",
-    translatedText: "요즘 광둥이 서서히 맑아지고 있어🌤️ 파란 하늘 보니까 기분이 좋아지더라 가면라이더 100인 일러스트 몇 명이나 맞출 수 있어 맥도날드에서 이 웃음 상자 봤는데 보면 기분이 좋아지지 않아ㅋㅋㅋㅋ 촬영장에서 임시로 봉투에 너랑 페라리 생일 파티 코디 그렸는데 좀 엉성해 마음에 들지 모르겠어 고3 첫 번째 주말 됐어 곧 집중 훈련 가야 해서 긴장돼 친구랑 같이 메이저우 와서 맛있는 것도 먹고 예쁜 사진도 찍고 재밌게 놀았어 고양이도 데리고 나갔어 마지막으로 요즘 봤던 멋진 노을 공유할게 거의 크랭크업이지? 자외선 차단 잘 하고 밥 잘 먹어!",
+    translatedText: "요즘 광둥이 서서히 맑아지고 있어🌤️ 파란 하늘 보니까 기분이 좋아지더라 가면라이더 100인 일러스트 몇 명이나 맞출 수 있어? 맥도날드에서 이 럭키 상자 봤는데 보면 기분 좋아지지 않아?ㅋㅋㅋㅋㅋ 촬영장에서 임시로 봉투에 너랑 페라리 생일 파티 코디 그렸는데 좀 엉성해 마음에 들지 모르겠어 고3 첫 번째 주말인데 곧 집중 훈련 가야 해서 긴장돼 친구랑 같이 메이저우 와서 맛있는 것도 먹고 예쁜 사진도 찍고 재밌게 놀기도 했어 고양이도 데리고 나갔어 마지막으로 요즘 봤던 멋진 노을 공유할게 이제 곧 크랭크업이지? 자외선 차단 잘 하고 밥 잘 챙겨 먹어!",
     images: ["images/2406-collage.jpg"],
     comments: [
       {
@@ -370,7 +371,7 @@ const posts_202406 = [
     authorName: "Zz小锦梨",
     authorAvatar: "🥣",
     originalText: "然哥 下周六就要学考了！emmm紧张又不紧张的，主要是很焦虑…可以提前祝福我学考顺利吗🥹",
-    translatedText: "익연이형 다음 주 토요일에 학업고사야! 긴장되는 것도 같고 아닌 것도 같고, 주로 불안해서… 미리 학업고사 잘 보게 빌어줄 수 있어?🥹",
+    translatedText: "익연 오빠 다음 주 토요일에 학업고사야! 긴장되는 것도 같기도 하고 아닌 것 같기도 하고, 아무래도 불안해… 미리 학업고사 잘 보게 빌어줄 수 있어?🥹",
     images: ["images/2406-exam.jpg"],
     comments: [
       {
@@ -418,7 +419,7 @@ const posts_202406 = [
         id: 172001,
         username: "周翊然Tz",
         originalText: "好吃吗",
-        translatedText: "맛있어?",
+        translatedText: "맛있지?",
         images: [],
         actorReply: null
       }
@@ -430,8 +431,8 @@ const posts_202406 = [
     weiboUrl: "https://weibo.com/7714686089/Okcn3rRgP",
     authorName: "树深时雾起i",
     authorAvatar: "🥣",
-    originalText: "在做项目呢😭 愁死人了😵😵😵",
-    translatedText: "프로젝트 하는 중이야😭 너무 힘들어 죽겠어😵😵😵",
+    originalText: "在做项目呢[来] 愁死人了[躺平][躺平][躺平]",
+    translatedText: "프로젝트 하는 중이야[来] 너무 힘들어 죽겠어[躺平][躺平][躺平]",
     images: ["images/2406-project.jpg"],
     comments: [
       {
@@ -458,7 +459,7 @@ const posts_202406 = [
         id: 172201,
         username: "周翊然Tz",
         originalText: "祝你愿望实现",
-        translatedText: "소원 이루길 바라",
+        translatedText: "소원 이루길 바랄게",
         images: [],
         actorReply: null
       }
@@ -470,8 +471,8 @@ const posts_202406 = [
     weiboUrl: "https://weibo.com/7549069415/OkcnZDmRr",
     authorName: "吧啦啦吧啦00",
     authorAvatar: "🥣",
-    originalText: "熟不熟悉😏",
-    translatedText: "낯익지 않아😏",
+    originalText: "熟不熟悉[doge]",
+    translatedText: "낯익지 않아[doge]",
     note: "원문은 동영상",
     images: [],
     comments: [
@@ -479,7 +480,7 @@ const posts_202406 = [
         id: 172301,
         username: "周翊然Tz",
         originalText: "眼熟",
-        translatedText: "눈에 익어",
+        translatedText: "어디서 봤는데",
         images: [],
         actorReply: null
       }
@@ -492,14 +493,14 @@ const posts_202406 = [
     authorName: "我不想长痘Hz-",
     authorAvatar: "🥣",
     originalText: "报告然然！最近有好好生活好好学习！虽然情绪有时候不太稳定哈哈… 快看我吃饭时饭店里的咪咪超可爱的！！给你看一下我的日记本日历！前两个月的聊天时刻我都在学校没办法留言呜呜呜🥹这个月终于给我等到了啊啊啊啊啊啊啊啊啊！高中的时光过得真快啊 一转眼高一就要结束啦！这个月真的超忙 一直在备考 呜呜呜呜 然然能祝我考试顺利吗😭😭🫶🏻 祝你生活顺利天天开心呀～我们一起努力吧！",
-    translatedText: "보고합니다 익연아! 요즘 잘 생활하고 공부 열심히 하고 있어! 가끔 감정이 불안정할 때도 있지만ㅋㅋ… 밥 먹을 때 식당에 있는 고양이 봐봐 너무 귀여워!! 내 다이어리 달력도 보여줄게! 지난 두 달은 학교에 있어서 댓글 달 수가 없었어🥹 이번 달에 드디어 기다리던 게 왔어!!!! 고등학교 시간이 너무 빨리 가네 눈 깜짝하면 고1이 끝나! 이번 달 진짜 바빴어 계속 시험 준비해서 으으으으 익연아 시험 잘 보게 빌어줄 수 있어?😭😭🫶🏻 네 생활도 순탄하고 매일 행복하길~ 같이 열심히 하자!",
+    translatedText: "보고합니다 익연아! 요즘 잘 지내고 공부 열심히 하고 있어! 가끔 감정이 불안정할 때도 있지만ㅋㅋ… 밥 먹으러 갔던 식당에 고양이 봐봐 너무 귀여워!! 내 다이어리 달력도 보여줄게! 2개월동안은 학교에 있어서 댓글 달 수가 없었어🥹 이번 달에 드디어 기다리던 게 왔어!!!! 고등학교 다니니까 시간이 너무 빨리 지나가 눈 깜짝하니까 고1이 끝이야! 이번 달 진짜 바빴어 계속 시험 준비해서ㅓㅓㅓㅓ 익연아 시험 잘 보게 빌어줄 수 있어?😭😭🫶🏻 오빠 생활도 순탄하고 매일 행복하길~ 같이 열심히 하자!",
     images: ["images/2406-cat2.jpg"],
     comments: [
       {
         id: 172401,
         username: "周翊然Tz",
         originalText: "考试顺利",
-        translatedText: "시험 잘 봐",
+        translatedText: "시험 잘 보고 와",
         images: [],
         actorReply: null
       }
@@ -511,15 +512,15 @@ const posts_202406 = [
     weiboUrl: "https://weibo.com/7613776623/Okcov2it2",
     authorName: "木子Kang-",
     authorAvatar: "🥣",
-    originalText: "然然，快出高考成绩了！好紧张啊😭😭",
-    translatedText: "익연아, 수능 성적 곧 나온다! 너무 떨려😭😭",
+    originalText: "然然，快出高考成绩了！好紧张啊[来][来]",
+    translatedText: "익연아, 수능 성적 곧 나온다! 너무 떨려[来][来]",
     images: [],
     comments: [
       {
         id: 172501,
         username: "周翊然Tz",
         originalText: "相信自己 一切都会有好结果",
-        translatedText: "자신을 믿어 다 좋은 결과 있을 거야",
+        translatedText: "스스로를 믿어 좋은 결과 있을 거야",
         images: [],
         actorReply: null
       }
@@ -531,15 +532,15 @@ const posts_202406 = [
     weiboUrl: "https://weibo.com/7738264793/OkcoGwqe5",
     authorName: "潦草小狗z",
     authorAvatar: "🥣",
-    originalText: "给你看可爱小狗和我拍的天空😎",
-    translatedText: "귀여운 강아지랑 내가 찍은 하늘 보여줄게😎",
+    originalText: "给你看可爱小狗和我拍的天空[并不简单]",
+    translatedText: "귀여운 강아지랑 내가 찍은 하늘 보여줄게[并不简单]",
     images: [],
     comments: [
       {
         id: 172601,
         username: "周翊然Tz",
         originalText: "潦草小狗",
-        translatedText: "엉성한 강아지",
+        translatedText: "어색한 강아지",
         images: [],
         actorReply: null
       }
@@ -550,7 +551,7 @@ const posts_202406 = [
     date: "2024-06-22",
     weiboUrl: "https://weibo.com/5994946077/Okcj03wKQ",
     originalText: "走了 搬砖去了",
-    translatedText: "갈게 일하러 간다",
+    translatedText: "가야겠다 일하러 가볼게",
     images: [],
     comments: []
   },
@@ -559,7 +560,7 @@ const posts_202406 = [
     date: "2024-06-29",
     weiboUrl: "https://weibo.com/5994946077/OlfzKrzbZ",
     originalText: "💇🏻‍♂️#本周报备#",
-    translatedText: "💇🏻‍♂️#本周报备#",
+    translatedText: "💇🏻‍♂️",
     images: [
       "images/2406-jun29-1.jpg",
       "images/2406-jun29-2.jpg",
