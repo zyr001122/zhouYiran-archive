231993b6acc0791e2497218e9264f9d13213730d
zyr001122 <vazseyo@gmail.com>
2026-07-29 16:50:15 +0900

    Add files via upload

---
+++ data-2025-01.js
@@ -0,0 +1,325 @@
+// 2025-01-22 게시물 - 기존 data-2025-01.js의 posts_202501 배열에 이어붙이면 됩니다.
+// id는 250101부터 시작했는데, 기존 파일에 이미 다른 1월 게시물이 있으면 겹치지 않게 조정 필요!
+
+{
+  id: 250101,
+  date: "2025-01-22",
+  weiboUrl: null,
+  authorName: "周翊然Tz",
+  originalText: "周翊然超话 晚上好",
+  translatedText: "周翊然超话 다들 좋은 저녁이에요",
+  images: [],
+  comments: [
+    {
+      id: 25010101,
+      username: "周翊然Tz",
+      originalText: "我要走了各位 假期里做了自己喜欢的事 然后时隔那么长时间 也能再一次陪在家人身边过年 我很开心 希望大家也能开开心心的 和家人们一起迎接新年！",
+      translatedText: "나 이제 가볼게 다들. 휴가 동안 좋아하는 일도 하고, 오랜만에 다시 가족들 곁에서 설을 보낼 수 있어서 정말 좋았어. 다들 즐겁게 가족들이랑 새해 맞이하길 바라!",
+      images: [],
+      actorReply: null
+    },
+    {
+      id: 25010102,
+      username: "周翊然Tz",
+      originalText: "大家最近有没有什么好听的歌分享一下",
+      translatedText: "요즘 다들 괜찮은 노래 있으면 하나 공유해줘",
+      images: [],
+      actorReply: null
+    },
+    {
+      id: 25010103,
+      username: "魚肉蛋黄酥o_",
+      originalText: "你好",
+      translatedText: "안녕",
+      images: [],
+      actorReply: {
+        originalText: "你咋啦",
+        translatedText: "왜 그래"
+      }
+    },
+    {
+      id: 25010105,
+      username: "慕翊婷",
+      originalText: "晚上好呀 开完赛车感觉如何呀",
+      translatedText: "좋은 저녁이야~ 카트레이싱 끝내고 기분 어때?",
+      images: [
+        "images/2501-doge-1.jpg",
+        "images/2501-doge-2.jpg"
+      ],
+      actorReply: {
+        originalText: "不错",
+        translatedText: "괜찮았어"
+      }
+    },
+    {
+      id: 25010107,
+      username: "十八点Singer_",
+      originalText: "棒棒的汪汪已经开始新年旺旺模式咯 @周翊然Tz",
+      translatedText: "멋진 우리 강아지 벌써 새해 왕왕 모드 시작했네 @周翊然Tz",
+      images: [],
+      actorReply: {
+        originalText: "什么鬼东西 归我了",
+        translatedText: "이게 뭐야ㅋㅋ 내 거 됐네"
+      }
+    },
+    {
+      id: 25010109,
+      username: "Dear-喇巴粥",
+      originalText: "你太过分了？！！",
+      translatedText: "너무한 거 아니야?!!",
+      images: [
+        "images/2501-ferrari-meme.jpg"
+      ],
+      actorReply: {
+        originalText: "哈哈哈哈哈哈哈 我真的哈哈哈哈",
+        translatedText: "ㅋㅋㅋㅋㅋㅋㅋ 진짜 개웃기다ㅋㅋㅋㅋ"
+      }
+    },
+    {
+      id: 25010111,
+      username: "附中刘亦菲TZ-",
+      originalText: "然子",
+      translatedText: "란쯔~",
+      images: [
+        "images/2501-lie-flat-sticker.jpg"
+      ],
+      actorReply: {
+        originalText: "咳咳",
+        translatedText: "흠흠"
+      }
+    },
+    {
+      id: 25010113,
+      username: "1点大王",
+      originalText: "我也想开赛车",
+      translatedText: "나도 카트레이싱 하고 싶다",
+      images: [],
+      actorReply: {
+        originalText: "走",
+        translatedText: "가자"
+      }
+    },
+    {
+      id: 25010115,
+      username: "祈川十里春",
+      originalText: "吃完饭了没 @周翊然Tz",
+      translatedText: "밥 다 먹었어? @周翊然Tz",
+      images: [],
+      actorReply: {
+        originalText: "我也吃了火锅",
+        translatedText: "나도 훠궈 먹었어"
+      }
+    },
+    {
+      id: 25010117,
+      username: "绝版的每一天咯",
+      originalText: "好累😔",
+      translatedText: "너무 피곤해😔",
+      images: [],
+      actorReply: {
+        originalText: "这画工比我好上百倍了",
+        translatedText: "그림 실력이 나보다 백배는 낫네"
+      }
+    },
+    {
+      id: 25010119,
+      username: "麦麦_Tz",
+      originalText: "然然评价一下",
+      translatedText: "然然아 평가 좀 해줘",
+      images: [
+        "images/2501-fan-drawing.jpg"
+      ],
+      actorReply: {
+        originalText: "……",
+        translatedText: "……"
+      }
+    },
+    {
+      id: 25010121,
+      username: "十八点Singer_",
+      originalText: "走吧小狗 周翊然是不会注意到我们的",
+      translatedText: "가자 강아지들아, 周翊然은 우릴 신경도 안 써줄 거야",
+      images: [],
+      actorReply: {
+        originalText: "images/2501-tz-sticker-1.jpg",
+        translatedText: "images/2501-tz-sticker-1.jpg"
+      }
+    },
+    {
+      id: 25010123,
+      username: "我衹是喜歡一頭可愛豬又有什麼錯",
+      originalText: "看看这是啥",
+      translatedText: "이게 뭔지 한번 봐봐",
+      images: [
+        "images/2501-thinking-photo.jpg"
+      ],
+      actorReply: {
+        originalText: "修勾",
+        translatedText: "댕댕이"
+      }
+    },
+    {
+      id: 25010125,
+      username: "liu豆豆子_",
+      originalText: "我的新年战壳",
+      translatedText: "내 새해맞이 폰 케이스",
+      images: [
+        "images/2501-phonecase.jpg"
+      ],
+      actorReply: {
+        originalText: "金色的很好看",
+        translatedText: "골드색 예쁘네"
+      }
+    },
+    {
+      id: 25010127,
+      username: "拜托了哥哥酱",
+      originalText: "我的心好痛",
+      translatedText: "마음이 너무 아파",
+      images: [],
+      actorReply: {
+        originalText: "images/2501-tz-sticker-2.jpg",
+        translatedText: "images/2501-tz-sticker-2.jpg"
+      }
+    },
+    {
+      id: 25010129,
+      username: "骤然吐司yr",
+      originalText: "哥哥你是有什么心事吗",
+      translatedText: "오빠 무슨 고민 있어?",
+      images: [],
+      actorReply: {
+        originalText: "images/2501-tz-sticker-3.jpg",
+        translatedText: "images/2501-tz-sticker-3.jpg"
+      }
+    },
+    {
+      id: 25010131,
+      username: "我产是世界第yi_",
+      originalText: "许愿新的一年开车哥能天天开赛车",
+      translatedText: "새해엔 드라이빙카 오빠가 매일 카트레이싱 하게 해달라고 소원 빌게",
+      images: [],
+      actorReply: {
+        originalText: "images/2501-tz-sticker-4.jpg",
+        translatedText: "images/2501-tz-sticker-4.jpg"
+      }
+    },
+    {
+      id: 25010133,
+      username: "猫卷眠眠然",
+      originalText: "不翻我 我直接皇化 @周翊然Tz",
+      translatedText: "나 안 넘겨주면 바로 폭주할 거야 @周翊然Tz",
+      images: [],
+      actorReply: {
+        originalText: "哈哈哈哈哈哈哈哈哈什么鬼",
+        translatedText: "ㅋㅋㅋㅋㅋㅋㅋㅋㅋ 뭐야 이게"
+      }
+    },
+    {
+      id: 25010135,
+      username: "Zzyirrr_",
+      originalText: "如何",
+      translatedText: "어때",
+      images: [
+        "images/2501-peace-photo.jpg"
+      ],
+      actorReply: {
+        originalText: "我猜他是奶龙",
+        translatedText: "쟤 나이룽(奶龙) 아닐까"
+      }
+    },
+    {
+      id: 25010137,
+      username: "陪我淋一场雨·",
+      originalText: "这个壁纸眼熟吗",
+      translatedText: "이 배경화면 낯익지 않아?",
+      images: [
+        "images/2501-wallpaper-1.jpg",
+        "images/2501-wallpaper-2.jpg"
+      ],
+      actorReply: {
+        originalText: "模仿我",
+        translatedText: "나 따라했네"
+      }
+    },
+    {
+      id: 25010139,
+      username: "我就是周六栗子日",
+      originalText: "@周六栗子日-Zzzzy：周翊然会翻我这件事假面骑士、假面骑士v3、假面骑士X、假面骑士亚马逊、假面骑士强人、假面骑士sky rider、假面骑士超一号、假面骑士暗日、假面骑士空我、假面骑士亚极陀、假面骑士龙骑、假面骑士555、假面骑士剑、假面骑士响鬼、假面骑士the first等诸多假面骑士都觉得很赞",
+      translatedText: "@周六栗子日-Zzzzy: 周翊然이 내 이 글도 넘겨준 적 있어. 가면라이더, 가면라이더 V3, 가면라이더 X, 가면라이더 아마존, 가면라이더 스트롱거, 가면라이더 스카이라이더, 가면라이더 슈퍼원, 가면라이더 블랙, 가면라이더 쿠우가, 가면라이더 아기토, 가면라이더 류우키, 가면라이더 555, 가면라이더 켄, 가면라이더 히비키, 가면라이더 더 퍼스트 등 수많은 가면라이더 팬들도 다 멋지다고 생각할 거야",
+      images: [],
+      actorReply: {
+        originalText: "给555的",
+        translatedText: "555용으로 준 거야"
+      }
+    },
+    {
+      id: 25010141,
+      username: "-融雪初岛-",
+      originalText: "问问小然年夜饭都吃什么 提前祝你小年快乐！",
+      translatedText: "小然한테 물어볼게, 연야반 뭐 먹었어? 미리 소년 축하해!",
+      images: [],
+      actorReply: {
+        originalText: "小年快乐！ 到时候拍给你们看看",
+        translatedText: "소년 축하해! 그때 되면 찍어서 보여줄게"
+      }
+    },
+    {
+      id: 25010143,
+      username: "Moonear__",
+      originalText: "支持我过年做这个美甲吗💅",
+      translatedText: "설날에 이 네일아트 해도 괜찮을까?💅",
+      images: [
+        "images/2501-nail.jpg"
+      ],
+      actorReply: {
+        originalText: "哈哈哈哈哈哈哈哈 这做了跑步很快吧",
+        translatedText: "ㅋㅋㅋㅋㅋㅋㅋㅋ 이거 하면 달리기 빨라지겠다"
+      }
+    },
+    {
+      id: 25010145,
+      username: "遇見小翊",
+      originalText: "然总，看我家猫会后空翻法拉利会吗？？？",
+      translatedText: "然총, 우리 집 고양이가 페라리 뒤로 공중제비 도는 거 볼래???",
+      images: [],
+      actorReply: {
+        originalText: "你发给我看看",
+        translatedText: "나한테 보내봐"
+      }
+    },
+    {
+      id: 25010147,
+      username: "然而醒_",
+      originalText: "今年茅台有没有资格带回家过年啊",
+      translatedText: "올해는 마오타이 집에 가져갈 자격 있어?",
+      images: [],
+      actorReply: {
+        originalText: "必须的",
+        translatedText: "당연하지"
+      }
+    },
+    {
+      id: 25010149,
+      username: "小周喜欢过秋天",
+      originalText: "今天是什么顺序翻牌？能翻到我不！ @周翊然Tz",
+      translatedText: "오늘은 무슨 순서로 넘기는 거야? 나도 넘겨줄 수 있어? @周翊然Tz",
+      images: [],
+      actorReply: {
+        originalText: "images/2501-tz-sticker-5.jpg",
+        translatedText: "images/2501-tz-sticker-5.jpg"
+      }
+    },
+    {
+      id: 25010151,
+      username: "只想看小渝_",
+      originalText: "请问这位赛车手，能否让我坐你的副驾驶",
+      translatedText: "저기 레이서님, 조수석에 태워주실 수 있나요",
+      images: [],
+      actorReply: {
+        originalText: "赛车都没有副驾驶",
+        translatedText: "카트레이싱카엔 조수석이 없어"
+      }
+    }
+  ]
+}
---
+++ data-2025-02.js
@@ -0,0 +1,208 @@
+// 2025-02-14 게시물 - 기존 data-2025-02.js의 posts_202502 배열에 이어붙이면 됩니다.
+// id는 250201부터 시작했는데, 기존 파일에 이미 다른 2월 게시물이 있으면 겹치지 않게 조정 필요!
+
+{
+  id: 250201,
+  date: "2025-02-14",
+  weiboUrl: "https://weibo.com/5994946077/PegtwetoV",
+  authorName: "周翊然Tz",
+  originalText: "周翊然超话 hello 节日快乐大家！",
+  translatedText: "周翊然超话 hello 다들 명절 잘 보내!",
+  images: [],
+  comments: [
+    {
+      id: 25020101,
+      username: "周翊然Tz",
+      originalText: "你们是不是很忙 没时间理我",
+      translatedText: "다들 바빠서 나 신경 쓸 시간이 없나 봐",
+      images: [],
+      actorReply: null
+    },
+    {
+      id: 25020102,
+      username: "周翊然Tz",
+      originalText: "我这会儿吃饭呢 吃的海鲜火锅",
+      translatedText: "나 지금 밥 먹고 있어, 해물 훠궈 먹는 중",
+      images: [],
+      actorReply: null
+    },
+    {
+      id: 25020103,
+      username: "周翊然Tz",
+      originalText: "我上菜了 该吃饭了 节日快乐🌹🌹🌹",
+      translatedText: "요리 나왔다! 다들 밥 먹을 시간이야, 명절 잘 보내🌹🌹🌹",
+      images: [],
+      actorReply: null
+    },
+    {
+      id: 25020104,
+      username: "珍珍珍珍妮妮儿",
+      originalText: "你一天没理我了，我变成了一只鸡，我被爱判处终生孤鸡",
+      translatedText: "너 하루 종일 나 신경 안 써줘서 나 닭 됐어, 사랑 때문에 평생 외로운 닭 신세가 됐다고",
+      images: [],
+      actorReply: {
+        originalText: "？？？",
+        translatedText: "？？？"
+      }
+    },
+    {
+      id: 25020106,
+      username: "我是神经病Tz",
+      originalText: "老公新歌什么时候！是不是今晚！",
+      translatedText: "여보 신곡 언제 나와! 오늘 밤이야?!",
+      images: [],
+      actorReply: {
+        originalText: "[裂开]我还没录",
+        translatedText: "[裂开]나 아직 녹음도 안 했어"
+      }
+    },
+    {
+      id: 25020108,
+      username: "小周喜欢过秋天",
+      originalText: "内部消息 周翊然今天要发18宫格自拍 🤳",
+      translatedText: "내부 소식통에 의하면 周翊然이 오늘 18분할 셀카 올린대🤳",
+      images: [],
+      actorReply: {
+        originalText: "哪个内部 我咋不知道",
+        translatedText: "무슨 내부? 나는 왜 몰라"
+      }
+    },
+    {
+      id: 25020110,
+      username: "十八点Singer_",
+      originalText: "看我元宵节吃的汤圆 这样你一口气能吃几个呀 @周翊然Tz",
+      translatedText: "내가 원소절에 먹은 탕위안 봐봐, 너라면 한 번에 몇 개나 먹을 수 있어? @周翊然Tz",
+      images: [
+        "images/2502-tangyuan.jpg"
+      ],
+      actorReply: {
+        originalText: "好可爱啊 哈哈哈哈哈哈",
+        translatedText: "완전 귀엽다 ㅋㅋㅋㅋㅋㅋ"
+      }
+    },
+    {
+      id: 25020112,
+      username: "拜托了哥哥酱",
+      originalText: "看看这是什么",
+      translatedText: "이게 뭔지 한번 봐봐",
+      images: [
+        "images/2502-mystery-food.jpg"
+      ],
+      actorReply: {
+        originalText: "别告诉我 可以边吃边看 我会很羡慕",
+        translatedText: "설마 먹으면서 보는 거라고 하지 마, 완전 부러울 것 같은데"
+      }
+    },
+    {
+      id: 25020114,
+      username: "偷吃一口饭饭",
+      originalText: "我今天吃卜卜贝耶 @周翊然Tz",
+      translatedText: "나 오늘 조개 요리(卜卜贝) 먹었다 @周翊然Tz",
+      images: [
+        "images/2502-bobei.jpg"
+      ],
+      actorReply: {
+        originalText: "我也吃的海鲜火锅！",
+        translatedText: "나도 해물 훠궈 먹었어!"
+      }
+    },
+    {
+      id: 25020116,
+      username: "落日屿橘",
+      originalText: "你怎么空降评论区也不打个招呼？我刚打开微博没做好心理准备 就这么大个帅哥跳出来 这合适吗？下次注意！！",
+      translatedText: "왜 댓글창에 갑자기 나타나면서 인사도 안 해? 나 방금 웨이보 켰는데 마음의 준비도 안 됐는데 이렇게 큰 훈남이 갑자기 튀어나오다니 이게 맞아? 다음부턴 주의해!!",
+      images: [],
+      actorReply: {
+        originalText: "好的",
+        translatedText: "알겠어"
+      }
+    },
+    {
+      id: 25020118,
+      username: "蜡笔小周Tz",
+      originalText: "周翊然！周翊然！周翊然！日子在忙也要记住 你别管记住什么 你就记住！一定要记住！",
+      translatedText: "周翊然! 周翊然! 周翊然! 아무리 바빠도 꼭 기억해! 뭘 기억해야 하는지는 몰라도 그냥 기억해! 무조건 기억해야 해!",
+      images: [
+        "images/2502-remember-reminder.jpg"
+      ],
+      actorReply: {
+        originalText: "所以到底是啥[裂开]",
+        translatedText: "그래서 대체 뭔데[裂开]"
+      }
+    },
+    {
+      id: 25020120,
+      username: "是我的小土豆-Tz",
+      originalText: "老公 生病了 你注意身体哦 😭",
+      translatedText: "여보 아팠어, 몸 조심해 😭",
+      images: [],
+      actorReply: {
+        originalText: "早日康复 要好好注意身体啊",
+        translatedText: "빨리 나아, 몸 잘 챙겨"
+      }
+    },
+    {
+      id: 25020122,
+      username: "下粥六见Tz",
+      originalText: "请你吃冰激凌宝宝",
+      translatedText: "아이스크림 사줄게 자기야",
+      images: [
+        "images/2502-icecream.jpg"
+      ],
+      actorReply: {
+        originalText: "小心别感冒了",
+        translatedText: "감기 조심해"
+      }
+    },
+    {
+      id: 25020124,
+      username: "拜托了哥哥酱",
+      originalText: "不理我就这样",
+      translatedText: "나 신경 안 써주면 이렇게 될 거야",
+      images: [
+        "images/2502-crying-dog-sticker.jpg"
+      ],
+      actorReply: {
+        originalText: "images/2502-tz-sticker-1.jpg",
+        translatedText: "images/2502-tz-sticker-1.jpg"
+      }
+    },
+    {
+      id: 25020126,
+      username: "Insiston1t",
+      originalText: "我家猫听说你发微博了都出来了 🤏",
+      translatedText: "우리 집 고양이가 너 웨이보 올렸다는 소식 듣고 다 뛰쳐나왔어 🤏",
+      images: [],
+      actorReply: {
+        originalText: "是不是到饭点了",
+        translatedText: "밥때 된 거 아니야?"
+      }
+    },
+    {
+      id: 25020128,
+      username: "浪漫贩卖i",
+      originalText: "有没有吃过缺牙齿",
+      translatedText: "이빨 빠진 거(缺牙齿) 먹어본 적 있어?",
+      images: [
+        "images/2502-headband-sticker.jpg"
+      ],
+      actorReply: {
+        originalText: "这个是不是能把人辣晕",
+        translatedText: "이거 먹으면 매워서 정신 못 차리는 거 아니야?"
+      }
+    },
+    {
+      id: 25020130,
+      username: "咬一口脆皮年糕_",
+      originalText: "你知不知道 我等了你一天",
+      translatedText: "너 알아? 나 하루 종일 기다렸어",
+      images: [
+        "images/2502-waited-photo.jpg"
+      ],
+      actorReply: {
+        originalText: "我来了啊",
+        translatedText: "왔잖아"
+      }
+    }
+  ]
+}
