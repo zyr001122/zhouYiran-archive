9da814ff8e07bc1cf94aae0a8a85b004a2c6904b
zyr001122 <vazseyo@gmail.com>
2026-07-30 04:06:40 +0900

    Update data-2025-10.js

--- data-2025-10.js
+++ data-2025-10.js
@@ -5,18 +5,18 @@ const posts_202510 = [
     weiboUrl: "https://weibo.com/5994946077/QakJun9Yo",
     authorName: "周翊然Tz",
     originalText: "hi 我来了",
-    translatedText: "hi 왔어",
+    translatedText: "hi 나 왔다",
     images: [],
     comments: [
       {
         id: 25100102,
         username: "扁桃体发炎不发言-",
         originalText: "哥哥！请问唐亦寻他第一次开赛车紧张吗哈哈哈哈😆",
-        translatedText: "오빠! 唐亦寻이 처음 카레이싱 할 때 긴장했어? ㅋㅋㅋㅋ😆",
+        translatedText: "오빠! 탕이쉰으로 처음 레이싱 하는건데 긴장했어? ㅋㅋㅋㅋ😆",
         images: ["images/2510-first-race.jpg"],
         actorReply: {
           originalText: "还行",
-          translatedText: "그냥 그랬어"
+          translatedText: "할 만했어"
         }
       },
       {
@@ -27,14 +27,14 @@ const posts_202510 = [
         images: ["images/2510-paper-craft.jpg"],
         actorReply: {
           originalText: "好厉害",
-          translatedText: "완전 대단하다"
+          translatedText: "진짜 잘한다"
         }
       },
       {
         id: 25100104,
         username: "魚肉蛋黄酥o_",
         originalText: "法拉利在赛车场享受按摩这事儿它跟你说了吗？🐶",
-        translatedText: "페라리가 서킷에서 마사지 받은 거, 걔가 너한테 말했어?🐶",
+        translatedText: "페라리가 서킷에서 마사지 받은 거, 페라리가 너한테 말했어?🐶",
         images: ["images/2510-ferrari-massage.jpg"],
         actorReply: {
           originalText: "听说了",
@@ -56,7 +56,7 @@ const posts_202510 = [
         id: 25100106,
         username: "牙不吃香菜",
         originalText: "周哥有钓到什么吗",
-        translatedText: "저우 형 뭐 낚은 거 있어?",
+        translatedText: "오빠 뭐 낚은 거 있어?",
         images: ["images/2510-fishing-catch.jpg"],
         actorReply: {
           originalText: "大鱼和小鱼",
@@ -67,7 +67,7 @@ const posts_202510 = [
         id: 25100107,
         username: "魚肉蛋黄酥o_",
         originalText: "老公 听说你最近在钓鱼🎣有没有什么收获可以分享一下呢？🥰",
-        translatedText: "자기야 요즘 낚시한다며🎣 뭐 건진 거 있으면 좀 보여줄 수 있어?🥰",
+        translatedText: "자기야 요즘 낚시한다며🎣 뭐 건진 거 있으면 공유 해주면 안 돼?🥰",
         images: ["images/2510-fishing-share.jpg"],
         actorReply: {
           originalText: "还在钻研中",
@@ -88,12 +88,12 @@ const posts_202510 = [
       {
         id: 25100109,
         username: "吧唧一口然然的脸蛋_zyrzyr",
-        originalText: "天冷😴记得多穿",
-        translatedText: "날씨 추워😴 옷 든든히 입어",
+        originalText: "天冷[泪]记得多穿",
+        translatedText: "날씨 너무 춥다[泪] 옷 든든히 입어",
         images: [],
         actorReply: {
           originalText: "大家也要多穿点",
-          translatedText: "다들 옷 든든히 입어"
+          translatedText: "너네도 옷 든든히 입고다녀"
         }
       },
       {
@@ -122,30 +122,18 @@ const posts_202510 = [
         id: 25100112,
         username: "小吴同学KC",
         originalText: "然哥快看大熊！超级可爱[哇]",
-        translatedText: "然 형 빨리 큰 곰돌이 봐! 완전 귀여워[哇]",
+        translatedText: "오빠 빨리 큰 곰돌이 좀 봐봐! 완전 귀엽지[哇]",
         images: ["images/2510-big-bear.jpg"],
         actorReply: {
           originalText: "哈哈哈感觉还没睡醒",
-          translatedText: "ㅋㅋㅋ 아직 잠이 덜 깬 것 같아"
-        }
-      },
-      {
-        id: 25100113,
-        username: "网易严选",
-        originalText: "小选和然然，10月31日晚8点抖音【网易严选官方旗舰店】不见不散~",
-        translatedText: "샤오쉬안이랑 然然, 10월 31일 밤 8시 도우인 【网易严选 공식 플래그십 스토어】에서 만나요~",
-        note: "협업 브랜드 공식 계정 (익연이 모델인 브랜드)",
-        images: ["images/2510-brand-collab.jpg"],
-        actorReply: {
-          originalText: "[握手]",
-          translatedText: "[握手]"
+          translatedText: "ㅋㅋㅋ 아직 잠에서 덜 깬 것 같은데"
         }
       },
       {
         id: 25100101,
         username: "周翊然Tz",
         originalText: "我走了 明天见",
-        translatedText: "나 갈게 내일 봐",
+        translatedText: "가볼게 내일 보자",
         images: [],
         actorReply: null
       }
