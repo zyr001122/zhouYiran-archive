51a4c2e6bffe4614b1750e824db8325bae6b3ab7
zyr001122 <vazseyo@gmail.com>
2026-07-30 03:08:31 +0900

    Add files via upload

---
+++ data-2025-08.js
@@ -0,0 +1,220 @@
+const posts_202508 = [
+  {
+    id: 250801,
+    date: "2025-08-03",
+    weiboUrl: "https://weibo.com/5994946077/PE7iaa2Jz",
+    authorName: "周翊然Tz",
+    originalText: "一起上班前 🐶#本周报备#",
+    translatedText: "출근하기 전에 🐶#本周报备#",
+    images: [
+      "images/2508-0803-01.jpg",
+      "images/2508-0803-02.jpg",
+      "images/2508-0803-03.jpg",
+      "images/2508-0803-04.jpg",
+      "images/2508-0803-05.jpg",
+      "images/2508-0803-06.jpg",
+      "images/2508-0803-07.jpg",
+      "images/2508-0803-08.jpg",
+      "images/2508-0803-09.jpg"
+    ],
+    comments: []
+  },
+  {
+    id: 250802,
+    date: "2025-08-10",
+    weiboUrl: "https://weibo.com/5994946077/PF8OQDgtI",
+    authorName: "周翊然Tz",
+    originalText: "🎹 #本周报备#",
+    translatedText: "🎹 #本周报备#",
+    images: [
+      "images/2508-0810-01.jpg",
+      "images/2508-0810-02.jpg",
+      "images/2508-0810-03.jpg",
+      "images/2508-0810-04.jpg",
+      "images/2508-0810-05.jpg",
+      "images/2508-0810-06.jpg",
+      "images/2508-0810-07.jpg",
+      "images/2508-0810-08.jpg",
+      "images/2508-0810-09.jpg"
+    ],
+    comments: []
+  },
+  {
+    id: 250803,
+    date: "2025-08-22",
+    weiboUrl: "https://weibo.com/5994946077/Q145z0ylg",
+    authorName: "周翊然Tz",
+    originalText: "hi 来了",
+    translatedText: "hi 왔어",
+    images: [],
+    comments: [
+      {
+        id: 25080302,
+        username: "魚肉蛋黄酥o_",
+        originalText: "老公 请问这个炸鸡是有多好吃才会让你做出这个反应？？？[流鼻血]",
+        translatedText: "자기야 이 튀긴 치킨이 얼마나 맛있길래 이런 반응을 보이는 거야???[流鼻血]",
+        note: "원문 동영상",
+        images: [],
+        actorReply: {
+          originalText: "很香[赞]",
+          translatedText: "냄새 좋아[赞]"
+        }
+      },
+      {
+        id: 25080303,
+        username: "梨花白翊白",
+        originalText: "好看不",
+        translatedText: "예쁘지 않아?",
+        images: ["images/2508-pretty-or-not.jpg"],
+        actorReply: {
+          originalText: "很可爱",
+          translatedText: "완전 귀엽다"
+        }
+      },
+      {
+        id: 25080304,
+        username: "Zz的保镖",
+        originalText: "然然我说阿寻，你应该说什么",
+        translatedText: "익연아 내가 '아쉰'이라고 부르면 뭐라고 해야 돼",
+        images: ["images/2508-what-should-you-say.jpg"],
+        actorReply: {
+          originalText: "我在 我在",
+          translatedText: "나 여기 있어 나 여기 있어"
+        }
+      },
+      {
+        id: 25080305,
+        username: "扁桃体发炎不发言-",
+        originalText: "目测你这个锅都粘了半斤肉😂 真的不考虑买个不粘锅吗",
+        translatedText: "눈대중으로 봐도 이 냄비에 고기 반 근은 눌어붙었겠다😂 진짜 코팅 냄비 하나 살 생각 없어?",
+        images: ["images/2508-nonstick-pan.jpg"],
+        actorReply: {
+          originalText: "[允悲]",
+          translatedText: "[允悲]"
+        }
+      },
+      {
+        id: 25080306,
+        username: "不吃年糕Tz",
+        originalText: "新疆网也不好吗",
+        translatedText: "신장도 와이파이 안 좋아?",
+        images: ["images/2508-xinjiang-wifi.jpg"],
+        actorReply: {
+          originalText: "网确实有点差",
+          translatedText: "와이파이 확실히 좀 안 좋아"
+        }
+      },
+      {
+        id: 25080307,
+        username: "咬一口脆皮年糕_",
+        originalText: "阿寻快来！",
+        translatedText: "아쉰 빨리 와!",
+        images: ["images/2508-come-quick.jpg"],
+        actorReply: {
+          originalText: "马上来了",
+          translatedText: "바로 갈게"
+        }
+      },
+      {
+        id: 25080308,
+        username: "综上所述你最萌",
+        originalText: "请你看海呀[期待][期待]",
+        translatedText: "바다 좀 봐봐[期待][期待]",
+        images: ["images/2508-look-at-sea.jpg"],
+        actorReply: {
+          originalText: "很好看 我旁边都是草原 信号很差",
+          translatedText: "예쁘다 나는 옆에 다 초원이라 신호가 안 좋아"
+        }
+      },
+      {
+        id: 25080309,
+        username: "祈川十里春",
+        originalText: "小然，下期解锁什么新菜",
+        translatedText: "익연아 다음 편에서 새로운 요리 뭐 배워?",
+        images: ["images/2508-new-dish.jpg"],
+        actorReply: {
+          originalText: "等我想一想",
+          translatedText: "생각 좀 해볼게"
+        }
+      },
+      {
+        id: 25080310,
+        username: "家有兔咪_6799",
+        originalText: "然然在新疆吃好吃的了没",
+        translatedText: "익연이 신장에서 맛있는 거 먹었어?",
+        images: [],
+        actorReply: {
+          originalText: "刚到 准备去吃饭了",
+          translatedText: "방금 도착해서 이제 밥 먹으러 가려고"
+        }
+      },
+      {
+        id: 25080311,
+        username: "Z周Y翊R然X",
+        originalText: "新疆有什么好玩或好吃的，求推荐",
+        translatedText: "신장에 뭐 재밌거나 맛있는 거 있어? 추천 좀",
+        images: [],
+        actorReply: {
+          originalText: "新疆必吃 手抓饭",
+          translatedText: "신장 가면 꼭 먹어야 하는 건 서우좌판(手抓饭)"
+        }
+      },
+      {
+        id: 25080312,
+        username: "我有严重的恋哥癖_",
+        originalText: "哥哥你看起来很害怕😨",
+        translatedText: "오빠 완전 무서워 보이는데😨",
+        images: ["images/2508-scared.jpg"],
+        actorReply: {
+          originalText: "不不 只是情况紧急",
+          translatedText: "아니야 아니야 그냥 상황이 급해서"
+        }
+      },
+      {
+        id: 25080301,
+        username: "周翊然Tz",
+        originalText: "我去吃饭了 下次见",
+        translatedText: "나 밥 먹으러 가 다음에 봐",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 250804,
+    date: "2025-08-24",
+    weiboUrl: "https://weibo.com/5994946077/Q1naF6kZN",
+    authorName: "周翊然Tz",
+    originalText: "天黑之前 #本周报备##微博奇遇记#",
+    translatedText: "해 지기 전에 #本周报备##微博奇遇记#",
+    images: [
+      "images/2508-0824-01.jpg",
+      "images/2508-0824-02.jpg",
+      "images/2508-0824-03.jpg",
+      "images/2508-0824-04.jpg",
+      "images/2508-0824-05.jpg",
+      "images/2508-0824-06.jpg",
+      "images/2508-0824-07.jpg",
+      "images/2508-0824-08.jpg",
+      "images/2508-0824-09.jpg"
+    ],
+    comments: []
+  },
+  {
+    id: 250805,
+    date: "2025-08-31",
+    weiboUrl: "https://weibo.com/5994946077/Q2nvFoYOm",
+    authorName: "周翊然Tz",
+    originalText: "✈️💌#本周报备##微博奇遇记#",
+    translatedText: "✈️💌#本周报备##微博奇遇记#",
+    images: [
+      "images/2508-0831-01.jpg",
+      "images/2508-0831-02.jpg",
+      "images/2508-0831-03.jpg",
+      "images/2508-0831-04.jpg",
+      "images/2508-0831-05.jpg",
+      "images/2508-0831-06.jpg"
+    ],
+    comments: []
+  }
+];
