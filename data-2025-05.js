aa09f00929d4895c45d2eb6e1603272752bc12c0
zyr001122 <vazseyo@gmail.com>
2026-07-30 01:44:19 +0900

    Add files via upload

---
+++ data-2025-05.js
@@ -0,0 +1,233 @@
+const posts_202505 = [
+  {
+    id: 250501,
+    date: "2025-05-06",
+    weiboUrl: "https://weibo.com/5994946077/PqyHVyjmD",
+    authorName: "周翊然Tz",
+    originalText: "各位亲爱的甜粥，在今天想和大家说几句心里话。对于一个演员来说，每一次作品的官宣都是一次新的历练和成长，一路上感恩有你们陪伴和支持。\n\n从上个戏杀青到现在，经历了很多，有开心的假期，有沉下心的学习，还有从未尝试的综艺，我在试着不断激励自己努力，持续学习，来回报所有支持我的人。\n\n你们的关注我都看到了，感谢你们的鼓励，同时还想和大家说希望你们聚焦我的每一部作品，每一个角色，每一次尝试，无论线上线下，创建美好清朗的创作环境，就是对我最大的支持。和我一起珍爱行业环境，珍视合作伙伴，珍惜创作机会，珍重你我大家。\n\n一起加油，未来更美好！",
+    translatedText: "사랑하는 우리 甜粥(톈저우)들, 오늘은 다들한테 진심 어린 얘기 몇 마디 하고 싶어. 배우한테는 작품 하나하나의 공식 발표가 새로운 경험이자 성장의 기회인데, 그 과정에서 너네가 함께해주고 응원해준 거 항상 감사해.\n\n지난 작품 촬영 끝난 뒤로 지금까지 많은 일들이 있었어. 즐거운 휴가도 있었고, 차분히 배우는 시간도 있었고, 처음 도전해보는 예능도 있었어. 계속 나 자신을 격려하면서 꾸준히 배우려고 노력하고 있고, 그렇게 날 응원해준 모든 사람들한테 보답하고 싶어.\n\n너네가 관심 가져준 거 다 봤고, 응원해줘서 고마워. 그리고 하나 더 부탁하고 싶은 게 있는데, 내 작품 하나하나, 캐릭터 하나하나, 도전 하나하나에 집중해주면 좋겠어. 온라인이든 오프라인이든 아름답고 맑은 창작 환경을 만들어주는 게 나한테는 가장 큰 응원이야. 나랑 같이 이 업계 환경을 아끼고, 함께하는 사람들을 소중히 여기고, 창작의 기회를 소중히 하고, 서로를 존중해주자.\n\n다 같이 화이팅, 앞으로 더 좋은 날이 오길!",
+    images: [],
+    comments: []
+  },
+  {
+    id: 250502,
+    date: "2025-05-22",
+    weiboUrl: "https://weibo.com/5994946077/Pt2aRCJmM",
+    authorName: "周翊然Tz",
+    originalText: "hi 我来打个招呼[航天员]",
+    translatedText: "hi 인사하러 왔어[航天员]",
+    images: [],
+    comments: [
+      {
+        id: 25050201,
+        username: "周翊然Tz",
+        originalText: "等等要上台了 感觉比生日会还紧张[裂开]",
+        translatedText: "이제 곧 무대 올라가 생일파티보다 더 떨려[裂开]",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 25050202,
+        username: "周翊然Tz",
+        originalText: "笑死 你们真的把 法拉利和茅台玩坏了",
+        translatedText: "웃겨 죽겠네 너네 진짜 페라리랑 마오타이 갖고 놀다가 망가뜨렸어",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 25050203,
+        username: "周翊然Tz",
+        originalText: "今天拍的",
+        translatedText: "오늘 찍은 거",
+        images: ["images/2505-today-photo.jpg"],
+        actorReply: null
+      },
+      {
+        id: 25050204,
+        username: "樱花味的cherry",
+        originalText: "这个翅膀很帅诶",
+        translatedText: "이 날개 완전 멋지다",
+        images: ["images/2505-wings.jpg"],
+        actorReply: {
+          originalText: "[doge]",
+          translatedText: "[doge]"
+        }
+      },
+      {
+        id: 25050205,
+        username: "祈川十里春",
+        originalText: "小然，想知道羊肉抓饭里的羊肉膻味会很重嘛",
+        translatedText: "익연아, 양고기 필라프에 들어간 양고기 누린내 많이 나?",
+        images: ["images/2505-pilaf.jpg"],
+        actorReply: {
+          originalText: "不会 新疆的真的没有一点膻味！",
+          translatedText: "안 나 신장 건 진짜 하나도 안 누려!"
+        }
+      },
+      {
+        id: 25050206,
+        username: "染然然然RY",
+        originalText: "宝宝帮我想一个很适配的文案！！",
+        translatedText: "자기야 이거 딱 어울리는 캡션 하나만 생각해줘!!",
+        images: ["images/2505-caption-request.jpg"],
+        actorReply: {
+          originalText: "弹琴的人",
+          translatedText: "피아노 치는 사람"
+        }
+      },
+      {
+        id: 25050207,
+        username: "想和然见翊面",
+        originalText: "老公是镜子还是双胞胎",
+        translatedText: "자기야 이거 거울이야 아니면 쌍둥이야?",
+        images: ["images/2505-mirror-or-twin.jpg"],
+        actorReply: {
+          originalText: "…..",
+          translatedText: "….."
+        }
+      },
+      {
+        id: 25050208,
+        username: "我真的真的好困_",
+        originalText: "周翊然！我报了男生女生向前冲, 但是他们没有同意, 说我不符合条件。后来我去问他们, 原来问题在于,我不是男生也不是女生,我是为你而生！！！",
+        translatedText: "주익연! 나 남녀 커플 게임쇼(男生女生向前冲) 신청했는데, 걔네가 안 된다 그러더라, 자격 안 된다고. 그래서 나중에 물어보러 갔는데, 알고 보니까 문제는, 나는 남자도 아니고 여자도 아니고, 난 너를 위해 태어난 거였어!!!",
+        images: [],
+        actorReply: {
+          originalText: "…..",
+          translatedText: "….."
+        }
+      },
+      {
+        id: 25050209,
+        username: "鸡锁骨大小姐想让我告白",
+        originalText: "哥哥欧巴今天的耳返是不是生日会那个呀 嘻嘻嘻🤫🤫🤫",
+        translatedText: "오빠 오늘 인이어 생일파티 때 그거 맞지? 히히히🤫🤫🤫",
+        images: ["images/2505-in-ear-monitor.jpg"],
+        actorReply: {
+          originalText: "对的！",
+          translatedText: "맞아!"
+        }
+      },
+      {
+        id: 25050210,
+        username: "C733-Tz",
+        originalText: "这个茅台跑出去 你是不是找很久[兔子]",
+        translatedText: "이 마오타이 뛰쳐나가면 자기 한참 찾아다녔지?[兔子]",
+        images: ["images/2505-moutai-run.jpg"],
+        actorReply: {
+          originalText: "主要是爱自由[裂开]",
+          translatedText: "그냥 자유를 사랑하는 거지[裂开]"
+        }
+      },
+      {
+        id: 25050211,
+        username: "芝士椰啵脆",
+        originalText: "宝宝 录制综艺你觉得小法和小茅谁表现的更好？",
+        translatedText: "자기야 예능 찍을 때 파라(法)랑 마오(茅) 중에 누가 더 잘한 것 같아?",
+        images: ["images/2505-fa-vs-mao.jpg"],
+        actorReply: {
+          originalText: "今天才给茅台捡完💩",
+          translatedText: "오늘에서야 마오타이 응가 다 치웠어💩"
+        }
+      },
+      {
+        id: 25050212,
+        username: "是茵不是焉吖",
+        originalText: "然然看！我无意间抬头看到的❤️形状的云[憧憬][憧憬]",
+        translatedText: "익연아 봐봐! 우연히 고개 들었다가 본 하트❤️ 모양 구름이야[憧憬][憧憬]",
+        images: ["images/2505-heart-cloud.jpg"],
+        actorReply: {
+          originalText: "好看！",
+          translatedText: "예쁘다!"
+        }
+      },
+      {
+        id: 25050213,
+        username: "yi翊口小周",
+        originalText: "回复我一个符号也行",
+        translatedText: "기호 하나로만 답장해줘도 돼",
+        images: ["images/2505-just-a-symbol.jpg"],
+        actorReply: {
+          originalText: "👍",
+          translatedText: "👍"
+        }
+      },
+      {
+        id: 25050214,
+        username: "C733-Tz",
+        originalText: "我们今天运动会 你看这是什么[兔子]",
+        translatedText: "우리 오늘 운동회인데 이거 봐, 이게 뭐게?[兔子]",
+        images: ["images/2505-sports-day.jpg"],
+        actorReply: {
+          originalText: "卡面来打",
+          translatedText: "카드로 붙자"
+        }
+      },
+      {
+        id: 25050215,
+        username: "甜丸泡奶芙Zz_",
+        originalText: "宝宝快看～看俺上次打到的车",
+        translatedText: "자기야 빨리 봐봐～ 내가 저번에 뽑은 차 봐봐",
+        images: ["images/2505-my-car.jpg"],
+        actorReply: {
+          originalText: "好可爱 哈哈哈哈哈",
+          translatedText: "완전 귀엽다 ㅋㅋㅋㅋㅋ"
+        }
+      },
+      {
+        id: 25050216,
+        username: "噜撸源",
+        originalText: "桃花币有多少呀现在[兔子]",
+        translatedText: "지금 타오화 코인 얼마나 있어?[兔子]",
+        images: [],
+        actorReply: {
+          originalText: "负的[裂开]",
+          translatedText: "마이너스야[裂开]"
+        }
+      },
+      {
+        id: 25050217,
+        username: "扁桃体发炎不发言-",
+        originalText: "今晚弹钢琴？？？别出错[并不简单]",
+        translatedText: "오늘 밤 피아노 쳐??? 실수하지 마[并不简单]",
+        images: [],
+        actorReply: {
+          originalText: "保佑吧[裂开]",
+          translatedText: "기도해줘[裂开]"
+        }
+      },
+      {
+        id: 25050218,
+        username: "燥候22号",
+        originalText: "新疆天黑了吗宝",
+        translatedText: "신장은 벌써 어두워졌어 자기야?",
+        images: [],
+        actorReply: {
+          originalText: "还是大太阳呢",
+          translatedText: "아직 쨍쨍해"
+        }
+      },
+      {
+        id: 25050219,
+        username: "Gardeniae_栀落翊归橙",
+        originalText: "我六一要唱谁是你朋友哈哈哈~",
+        translatedText: "나 6월 1일에 누가 니 친구야 부를 거야ㅋㅋㅋ~",
+        images: [],
+        actorReply: {
+          originalText: "哈哈哈 能@ 给我看看吗",
+          translatedText: "ㅋㅋㅋ 나 태그해서 보여줄 수 있어?"
+        }
+      }
+    ]
+  },
+  {
+    id: 250503,
+    date: "2025-05-28",
+    weiboUrl: "https://weibo.com/5994946077/PtTCwm8ea",
+    authorName: "周翊然Tz",
+    originalText: "收藏了 下次见",
+    translatedText: "저장해놨어 다음에 봐",
+    images: ["images/2505-see-you-next.jpg"],
+    comments: []
+  }
+];
