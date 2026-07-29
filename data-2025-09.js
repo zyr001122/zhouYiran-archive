da4aa41153e6bb722412434362b69ceb06ebbf38
zyr001122 <vazseyo@gmail.com>
2026-07-30 03:52:09 +0900

    Update data-2025-09.js

--- data-2025-09.js
+++ data-2025-09.js
@@ -5,18 +5,18 @@ const posts_202509 = [
     weiboUrl: "https://weibo.com/5994946077/Q5JmgEOXu",
     authorName: "周翊然Tz",
     originalText: "我来了！",
-    translatedText: "왔어!",
+    translatedText: "나 왔어!",
     images: [],
     comments: [
       {
         id: 25090102,
         username: "偷吃一口饭饭",
         originalText: "准备给小茅小茅开巨型展览会如何[毕业快乐]",
-        translatedText: "샤오마오샤오마오한테 초대형 전시회 열어주는 거 어때?[毕业快乐]",
+        translatedText: "마오타이한테 초대형 전시회 열어주는 건 어때?[毕业快乐]",
         images: ["images/2509-fan-expo.jpg"],
         actorReply: {
           originalText: "有点意思",
-          translatedText: "좀 재밌네"
+          translatedText: "좀 재밌겠는데"
         }
       },
       {
@@ -27,14 +27,14 @@ const posts_202509 = [
         images: ["images/2509-typhoon-sunset.jpg"],
         actorReply: {
           originalText: "好看 注意安全",
-          translatedText: "예쁘다 조심해"
+          translatedText: "예쁘다 안전 조심해"
         }
       },
       {
         id: 25090104,
         username: "亿口甜粥_",
         originalText: "宝宝在成都玩的开心嘛！有没有去吃火锅[doge]",
-        translatedText: "자기야 청두에서 재밌게 놀았어? 훠궈 먹으러 갔어?[doge]",
+        translatedText: "익연아 청두에서 재밌게 놀았어? 훠궈는 먹었어?[doge]",
         images: ["images/2509-chengdu-hotpot.jpg"],
         actorReply: {
           originalText: "这次没吃上",
@@ -45,18 +45,18 @@ const posts_202509 = [
         id: 25090105,
         username: "蒋礼貌JOVZ",
         originalText: "老公你看可爱不[手指比心]",
-        translatedText: "자기야 이거 봐봐 귀엽지 않아?[手指比心]",
+        translatedText: "자기야 이거 봐봐 귀엽지?[手指比心]",
         images: ["images/2509-cute-or-not.jpg"],
         actorReply: {
           originalText: "可爱",
-          translatedText: "귀엽다"
+          translatedText: "귀여워"
         }
       },
       {
         id: 25090106,
         username: "_祈尔繁芜胜常春",
         originalText: "看桃花坞别人都在吃然后你能忍住不吃发誓一定要跟你一样能忍住自己😮[苦涩]\n看我给自己做的咸蛋黄豆腐蔬菜汤（减脂餐）[耶]",
-        translatedText: "타오화우 보면 다른 사람들 다 먹을 때 너는 참는 거 보고 나도 너처럼 참아보겠다고 다짐했어😮[苦涩]\n내가 직접 만든 계란노른자 두부 야채탕 봐줘 (다이어트 식단)[耶]",
+        translatedText: "도화오 보는데 다른 사람들 다 먹을 때 너는 참는 거 보고 나도 너처럼 참아야겠다고 다짐했어😮[苦涩]\n내가 직접 만든 계란노른자 두부 야채탕 봐봐 (다이어트 식단)[耶]",
         images: ["images/2509-diet-soup.jpg"],
         actorReply: {
           originalText: "[赞]",
@@ -67,7 +67,7 @@ const posts_202509 = [
         id: 25090107,
         username: "樱花味的cherry",
         originalText: "给你看这个[手指比心]",
-        translatedText: "이거 보여줄게[手指比心]",
+        translatedText: "보여줄게 있어[手指比心]",
         images: ["images/2509-look-this.jpg"],
         actorReply: {
           originalText: "[思考][嘘]",
@@ -78,11 +78,11 @@ const posts_202509 = [
         id: 25090108,
         username: "咬一口脆皮年糕_",
         originalText: "老公 有看到我们挂的大旗吗[哇]",
-        translatedText: "자기야 우리가 건 깃발 봤어?[哇]",
+        translatedText: "자기야 우리가 걸어둔 깃발 봤어?[哇]",
         images: ["images/2509-our-banner.jpg"],
         actorReply: {
           originalText: "看到了谢谢大家[钢铁侠]",
-          translatedText: "봤어 다들 고마워[钢铁侠]"
+          translatedText: "봤지 다들 너무 고마워[钢铁侠]"
         }
       },
       {
@@ -93,14 +93,14 @@ const posts_202509 = [
         images: ["images/2509-tongue-piercing.jpg"],
         actorReply: {
           originalText: "很痛吧？",
-          translatedText: "많이 아팠지?"
+          translatedText: "많이 아프지?"
         }
       },
       {
         id: 25090110,
         username: "啵唧翊口耶啵哇",
         originalText: "然子 可以推荐一首歌吗？",
-        translatedText: "然子야 노래 하나 추천해줄 수 있어?",
+        translatedText: "익욘아 노래 하나 추천해줄래?",
         images: ["images/2509-recommend-song.jpg"],
         actorReply: {
           originalText: "Beanie",
@@ -111,7 +111,7 @@ const posts_202509 = [
         id: 25090111,
         username: "-xixi陈-",
         originalText: "老公 看了这么多评论 来做个选择题吧[兔子]",
-        translatedText: "자기야 댓글 이렇게 많이 봤으니까 선택 문제 하나 내볼게[兔子]",
+        translatedText: "자기야 댓글 많이 봤으니까 객관식 문제 하나 내볼게[兔子]",
         images: ["images/2509-quiz.jpg"],
         actorReply: {
           originalText: "3",
@@ -126,14 +126,14 @@ const posts_202509 = [
         images: ["images/2509-dare-to-touch.jpg"],
         actorReply: {
           originalText: "敢拍照",
-          translatedText: "사진 찍을 용기는 있지"
+          translatedText: "사진 찍을 용기는 있어"
         }
       },
       {
         id: 25090113,
         username: "Cocooo---",
         originalText: "哥哥偶驾照就差科目四惹🤡",
-        translatedText: "오빠 나 운전면허 이제 4종목만 남았어🤡",
+        translatedText: "오빠 나 운전면허 이제 필기시험만 남았어🤡",
         images: ["images/2509-license-test.jpg"],
         actorReply: {
           originalText: "加油",
@@ -144,7 +144,7 @@ const posts_202509 = [
         id: 25090101,
         username: "周翊然Tz",
         originalText: "我先走了 大家好好工作好好上学好好生活",
-        translatedText: "나 먼저 갈게 다들 일 열심히, 공부 열심히, 잘 지내",
+        translatedText: "먼저 가볼게 다들 일 열심히 하고 공부 열심히 하고 잘 지내",
         images: [],
         actorReply: null
       }
