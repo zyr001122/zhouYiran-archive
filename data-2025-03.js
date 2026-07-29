991c0579dc1f41d8a6c785042b5e619447a4719c
zyr001122 <vazseyo@gmail.com>
2026-07-29 22:22:21 +0900

    Update data-2025-03.js

--- data-2025-03.js
+++ data-2025-03.js
@@ -1,169 +1,177 @@
-// 2025-03-22 게시물 - 새 data-2025-03.js 파일의 posts_202503 배열로 쓰거나 기존 파일에 이어붙이면 됩니다.
-
-{
-  id: 250301,
-  date: "2025-03-22",
-  weiboUrl: "https://weibo.com/5994946077/PjJvlEQbr",
-  authorName: "周翊然Tz",
-  originalText: "周翊然超话 大家在干嘛 我来了！",
-  translatedText: "周翊然超话 다들 뭐 하고 있어, 나 왔어!",
-  images: [],
-  comments: [
-    {
-      id: 25030101,
-      username: "周翊然Tz",
-      originalText: "新疆真的很冷",
-      translatedText: "신장 진짜 춥다",
-      images: [],
-      actorReply: null
-    },
-    {
-      id: 25030102,
-      username: "周翊然Tz",
-      originalText: "我先走了各位 在叫我了 下次见！！！",
-      translatedText: "나 먼저 갈게 다들, 부르네, 다음에 봐!!!",
-      images: [],
-      actorReply: null
-    },
-    {
-      id: 25030103,
-      username: "1点大王",
-      originalText: "好玩吗桃花坞！",
-      translatedText: "타오화우 재밌어?!",
-      images: [],
-      actorReply: {
-        originalText: "好玩 就是每天吃很多 [doge]",
-        translatedText: "재밌어, 근데 매일 많이 먹어 [doge]"
+const posts_202503 = [
+  {
+    id: 250301,
+    date: "2025-03-22",
+    weiboUrl: "https://weibo.com/5994946077/PjJvlEQbr",
+    authorName: "周翊然Tz",
+    originalText: "大家在干嘛  我来了！",
+    translatedText: "다들 뭐 하고 있어? 나 왔어!",
+    images: [],
+    comments: [
+      {
+        id: 25030101,
+        username: "周翊然Tz",
+        originalText: "新疆真的很冷",
+        translatedText: "신장 진짜 춥다",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 25030103,
+        username: "1点大王",
+        originalText: "好玩吗桃花坞！",
+        translatedText: "타오화우 재밌어?!",
+        images: [],
+        actorReply: {
+          originalText: "好玩 就是每天吃很多",
+          translatedText: "재밌어 근데 맨날 많이 먹어"
+        }
+      },
+      {
+        id: 25030104,
+        username: "翊分钟恋人z",
+        originalText: "哥你又开车了",
+        translatedText: "오빠 또 운전하네",
+        images: [
+          "images/2503-driving.jpg"
+        ],
+        actorReply: {
+          originalText: "唯一能干的事儿  咳咳",
+          translatedText: "유일하게 잘 하는 거지 흠흠"
+        }
+      },
+      {
+        id: 25030105,
+        username: "樱花味的cherry",
+        originalText: "吃到什么好吃的了吗",
+        translatedText: "뭐 맛있는 거 먹었어?",
+        images: [],
+        actorReply: {
+          originalText: "今天吃的硬菜 烤全羊",
+          translatedText: "오늘 든든한 거 먹었어 양 통구이"
+        }
+      },
+      {
+        id: 25030106,
+        username: "先干饭吧Tz",
+        originalText: "来了 在干嘛",
+        translatedText: "왔어 뭐 하고 있어?",
+        note: "원문 동영상",
+        images: [],
+        actorReply: {
+          originalText: "此时此刻坐在沙发上 在看你们的评论",
+          translatedText: "지금 이 순간 소파에 앉아서 너네 댓글 보는 중"
+        }
+      },
+      {
+        id: 25030107,
+        username: "z-吃翊碗小面",
+        originalText: "你再不回，我真的生气了",
+        translatedText: "너 계속 답장 안 해주면, 진짜 화낼 거야",
+        images: [
+          "images/2503-angry.jpg"
+        ],
+        actorReply: {
+          originalText: "我来了",
+          translatedText: "나 왔어"
+        }
+      },
+      {
+        id: 25030108,
+        username: "祈川十里春",
+        originalText: "好奇这个番茄炒蛋什么味的",
+        translatedText: "이 토마토계란볶음 무슨 맛인지 궁금해",
+        images: [
+          "images/2503-tomato-egg.jpg"
+        ],
+        actorReply: {
+          originalText: "很下饭就对了",
+          translatedText: "밥이랑 먹으면 딱이야"
+        }
+      },
+      {
+        id: 25030109,
+        username: "r男朋友真的只想开车",
+        originalText: "番茄排骨汤，我做的",
+        translatedText: "토마토 갈비탕, 내가 만든 거야",
+        images: [
+          "images/2503-tomato-ribsoup.jpg"
+        ],
+        actorReply: {
+          originalText: "wow 我今天也准备做这个",
+          translatedText: "wow 나도 오늘 이거 만들어볼까 했는데"
+        }
+      },
+      {
+        id: 25030110,
+        username: "牙不吃香菜",
+        originalText: "真的不理我吗",
+        translatedText: "진짜 나 신경 안 써줄 거야?",
+        images: [
+          "images/2503-really-ignore-me.jpg"
+        ],
+        actorReply: {
+          replyImages: ["images/2503-tz-sticker-1.jpg"]
+        }
+      },
+      {
+        id: 25030111,
+        username: "翊条魚干",
+        originalText: "老公什么时候炒",
+        translatedText: "자기야 언제 볶아?",
+        images: [
+          "images/2503-when-cook.jpg"
+        ],
+        actorReply: {
+          originalText: "还差一些食材",
+          translatedText: "재료가 좀 모자라"
+        }
+      },
+      {
+        id: 25030112,
+        username: "翊条魚干",
+        originalText: "老公猜猜这是啥",
+        translatedText: "자기야 이게 뭔지 맞혀봐",
+        images: [
+          "images/2503-guess-this.jpg"
+        ],
+        actorReply: {
+          originalText: "少了一个颜色",
+          translatedText: "색깔 하나가 부족해"
+        }
+      },
+      {
+        id: 25030113,
+        username: "Jia一口鲫鱼饼_",
+        originalText: "挑一个回我吧 周翊然",
+        translatedText: "하나 골라서 나한테 답장 좀 해줘 주익연아",
+        images: [
+          "images/2503-pick-one.jpg"
+        ],
+        actorReply: {
+          originalText: "7. 非常的开心 然后也不认生 到处跑",
+          translatedText: "7번. 완전 신났고 낯도 안 가리고 여기저기 뛰어다녀"
+        }
+      },
+      {
+        id: 25030114,
+        username: "_5uu干脆面",
+        originalText: "茅台法拉利玩的开心吗",
+        translatedText: "마오타이랑 페라리 재밌게 놀았어?",
+        images: [],
+        actorReply: {
+          originalText: "法拉利开心 茅台会被关起来 因为到处拉！",
+          translatedText: "페라리는 신났고 마오타이는 갇혀 있을걸 여기저기 실수해서!"
+        }
+      },
+      {
+        id: 25030102,
+        username: "周翊然Tz",
+        originalText: "我先走了各位 在叫我了 下次见！！！",
+        translatedText: "나 먼저 갈게 얘들아 나 부르네 다음에 봐!!!",
+        images: [],
+        actorReply: null
       }
-    },
-    {
-      id: 25030105,
-      username: "翊分钟恋人z",
-      originalText: "哥你又开车了 🤔",
-      translatedText: "오빠 또 아슬아슬한 드립 치네 🤔",
-      images: [],
-      actorReply: {
-        originalText: "唯一能干的事儿 咳咳",
-        translatedText: "유일하게 할 수 있는 거잖아, 흠흠"
-      }
-    },
-    {
-      id: 25030107,
-      username: "樱花味的cherry",
-      originalText: "吃到什么好吃的了吗",
-      translatedText: "맛있는 거 뭐 먹었어?",
-      images: [
-        "images/2503-0322-fan-face.jpg"
-      ],
-      actorReply: {
-        originalText: "今天吃的硬菜 烤全羊 [doge]",
-        translatedText: "오늘은 제대로 된 요리 먹었어, 통양구이 [doge]"
-      }
-    },
-    {
-      id: 25030109,
-      username: "先干饭吧Tz",
-      originalText: "来了 在干嘛 😆😆😆",
-      translatedText: "왔어, 뭐 하고 있어 😆😆😆",
-      images: [],
-      actorReply: {
-        originalText: "此时此刻坐在沙发上 在看你们的评论",
-        translatedText: "지금 이 순간 소파에 앉아서 너희들 댓글 보고 있어"
-      }
-    },
-    {
-      id: 25030111,
-      username: "z-吃翊碗小面",
-      originalText: "你再不回，我真的生气了",
-      translatedText: "너 계속 답장 안 하면 나 진짜 화낼 거야",
-      images: [
-        "images/2503-0322-image1.jpg"
-      ],
-      actorReply: {
-        originalText: "我来了",
-        translatedText: "왔어"
-      }
-    },
-    {
-      id: 25030113,
-      username: "祈川十里春",
-      originalText: "好奇这个番茄炒蛋什么味的 🤩",
-      translatedText: "이 토마토 계란볶음 무슨 맛인지 궁금하다 🤩",
-      images: [],
-      actorReply: {
-        originalText: "很下饭就对了",
-        translatedText: "밥이랑 딱이야"
-      }
-    },
-    {
-      id: 25030115,
-      username: "r男朋友真的只想开车",
-      originalText: "番茄排骨汤，我做的 😁",
-      translatedText: "토마토 갈비탕, 내가 만들었어 😁",
-      images: [],
-      actorReply: {
-        originalText: "wow 我今天也准备做这个",
-        translatedText: "우와 나도 오늘 이거 만들려고 했는데"
-      }
-    },
-    {
-      id: 25030117,
-      username: "牙不吃香菜",
-      originalText: "真的不理我吗",
-      translatedText: "진짜 나 무시하는 거야?",
-      images: [
-        "images/2503-0322-image2.jpg"
-      ],
-      actorReply: {
-        originalText: "images/2503-0322-tz-sticker-1.jpg",
-        translatedText: "images/2503-0322-tz-sticker-1.jpg"
-      }
-    },
-    {
-      id: 25030119,
-      username: "翊条魚干",
-      originalText: "老公什么时候炒 [doge][doge]",
-      translatedText: "여보 언제 볶을 거야 [doge][doge]",
-      images: [],
-      actorReply: {
-        originalText: "还差一些食材",
-        translatedText: "재료가 좀 더 필요해"
-      }
-    },
-    {
-      id: 25030121,
-      username: "翊条魚干",
-      originalText: "老公猜猜这是啥",
-      translatedText: "여보 이게 뭔지 맞혀봐",
-      images: [
-        "images/2503-0322-broken-face.jpg"
-      ],
-      actorReply: {
-        originalText: "少了一个颜色 [doge]",
-        translatedText: "색깔 하나가 빠졌네 [doge]"
-      }
-    },
-    {
-      id: 25030123,
-      username: "Jia一口鲫鱼饼_",
-      originalText: "挑一个回我吧 [躺平][躺平] 周翊然 @周翊然Tz",
-      translatedText: "하나 골라서 답장해줘 [躺平][躺平] 周翊然",
-      images: [],
-      actorReply: {
-        originalText: "7. 非常的开心 然后也不认生 到处跑",
-        translatedText: "7번. 완전 신났고, 낯도 안 가리고 여기저기 뛰어다니네"
-      }
-    },
-    {
-      id: 25030125,
-      username: "_5uu干脆面",
-      originalText: "茅台法拉利玩的开心吗",
-      translatedText: "마오타이랑 페라리 재밌게 놀았어?",
-      images: [],
-      actorReply: {
-        originalText: "法拉利开心 茅台会被关起来 因为到处拉！",
-        translatedText: "페라리는 신났는데 마오타이는 가둬야 할 것 같아, 여기저기 응가하고 다녀서!"
-      }
-    }
-  ]
-}
+    ]
+  }
+];
