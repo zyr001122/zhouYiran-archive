4193ee01ff186fba3c689a61dd1814d665ef801c
zyr001122 <vazseyo@gmail.com>
2026-07-29 23:18:37 +0900

    Add files via upload

---
+++ data-2025-04.js
@@ -0,0 +1,197 @@
+const posts_202504 = [
+  {
+    id: 250401,
+    date: "2025-04-22",
+    weiboUrl: "https://weibo.com/5994946077/Pos6l7vBo",
+    authorName: "周翊然Tz",
+    originalText: "hello 各位在干嘛",
+    translatedText: "hello 다들 뭐 해?",
+    images: [],
+    comments: [
+      {
+        id: 25040102,
+        username: "翊条魚干",
+        originalText: "老公你看[可怜]",
+        translatedText: "자기야 봐봐[可怜]",
+        images: ["images/2504-look-here.jpg"],
+        actorReply: {
+          originalText: "赛车手来了[航天员][航天员][航天员]",
+          translatedText: "레이서 왔네[航天员][航天员][航天员]"
+        }
+      },
+      {
+        id: 25040103,
+        username: "扁桃体发炎不发言-",
+        originalText: "问问哥哥学骑马难不难？马不会到处乱跑吧 一直想学 有点害怕来着",
+        translatedText: "오빠한테 승마 어려운지 물어봐줘? 말이 막 돌아다니진 않겠지 계속 배우고 싶었는데 좀 무서워서",
+        images: [],
+        actorReply: {
+          originalText: "我觉得不难 但马会欺负你 它如果知道你不会骑就会欺负你[裂开]",
+          translatedText: "안 어려운 것 같아 근데 말이 너 무시할 수도 있어 말이 니가 못 탈 줄 알면 무시하거든[裂开]"
+        }
+      },
+      {
+        id: 25040104,
+        username: "落日屿橘",
+        originalText: "不理我是吧！我让法拉利揍你！💢💢💢",
+        translatedText: "나 무시하는 거지! 페라리 시켜서 너 패줄 거야!💢💢💢",
+        note: "원문 동영상",
+        images: [],
+        actorReply: {
+          originalText: "[doge]",
+          translatedText: "[doge]",
+          replyImages: ["images/2504-racer-reply.jpg"]
+        }
+      },
+      {
+        id: 25040105,
+        username: "Coisiniqqo",
+        originalText: "喵喵喵老公你在干嘛呀[老师爱你][老师爱你]",
+        translatedText: "야옹야옹 자기야 뭐 해[老师爱你][老师爱你]",
+        images: ["images/2504-meow.jpg"],
+        actorReply: {
+          originalText: "刚刚骑完马 歇会儿[裂开]",
+          translatedText: "방금 승마 끝내고 좀 쉬는 중[裂开]"
+        }
+      },
+      {
+        id: 25040106,
+        username: "综上所述你最萌",
+        originalText: "恭喜我顺利收到offer好不好宝宝[给你小心心]",
+        translatedText: "나 오퍼 무사히 받았어 축하해줘 자기야[给你小心心]",
+        images: ["images/2504-offer.jpg"],
+        actorReply: {
+          originalText: "🙏！",
+          translatedText: "🙏!"
+        }
+      },
+      {
+        id: 25040107,
+        username: "爱翅小西瓜_",
+        originalText: "老公你觉得重庆火锅好吃还是成都火锅好吃[思考][思考]",
+        translatedText: "자기야 충칭 훠궈랑 청두 훠궈 중에 뭐가 더 맛있어?[思考][思考]",
+        images: [],
+        actorReply: {
+          originalText: "重庆！",
+          translatedText: "충칭!"
+        }
+      },
+      {
+        id: 25040108,
+        username: "扁桃体发炎不发言-",
+        originalText: "想采访一下周翊然对于赶羊有什么心得和技巧[哈哈][哈哈]",
+        translatedText: "주익연한테 양몰이에 대한 노하우랑 팁 좀 인터뷰하고 싶어[哈哈][哈哈]",
+        images: [],
+        actorReply: {
+          originalText: "我的心得是： 不要觉得自己的腿能跑得过羊 在自己体力耗尽以前 尽早的求助骑马的牧民大哥[裂开]",
+          translatedText: "내 노하우는: 내 다리가 양보다 빠를 거라고 생각하지 말 것 체력 다 떨어지기 전에 최대한 빨리 말 탄 목동 형한테 도움 요청할 것[裂开]"
+        }
+      },
+      {
+        id: 25040109,
+        username: "拜托了哥哥酱",
+        originalText: "你那里今天天气怎么样",
+        translatedText: "거기 오늘 날씨 어때?",
+        images: ["images/2504-weather.jpg"],
+        actorReply: {
+          originalText: "两天经历了 冬天和夏天",
+          translatedText: "이틀 만에 겨울이랑 여름을 다 겪었어"
+        }
+      },
+      {
+        id: 25040110,
+        username: "染然然然RY",
+        originalText: "宝宝请帮我给这张图想个文案[好喜欢]",
+        translatedText: "자기야 이 사진 캡션 좀 생각해줘[好喜欢]",
+        images: ["images/2504-caption-request.jpg"],
+        actorReply: {
+          originalText: "骑马的人",
+          translatedText: "말 타는 사람"
+        }
+      },
+      {
+        id: 25040111,
+        username: "甜丸泡奶芙Zz_",
+        originalText: "眼熟吗宝宝",
+        translatedText: "낯익지 자기야?",
+        images: ["images/2504-familiar.jpg"],
+        actorReply: {
+          originalText: "有意思",
+          translatedText: "재밌네"
+        }
+      },
+      {
+        id: 25040112,
+        username: "别丸我啦",
+        originalText: "哥哥最近特别特别累",
+        translatedText: "오빠 요즘 완전 완전 피곤해",
+        images: [],
+        actorReply: {
+          replyImages: ["images/2504-tz-reply-1.jpg"]
+        }
+      },
+      {
+        id: 25040113,
+        username: "棉花酥酥糖",
+        originalText: "哥，骑马，赛车，滑雪哪个好玩儿些",
+        translatedText: "오빠, 승마, 레이싱, 스키 중에 뭐가 더 재밌어?",
+        images: [],
+        actorReply: {
+          originalText: "赛车》滑雪〉骑马",
+          translatedText: "레이싱 > 스키 > 승마"
+        }
+      },
+      {
+        id: 25040114,
+        username: "-xixi陈-",
+        originalText: "哥 今天怎么那么喜欢用感叹号",
+        translatedText: "오빠 오늘 왜 이렇게 느낌표를 좋아해?",
+        images: [],
+        actorReply: {
+          originalText: "让文字看起来有温度一点[裂开]",
+          translatedText: "글자가 좀 더 따뜻해 보이라고[裂开]"
+        }
+      },
+      {
+        id: 25040115,
+        username: "YuniSiltni",
+        originalText: "哥哥有没有看过 紫罗兰的永恒花园 是我的白月光推荐给我的 我还没看完[淡淡的][淡淡的]",
+        translatedText: "오빠 바이올렛 에버가든 본 적 있어? 내 최애가 추천해준 건데 나 아직 다 못 봤어[淡淡的][淡淡的]",
+        images: [],
+        actorReply: {
+          originalText: "看过 好看的！",
+          translatedText: "봤어 재밌더라!"
+        }
+      },
+      {
+        id: 25040116,
+        username: "我真的只想开机车啊",
+        originalText: "猜我做完什么了",
+        translatedText: "내가 뭐 다 끝냈는지 맞혀봐",
+        images: ["images/2504-guess-what.jpg"],
+        actorReply: {
+          originalText: "后面人脸吓我一跳",
+          translatedText: "뒤에 사람 얼굴 보고 깜짝 놀랐어"
+        }
+      },
+      {
+        id: 25040117,
+        username: "翊芷涵_Tz",
+        originalText: "茅台，在你身边吗",
+        translatedText: "마오타이, 네 옆에 있어?",
+        images: [],
+        actorReply: {
+          replyImages: ["images/2504-tz-reply-2.jpg"]
+        }
+      },
+      {
+        id: 25040101,
+        username: "周翊然Tz",
+        originalText: "走了各位 要去录制了 下次见吧！[收到]",
+        translatedText: "다들 나 갈게 촬영하러 가야 해서 다음에 봐![收到]",
+        images: [],
+        actorReply: null
+      }
+    ]
+  }
+];
