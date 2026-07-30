8ea36972b779aac619a0676d13e75bad8a715489
zyr001122 <vazseyo@gmail.com>
2026-07-30 23:24:07 +0900

    Update data-2026-06.js

--- data-2026-06.js
+++ data-2026-06.js
@@ -9,6 +9,24 @@ const posts_202606 = [
     images: [],
     comments: [
       {
+        id: 26060107,
+        username: "周翊然Tz",
+        originalText: "试一下这个新的功能，听说现在可以发语音",
+        translatedText: "새 기능이 있다 그래서 한번 써보려고, 이제 음성도 보낼 수 있다던데",
+        note: "원문 음성 메시지",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 26060127,
+        username: "周翊然Tz",
+        originalText: "",
+        translatedText: "최근에 많은 분들을 봤어요 사연래를 통해서 저를 알게 된 분들 정말 영광스럽고 또 너무 기뻐요 그리고 많은 팬분들이 저한테 남겨준 댓글이랑 남겨주신 메시지도 봤는데 저를 응원해 주시고 인정해 주시는 내용이 많더라고요 전부 다 확인했고 정말 모든분들께 감사드립니다 사연래에 대한 여러분의 인정에도 너무 감사드리고 그리고 팬분들께도 정말 고마워요 지금까지 변함없이 응원해 주셔서 어.. 사실 저는 가끔ㅎㅎ 팬분들이 저보다 더 힘들 수도 있다는 걸 알고 있어요 그래서… 저는 이게 제 영광이라고 생각하고 너무 감사해요 그래서 이 자리를 빌려 여러분께 정중하게 감사 인사를 드리고 싶어요",
+        note: "원문 음성 메시지 (51초)",
+        images: [],
+        actorReply: null
+      },
+      {
         id: 26060101,
         username: "祈川十里春",
         originalText: "哥哥 这是你的新宠吗！什么时候给我们介绍介绍哈哈哈",
@@ -36,8 +54,6 @@ const posts_202606 = [
         originalText: "hey 老公 看我钓的鱼！但是上钩的都太小了最后都放生了你最近钓鱼有啥收获不",
         translatedText: "hey 자기야 내가 낚은 물고기 봐봐! 근데 걸린 게 다 너무 작아서 그냥 다 놓아줬어 넌 요즘 낚시해서 뭐 좀 건진 거 있어?",
         note: "원문 음성 메시지",
-        audio: "audio/2606-voice-c-zhichiliangwanfan1.mp3",
-        audioDuration: 3,
         images: ["images/2606-fishing.jpg"],
         actorReply: {
           originalText: "你这个是抬调还是是露亚？看着像抬掉?",
@@ -78,24 +94,11 @@ const posts_202606 = [
         }
       },
       {
-        id: 26060107,
-        username: "周翊然Tz",
-        originalText: "试一下这个新的功能，听说现在可以发语音",
-        translatedText: "새 기능이 있다 그래서 한번 써보려고, 이제 음성도 보낼 수 있다며",
-        note: "원문 음성 메시지",
-        audio: "audio/2606-voice-0622-1752-self.mp3",
-        audioDuration: 5,
-        images: [],
-        actorReply: null
-      },
-      {
         id: 26060108,
         username: "是我的小土豆-Tz",
         originalText: "老公 我拍的日落怎么样",
         translatedText: "자기야 내가 찍은 노을 어때",
         note: "원문 음성 메시지",
-        audio: "audio/2606-voice-c-shiwodetudou.mp3",
-        audioDuration: 3,
         images: ["images/2606-sunset-photo-2.jpg"],
         actorReply: {
           originalText: "不错不错",
@@ -108,8 +111,6 @@ const posts_202606 = [
         originalText: "跟你好朋友昨天这么同步 啥时候再还原一个同款表情[钢铁侠]",
         translatedText: "어제 절친 아니랄까봐 표정도 완전 똑같던데 언제 또 같은 표정 재현해줄 거야[钢铁侠]",
         note: "원문 음성 메시지",
-        audio: "audio/2606-voice-c-zhichiliangwanfan2.mp3",
-        audioDuration: 4,
         images: ["images/2606-same-expression.jpg"],
         actorReply: {
           originalText: "",
@@ -133,8 +134,6 @@ const posts_202606 = [
         originalText: "为什么！为什么！为什么我发了一堆话换来的还是你的只言片语，你真的好冷漠 你的冷漠就像北极的冰川你的冷漠好像从喜马拉雅山上倒下来的一瓶水把山脚下热情的我浇的浑身湿透手脚发抖告诉我究竟怎么样才能让你对我重拾往日的热情我们难道真的已经回不去了吗！我的心就像浸泡在负一百度的冰水里一样麻木",
         translatedText: "왜! 왜! 왜 나는 한참 얘기해도 넌 왜 몇 마디만 보내는건데? 너 진짜 차갑다 너의 차가움은 마치 북극의 얼음 같아. 너의 차가움은 히말라야산에서 쏟아지는 얼음물이 뜨거운 내 마음속 열정에 부어지는 것 같아, 온몸이 다 젖어서 손발까지 떨려. 대체 어떻게 해야 네가 예전처럼 다시 나한테 열정을 보여줄 수 있는지 말해줘 우리 진짜 이대로 돌아갈 수 없는 거 아니지?! 내 마음은 마치 영하 100도 물에 잠긴 것처럼 마비된 것 같다고",
         note: "원문 음성 메시지",
-        audio: "audio/2606-voice-c-ranzhimaokan.mp3",
-        audioDuration: 2,
         images: ["images/2606-cold-drawing.jpg"],
         actorReply: {
           originalText: "",
@@ -191,8 +190,6 @@ const posts_202606 = [
         originalText: "周翊然你这个是咋做到的？有教程可学吗哈哈哈",
         translatedText: "주익연 이거 어떻게 한 거야? 따라하게 강의 좀ㅋㅋㅋ",
         note: "원문 음성 메세지",
-        audio: "audio/2606-voice-c-zhouniuniu.mp3",
-        audioDuration: 5,
         images: ["images/2606-how-to.jpg"],
         actorReply: {
           originalText: "这个就是这样做，然后那样做，然后再这样做就可以了",
@@ -205,8 +202,6 @@ const posts_202606 = [
         originalText: "哥哥快看 小狗对烧卖🐶👅",
         translatedText: "오빠 봐봐 울 강아지가🐶👅",
         note: "원문 음성 메세지",
-        audio: "audio/2606-voice-c-yikouhanshi.mp3",
-        audioDuration: 3,
         images: ["images/2606-dog-siomai.jpg"],
         actorReply: {
           originalText: "",
@@ -225,17 +220,6 @@ const posts_202606 = [
         }
       },
       {
-        id: 26060127,
-        username: "周翊然Tz",
-        originalText: "",
-        translatedText: "최근에 많은 분들을 봤어요 사연래를 통해서 저를 알게 된 분들 정말 영광스럽고 또 너무 기뻐요 그리고 많은 팬분들이 저한테 남겨준 댓글이랑 남겨주신 메시지도 봤는데 저를 응원해 주시고 인정해 주시는 내용이 많더라고요 전부 다 확인했고 정말 모든분들께 감사드립니다 사연래에 대한 여러분의 인정에도 너무 감사드리고 그리고 팬분들께도 정말 고마워요 지금까지 변함없이 응원해 주셔서 어.. 사실 저는 가끔ㅎㅎ 팬분들이 저보다 더 힘들 수도 있다는 걸 알고 있어요 그래서… 저는 이게 제 영광이라고 생각하고 너무 감사해요 그래서 이 자리를 빌려 여러분께 정중하게 감사 인사를 드리고 싶어요",
-        note: "원문 음성 메시지 (51초)",
-        audio: "audio/2606-voice-0622-1807-self.mp3",
-        audioDuration: 56,
-        images: [],
-        actorReply: null
-      },
-      {
         id: 26060119,
         username: "圣焄妻",
         originalText: "哥哥巴黎有没有手搓五老哥[可怜]",
@@ -244,18 +228,8 @@ const posts_202606 = [
         images: [],
         actorReply: {
           originalText: "我只能尽力而为",
-          translatedText: "나도 최선을 다하고 있어",
-          audio: "audio/2606-voice-c-shengxunqi.mp3",
-          audioDuration: 5
+          translatedText: "나도 최선을 다하고 있어"
         }
-      },
-      {
-        id: 26060128,
-        username: "圣焄妻",
-        originalText: "[@微博智搜] 夸一下周翊然",
-        translatedText: "[@微博智搜] 주익연 칭찬 좀 해줘",
-        images: [],
-        actorReply: null
       },
       {
         id: 26060120,
@@ -307,8 +281,6 @@ const posts_202606 = [
         originalText: "老公这次vlog录个1小时的时长就行，别太辛苦了[并不简单]",
         translatedText: "자기야 이번 브이로그는 1시간 정도 분량이면 돼, 너무 무리하지 마[并不简单]",
         note: "원문 음성 메세지",
-        audio: "audio/2606-voice-c-shiyuegege.mp3",
-        audioDuration: 2,
         images: [],
         actorReply: {
           originalText: "好的好的，我尽力而为",
