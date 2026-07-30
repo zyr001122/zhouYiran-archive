9ddecb6a7108077df9924be7cc3286256e55a428
zyr001122 <vazseyo@gmail.com>
2026-07-30 15:02:01 +0900

    Update data-2025-11.js

--- data-2025-11.js
+++ data-2025-11.js
@@ -21,7 +21,7 @@ const posts_202511 = [
         username: "是我的小土豆-Tz",
         originalText: "然然 我今天也过生日！🎂想要你的祝福～",
         translatedText: "익연아 나도 오늘 생일이야! 🎂 너한테 축하 받고 싶어~",
-        images: ["images/2511-birthday-too.jpg"],
+        images: ["images/2511-see-you-track.jpg"],
         actorReply: {
           originalText: "生日快乐！",
           translatedText: "생일 축하해!"
@@ -32,7 +32,7 @@ const posts_202511 = [
         username: "樱花味的cherry",
         originalText: "哥哥生日快乐！每天都快乐！我的愿望就是你许下的愿望都能实现！要一直一直幸福！",
         translatedText: "오빠 생일 축하해! 매일매일 행복하길! 내 소원은 오빠 소원이 다 이루어지는 거야! 항상 언제나 행복해야 해!",
-        images: ["images/2511-my-wish.jpg"],
+        images: ["images/2511-hat-like-socks.jpg"],
         actorReply: {
           originalText: "❤️",
           translatedText: "❤️"
@@ -43,7 +43,7 @@ const posts_202511 = [
         username: "心动壹年",
         originalText: "今天去鸡鸣寺替你祈福了！25岁一定要顺利！",
         translatedText: "오늘 지밍사 가서 너 대신 기도하고 왔어! 25살 꼭 모든 일들이 순조롭길!",
-        images: ["images/2511-temple-prayer.jpg"],
+        images: ["images/2511-give-me-xiaoxiao.jpg"],
         actorReply: {
           originalText: "也记得给自己祈福一下！",
           translatedText: "너도 널 위해 기도하는 거 잊지 마!"
@@ -65,7 +65,7 @@ const posts_202511 = [
         username: "想吃荔枝冰_",
         originalText: "希望有机会我们赛道上见好吗",
         translatedText: "기회 되면 우리 서킷에서 만나는 거 어때",
-        images: ["images/2511-see-you-track.jpg"],
+        images: ["images/2511-birthday-photo.jpg"],
         actorReply: {
           originalText: "来真的？",
           translatedText: "진심이야?"
@@ -75,8 +75,8 @@ const posts_202511 = [
         id: 25110106,
         username: "小圆肚子啦啦啦",
         originalText: "老公其实我想说，你画的帽子像袜子[doge]",
-        translatedText: "자기야 사실 하고 싶은 말이 있는데, 네가 그린 모자 양말 닮았어[doge]",
-        images: ["images/2511-hat-like-socks.jpg"],
+        translatedText: "자기야 사실 하고 싶은 말이 있는데, 자기가 그린 모자 양말 닮았어[doge]",
+        images: ["images/2511-temple-prayer.jpg"],
         actorReply: {
           originalText: "？",
           translatedText: "?"
@@ -98,7 +98,7 @@ const posts_202511 = [
         username: "魚肉蛋黄酥o_",
         originalText: "来来来 给你找的耳朵痛感图，你参考一下吧，其实耳垂不是很疼的！！！看见谢燕来戴耳钉的造型我觉得超级好看！",
         translatedText: "자자자 귀 통증표 가져왔으니까 참고해, 사실 귓볼은 그렇게 안 아파!!! 사연래 때 귀걸이 한 거 보니까 완전 예쁘더라!",
-        images: ["images/2511-ear-pain-chart.jpg"],
+        images: ["images/2511-are-you-serious.jpg"],
         actorReply: {
           originalText: "谢谢您",
           translatedText: "감사합니다"
@@ -109,7 +109,7 @@ const posts_202511 = [
         username: "-翊雨TZ-",
         originalText: "然然 你能不能把笑笑送给我",
         translatedText: "익연아 샤오샤오 나 주면 안 돼?",
-        images: ["images/2511-give-me-xiaoxiao.jpg"],
+        images: ["images/2511-ear-piercing.jpg"],
         actorReply: {
           originalText: "肯定不行啊",
           translatedText: "당연히 안 되지"
@@ -142,7 +142,7 @@ const posts_202511 = [
         username: "争争翊上Tz",
         originalText: "老公 你怎么冒鼻涕泡了",
         translatedText: "자기야 너 왜 콧물 방울이 났어",
-        images: ["images/2511-runny-nose.jpg"],
+        images: ["images/2511-ear-pain-chart.jpg"],
         actorReply: {
           originalText: "冬天到了",
           translatedText: "겨울이 왔구만"
@@ -153,7 +153,7 @@ const posts_202511 = [
         username: "翊点点喜欢_楠",
         originalText: "哥哥给你看我的耳洞",
         translatedText: "오빠 나 귀 뚫은 거 보여줄게",
-        images: ["images/2511-ear-piercing.jpg"],
+        images: ["images/2511-my-wish.jpg"],
         actorReply: {
           originalText: "打的时候疼不",
           translatedText: "뚫을 때 아팠어?"
@@ -164,7 +164,7 @@ const posts_202511 = [
         username: "翊碗烤冷面",
         originalText: "哥哥认真的吗",
         translatedText: "오빠 진심이야?",
-        images: ["images/2511-are-you-serious.jpg"],
+        images: ["images/2511-birthday-too.jpg"],
         actorReply: {
           originalText: "咋了？",
           translatedText: "뭐가?"
@@ -174,34 +174,34 @@ const posts_202511 = [
         id: 25110115,
         username: "周翊然Tz",
         originalText: "此时此刻去完成25岁的第一件事了",
-        translatedText: "실시간으로 25살의 첫 번째 일을 하러 갑니다",
+        translatedText: "지금 실시간으로 25살 된 기념 첫 번째 일을 하러 가볼거야",
         images: [],
         actorReply: null
       },
-      {
-        id: 25110119,
+       {
+        id: 25110115,
         username: "周翊然Tz",
         originalText: "",
         translatedText: "",
-        images: ["images/2511-birthday-photo.jpg"],
+        images: ["images/2511-new-look.jpg"],
         actorReply: null
       },
       {
         id: 25110116,
         username: "甜粥软乎乎Tz",
         originalText: "新做的好看吗",
-        translatedText: "새로 한 거 예쁘지 않아?",
+        translatedText: "새로 했는데 예쁘지?",
         images: ["images/2511-new-look.jpg"],
         actorReply: {
           originalText: "好看",
-          translatedText: "예쁘다"
+          translatedText: "예쁘네"
         }
       },
       {
         id: 25110118,
         username: "周翊然Tz",
         originalText: "我走啦 各位 冬天记得保暖 生日很开心 ❤️❤️❤️",
-        translatedText: "나 갈게 얘들아 겨울이니까 따뜻하게 입고 다니고 덕분에 생일이 정말 행복했어 ❤️❤️❤️",
+        translatedText: "나 갈게 얘들아 겨울이니까 따뜻하게 입고 다니고 덕분에 생일 너무 행복했어 ❤️❤️❤️",
         images: [],
         actorReply: null
       }
