044fd8525be3095dba4b57f73c6e30041475b8c7
zyr001122 <vazseyo@gmail.com>
2026-07-30 15:17:33 +0900

    Add files via upload

---
+++ data-2025-12.js
@@ -0,0 +1,257 @@
+const posts_202512 = [
+  {
+    id: 251201,
+    date: "2025-12-21",
+    weiboUrl: "https://weibo.com/5994946077/QjseswNTk",
+    authorName: "周翊然Tz",
+    originalText: "冬至快乐！！",
+    translatedText: "동지 즐겁게 보내!!",
+    images: ["images/2512-0221-02.jpg", "images/2512-0221-01.jpg"],
+    comments: [
+      {
+        id: 25120101,
+        username: "周翊然Tz",
+        originalText: "记得吃饺子 各位 我这个属于 饺子和抄手的混合体 但应该也没毛病",
+        translatedText: "다들 만두 챙겨 먹어. 나는 이거 만두랑 초서우(抄手) 중간쯤 되는 건데 그래도 괜찮을 것 같아",
+        note: "抄手(초서우)는 쓰촨식 만둣국 종류",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 25120102,
+        username: "周翊然Tz",
+        originalText: "牛肉馅的抄手饺子",
+        translatedText: "소고기 소 넣은 초서우 만두",
+        images: [],
+        actorReply: null
+      }
+    ]
+  },
+  {
+    id: 251202,
+    date: "2025-12-22",
+    weiboUrl: "https://weibo.com/5994946077/QjC73yPeF",
+    authorName: "周翊然Tz",
+    originalText: "hey 我来了",
+    translatedText: "hey 왔어",
+    images: [],
+    comments: [
+      {
+        id: 25120201,
+        username: "周翊然Tz",
+        originalText: "才配音完",
+        translatedText: "방금 더빙 끝냈어",
+        images: [],
+        actorReply: null
+      },
+      {
+        id: 25120202,
+        username: "烟竹阮桐粥",
+        originalText: "翊然。你的画技真的。。很诡异",
+        translatedText: "익연아. 네 그림 실력은 진짜.. 좀 기괴해",
+        images: ["images/2512-weird-drawing.jpg"],
+        actorReply: {
+          originalText: "你啥意思",
+          translatedText: "그게 무슨 소리야"
+        }
+      },
+      {
+        id: 25120203,
+        username: "芝士椰啵脆",
+        originalText: "哥你快看我在网上刷到的爆闪耳钉，你见过吗？",
+        translatedText: "오빠 나 인터넷에서 본 반짝이는 귀걸이 좀 봐봐, 본 적 있어?",
+        images: ["images/2512-flash-earring.jpg"],
+        actorReply: {
+          originalText: "？？？好酷",
+          translatedText: "???완전 멋있다"
+        }
+      },
+      {
+        id: 25120204,
+        username: "翊点睡_",
+        originalText: "老公 他们都说女儿随爹 你觉得呢",
+        translatedText: "자기야 다들 딸은 아빠 닮는다는데 어떻게 생각해?",
+        images: ["images/2512-daughter-like-dad.jpg"],
+        actorReply: {
+          originalText: "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈",
+          translatedText: "ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ"
+        }
+      },
+      {
+        id: 25120205,
+        username: "小狗蔓越莓",
+        originalText: "听到了吗宝宝",
+        translatedText: "들었어 자기야?",
+        images: ["images/2512-did-you-hear.jpg"],
+        actorReply: {
+          originalText: "来了",
+          translatedText: "왔어"
+        }
+      },
+      {
+        id: 25120206,
+        username: "周周想念然Tz",
+        originalText: "然然今年最后一个月你可以先实现我的愿望“翻翻我” 我再祝你实现你的心愿好嘛",
+        translatedText: "익연아 올해 마지막 달에는 내 소원 '나 좀 봐줘'부터 이뤄주면 안 될까, 그다음에 내가 네 소원 이뤄지길 빌어줄게",
+        images: ["images/2512-flip-me.jpg"],
+        actorReply: {
+          originalText: "[doge]",
+          translatedText: "[doge]"
+        }
+      },
+      {
+        id: 25120207,
+        username: "魚肉蛋黄酥o_",
+        originalText: "想采访你一下倒进这个厚雪地里的感觉如何🤔",
+        translatedText: "두꺼운 눈밭에 넘어진 느낌이 어떤지 인터뷰하고 싶어🤔",
+        note: "원문 동영상",
+        images: [],
+        actorReply: {
+          originalText: "下面是冰",
+          translatedText: "밑에는 얼음이야"
+        }
+      },
+      {
+        id: 25120208,
+        username: "祈川十里春",
+        originalText: "躺在雪地里是不是很舒服！",
+        translatedText: "눈밭에 누우니까 완전 편하지!",
+        images: ["images/2512-lying-snow.jpg"],
+        actorReply: {
+          originalText: "那是你没有看到 我起来以后 抖衣服里的雪的样子 哈哈哈哈",
+          translatedText: "그건 네가 내가 일어난 다음에 옷 속 눈 터는 모습을 못 봐서 그래 ㅋㅋㅋㅋ"
+        }
+      },
+      {
+        id: 25120209,
+        username: "轻轻茉莉不加冰-",
+        originalText: "老公好久不见茅台 它是不是圆润了",
+        translatedText: "자기야 오랜만에 마오타이 보는데 좀 통통해지지 않았어?",
+        note: "茅台는 반려동물 이름으로 추정. 밑에 '发毛了'는 '살찐 게 아니라 털이 부숭해진 거야'라는 말장난(发福 대신 发毛)",
+        images: ["images/2512-maotai-chubby.jpg"],
+        actorReply: {
+          originalText: "发毛了",
+          translatedText: "살찐 게 아니라 털이 부숭해진 거야"
+        }
+      },
+      {
+        id: 25120210,
+        username: "椰椰糕冷",
+        originalText: "哥哥快看！我生日朋友给我准备的小惊喜 谢谢“你”陪我过生日哈哈哈",
+        translatedText: "오빠 봐봐! 내 생일에 친구가 준비해준 깜짝 선물, '너'가 내 생일 함께해줘서 고마워 ㅋㅋㅋ",
+        images: ["images/2512-birthday-surprise.jpg"],
+        actorReply: {
+          originalText: "生日快乐！",
+          translatedText: "생일 축하해!"
+        }
+      },
+      {
+        id: 25120211,
+        username: "趁热把甜粥喝了",
+        originalText: "然然 星露谷怎么玩 我咋看不懂捏",
+        translatedText: "익연아 스타듀밸리 어떻게 해? 나 봐도 잘 모르겠어",
+        images: [],
+        actorReply: {
+          originalText: "可以上网搜一下攻略 哈哈哈哈",
+          translatedText: "인터넷에 공략 검색해봐 ㅋㅋㅋㅋ"
+        }
+      },
+      {
+        id: 25120212,
+        username: "川野衔雾",
+        originalText: "美甲好看吗？回答我",
+        translatedText: "네일 예뻐? 대답해줘",
+        images: ["images/2512-nail-check.jpg"],
+        actorReply: {
+          originalText: "好看的",
+          translatedText: "예뻐"
+        }
+      },
+      {
+        id: 25120213,
+        username: "粥里有什么_",
+        originalText: "这真的是一个地方吗然",
+        translatedText: "然아 이거 진짜 같은 장소 맞아?",
+        images: ["images/2512-same-place.jpg"],
+        actorReply: {
+          originalText: "难道没有百分之90的相似度吗",
+          translatedText: "90% 정도는 닮지 않았어?"
+        }
+      },
+      {
+        id: 25120214,
+        username: "芋泥没烦恼",
+        originalText: "哥哥球氛围感男头拍摄技巧",
+        translatedText: "오빠 분위기 있는 남자 프로필 사진 찍는 팁 좀",
+        images: ["images/2512-mood-selfie-tips.jpg"],
+        actorReply: {
+          originalText: "正方形 半张脸…",
+          translatedText: "정사각형, 얼굴 반만…"
+        }
+      },
+      {
+        id: 25120215,
+        username: "亿禹",
+        originalText: "宝宝教你一个低卡减脂菜谱，先把香芋剁成泥，再把可乐汉堡放入杯中，这就是香泥乐堡杯（想你了bb",
+        translatedText: "자기야 내가 저칼로리 다이어트 레시피 알려줄게, 먼저 토란을 곱게 다져서 으깨고, 콜라버거를 컵에 넣으면, 이게 바로 토란콜버거컵이야 (보고 싶어 bb",
+        images: [],
+        actorReply: {
+          originalText: "……",
+          translatedText: "……"
+        }
+      },
+      {
+        id: 25120216,
+        username: "困困小然z",
+        originalText: "你的耳洞养的怎么样了",
+        translatedText: "귀 뚫은 거 잘 관리되고 있어?",
+        images: [],
+        actorReply: {
+          originalText: "我觉得没问题 我都换了新的",
+          translatedText: "문제없는 것 같아 새걸로 다 바꿨어"
+        }
+      },
+      {
+        id: 25120217,
+        username: "JuscHe",
+        originalText: "你要不要！",
+        translatedText: "너 가질래?!",
+        images: ["images/2512-want-this.jpg"],
+        actorReply: {
+          originalText: "这是什么好东西",
+          translatedText: "이거 뭔데 이렇게 좋아 보여"
+        }
+      },
+      {
+        id: 25120218,
+        username: "翊只小咪-",
+        originalText: "然然怎么所有人都翻了除了我？",
+        translatedText: "익연아 왜 다들 다 답장받았는데 나만 빼고 없어?",
+        images: [],
+        actorReply: {
+          originalText: "我来了",
+          translatedText: "나 왔어"
+        }
+      },
+      {
+        id: 25120219,
+        username: "梨涡加一又加一",
+        originalText: "老公想不想评价一下我的自制～",
+        translatedText: "자기야 내가 직접 만든 거 평가 좀 해줄래~",
+        images: ["images/2512-my-craft.jpg"],
+        actorReply: {
+          originalText: "卖我一个！",
+          translatedText: "나한테 하나 팔아!"
+        }
+      },
+      {
+        id: 25120220,
+        username: "周翊然Tz",
+        originalText: "我走了 各位 大家真的要注意保暖 真的很冷 不要生病 要天天开心各位",
+        translatedText: "나 갈게 얘들아, 진짜 다들 따뜻하게 챙겨 입어 진짜 추워, 아프지 말고 매일매일 행복해 얘들아",
+        images: [],
+        actorReply: null
+      }
+    ]
+  }
+];
