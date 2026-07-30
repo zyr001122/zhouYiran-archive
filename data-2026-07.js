05eaf44f6a0c65c08ee59d24b2571e703681d9f3
zyr001122 <vazseyo@gmail.com>
2026-07-31 00:13:09 +0900

    Update data-2026-07.js

--- data-2026-07.js
+++ data-2026-07.js
@@ -11,30 +11,30 @@ const posts_202607 = [
       {
         id: 26070101,
         username: "翊只许刈",
-        originalText: "今天登了女儿wb[鲜花][鲜花]看看女儿平时刷些什么[good][good]还怕女儿看别的小男生[haha]是你我就放心了[手指比心][手指比心]小女孩儿就该像我女儿这样天天看周翊然[赞]真不错[good][good]",
-        translatedText: "오늘 딸 웨이보에 로그인해서[鲜花][鲜花] 딸이 평소에 뭘 보는지 봤는데[good][good] 딸이 다른 남자애 보는 거 걱정했었는데[haha] 너인 걸 보니까 마음이 놓이네[手指比心][手指比心] 어린 여자애는 내 딸처럼 매일 저우이란 보는 게 딱이지[赞] 진짜 좋다[good][good]",
+        originalText: "今天登了女儿wb🌹🌹看看女儿平时刷些什么[good][good]还怕女儿看别的小男生😄是你我就放心了[手指比心][手指比心]小女孩儿就该像我女儿这样天天看周翊然[赞]真不错[good][good]",
+        translatedText: "오늘 딸 웨이보에 로그인해서[鲜花][鲜花] 딸이 평소에 뭘 보는지 봤는데[good][good] 딸이 다른 남자애 보는 거 걱정했었거든[haha] 근데 너인 걸 보니까 마음이 놓이네[手指比心][手指比心] 어린 여자애는 내 딸처럼 매일 주익연 보는 게 맞지👍 진짜 최고[good][good]",
         images: [],
         actorReply: {
           originalText: "阿姨你好",
-          translatedText: "이모 안녕하세요"
+          translatedText: "어머님 안녕하세요"
         }
       },
       {
         id: 26070102,
         username: "翊分钟恋人z",
         originalText: "我在沙滩上看到你了🐷",
-        translatedText: "해변에서 너 봤어🐷",
+        translatedText: "나 해변에서 너 봤다🐷",
         images: ["images/2607-c02.jpg"],
         actorReply: {
           originalText: "画的不错啊",
-          translatedText: "그림 잘 그렸네"
+          translatedText: "그림 잘 그린다"
         }
       },
       {
         id: 26070103,
         username: "忙内专家",
         originalText: "你喜欢哪个",
-        translatedText: "너 어떤 게 좋아",
+        translatedText: "넌 어떤 게 좋아",
         images: ["images/2607-c03.jpg"],
         actorReply: {
           originalText: "喜欢的没在里面啊",
@@ -49,14 +49,14 @@ const posts_202607 = [
         images: ["images/2607-c04.jpg"],
         actorReply: {
           originalText: "我能只负责吃吗",
-          translatedText: "나는 그냥 먹기만 하면 안 될까"
+          translatedText: "나는 그냥 먹기만 하면 안 돼?"
         }
       },
       {
         id: 26070105,
         username: "翊语纯态",
         originalText: "你说啥？不到啊！我哥哥很萌的～[doge]",
-        translatedText: "뭐라고? 아니거든! 우리 오빠 완전 귀여운데~[doge]",
+        translatedText: "뭐라구? 아니거든! 우리 오빠 완전 귀엽거든~[doge]",
         images: ["images/2607-c05.jpg"],
         actorReply: {
           originalText: "？？？",
@@ -66,13 +66,13 @@ const posts_202607 = [
       {
         id: 26070106,
         username: "kiku_翊序雨夜书i",
-        originalText: "哥哥我和茅台来接你回家吃饭啦[yeah]",
-        translatedText: "오빠 나랑 마오타이가 밥 먹으러 집에 데리러 왔어[yeah]",
+        originalText: "哥哥我和茅台来接你回家吃饭啦[耶耶]",
+        translatedText: "오빠 나랑 마오타이가 오빠 밥 먹을 거 가져왔어[耶耶]",
         note: "茅台는 반려동물 이름으로 추정",
         images: ["images/2607-c06.jpg"],
         actorReply: {
           originalText: "有驾照吗 就骑车",
-          translatedText: "면허는 있어? 그럼 자전거 타고 가야지"
+          translatedText: "면허는 있는 거야? 자전거부터 타야지"
         }
       },
       {
@@ -83,18 +83,18 @@ const posts_202607 = [
         images: ["images/2607-c07.jpg"],
         actorReply: {
           originalText: "酒店的工作人员",
-          translatedText: "호텔 직원이요"
+          translatedText: "호텔 직원분이 그려주셨어"
         }
       },
       {
         id: 26070108,
         username: "大圣Marbury杰锅是nightstar",
-        originalText: "哈喽呀[舔屏]在干嘛",
-        translatedText: "안녕[舔屏] 뭐해",
+        originalText: "哈喽呀[甜]在干嘛",
+        translatedText: "하이잉[甜] 뭐 해?",
         images: [],
         actorReply: {
           originalText: "在等飞机",
-          translatedText: "비행기 기다리는 중"
+          translatedText: "비행기 기다리고 있어"
         }
       },
       {
@@ -105,18 +105,18 @@ const posts_202607 = [
         images: [],
         actorReply: {
           originalText: "必须德州搭配vib",
-          translatedText: "무조건 텍사스 리그에 바이브레이션이지"
+          translatedText: "무조건 텍사스 리그 바이브로"
         }
       },
       {
         id: 26070110,
         username: "喝口QQ粥-",
         originalText: "说吧这拍了几遍",
-        translatedText: "말해봐 이거 몇 번 찍은 거야",
+        translatedText: "대답해 이거 몇 번 찍은 거야",
         images: ["images/2607-c10.jpg"],
         actorReply: {
           originalText: "一两遍吧[doge]",
-          translatedText: "한두 번 정도[doge]"
+          translatedText: "한두 번 정도?[doge]"
         }
       },
       {
@@ -127,18 +127,18 @@ const posts_202607 = [
         images: ["images/2607-c11.jpg"],
         actorReply: {
           originalText: "让我想到了那个🐷",
-          translatedText: "그거 생각나게 하네🐷"
+          translatedText: "보니까 생각나네🐷"
         }
       },
       {
         id: 26070112,
         username: "柚子闭了o-o",
         originalText: "哥哥你这样戴帽子是什么新的叠戴时尚吗哈哈哈哈",
-        translatedText: "오빠 이렇게 모자 쓰는 거 새로운 레이어드 패션이야? ㅋㅋㅋㅋ",
+        translatedText: "오빠 모자 이렇게 쓰는 거 새로 시도하는 레이어드 패션이야? ㅋㅋㅋㅋ",
         images: ["images/2607-c12.jpg"],
         actorReply: {
           originalText: "马里奥里的蘑菇啊",
-          translatedText: "마리오에 나오는 버섯이잖아"
+          translatedText: "마리오에 나오는 버섯이야"
         }
       },
       {
@@ -149,69 +149,69 @@ const posts_202607 = [
         images: ["images/2607-c13.jpg"],
         actorReply: {
           originalText: "恭喜你啊",
-          translatedText: "축하해"
+          translatedText: "진짜 축하해"
         }
       },
       {
         id: 26070114,
         username: "薯条大王Tz",
         originalText: "老公，成都有什么好吃的推荐吗？[航天员]",
-        translatedText: "자기야, 청두에 맛있는 거 추천해줄 거 있어?[航天员]",
+        translatedText: "자기야, 청두 맛집 추천할 만한 데 있어?[航天员]",
         images: ["images/2607-c14.jpg"],
         actorReply: {
           originalText: "必须吃抄手",
-          translatedText: "무조건 차오서우 먹어야지"
+          translatedText: "무조건 차오서우(비빔만두) 먹어"
         }
       },
       {
         id: 26070115,
         username: "翊点睡_",
         originalText: "然然 这是你的衣柜嘛",
-        translatedText: "익연아 이거 네 옷장이야?",
+        translatedText: "익연아 이거 너 옷장 아냐?",
         images: ["images/2607-c15.jpg"],
         actorReply: {
-          originalText: "[笑cry][笑cry][笑cry]",
-          translatedText: "[笑cry][笑cry][笑cry]"
+          originalText: "😂😂😂",
+          translatedText: "😂😂😂"
         }
       },
       {
         id: 26070116,
         username: "是我的小土豆-Tz",
         originalText: "223号很酷哦 比赛要注意安全呀老公 知道不！[开学季]",
-        translatedText: "223번 완전 멋있다 경기할 때 안전 조심해 자기야 알겠지![开学季]",
+        translatedText: "223번 짱 멋있어 경기할 때 안전 조심해 자기야 알겠지![开学季]",
         images: ["images/2607-c16.jpg"],
         actorReply: {
           originalText: "放心 我平均20km这样的开 很安全",
-          translatedText: "걱정 마 나 평균 20km 정도로 몰아서 안전해"
+          translatedText: "걱정 마 나 평균 20km 정도로 달려서 되게 안전해"
         }
       },
       {
         id: 26070117,
         username: "甜丸泡奶芙Zz_",
         originalText: "科学怪人[航天员][航天员]除了贴纸和衣服还有啥呀～能不能上个链接 我也想要！🥹",
-        translatedText: "프랑켄슈타인[航天员][航天员] 스티커랑 옷 말고 또 뭐 있어~ 링크 좀 올려줄 수 있어? 나도 갖고 싶어!🥹",
+        translatedText: "프랑켄슈타인[航天员][航天员] 스티커랑 옷 말고 또 뭐 있어~? 링크 올려주면 안 돼? 나도 갖고 싶어!🥹",
         images: ["images/2607-c17.jpg"],
         actorReply: {
           originalText: "之后做一些 送给大家",
-          translatedText: "나중에 좀 만들어서 다들한테 나눠줄게"
+          translatedText: "나중에 몇 개 만들어서 나눠 줄게"
         }
       },
       {
         id: 26070118,
         username: "kiku_翊序雨夜书i",
         originalText: "哥哥给你看茅台法拉利上海一日游vlog[打call]",
-        translatedText: "오빠 마오타이랑 페라리 상하이 하루 여행 브이로그 보여줄게[打call]",
+        translatedText: "오빠 마오타이랑 페라리의 상하이 당일치기 여행 브이로그 보여줄게[打call]",
         images: ["images/2607-c18.jpg"],
         actorReply: {
           originalText: "记得回家[doge]",
-          translatedText: "집에 오는 거 잊지 마[doge]"
+          translatedText: "집에 돌아오는 거 잊지 마[doge]"
         }
       },
       {
         id: 26070119,
         username: "啵唧翊口耶啵哇",
         originalText: "赛车手，那准备好了吗[doge][doge]@周翊然Tz",
-        translatedText: "레이서, 준비 다 됐어?[doge][doge]@周翊然Tz",
+        translatedText: "레이서, 준비 완료?[doge][doge]@周翊然Tz",
         images: ["images/2607-c19.jpg"],
         actorReply: {
           originalText: "头盔帮我换一下",
@@ -223,11 +223,11 @@ const posts_202607 = [
         id: 26070120,
         username: "淇淇爱喝水qiiii",
         originalText: "欧巴今天戴的什么耳钉！！",
-        translatedText: "오빠 오늘 무슨 귀걸이 한 거야!!",
+        translatedText: "오빠 오늘 무슨 귀걸이 했어!!",
         images: [],
         actorReply: {
           originalText: "",
-          translatedText: "(사진 답글)",
+          translatedText: "",
           images: ["images/2607-c20-reply.jpg"]
         }
       },
@@ -235,18 +235,18 @@ const posts_202607 = [
         id: 26070121,
         username: "翊粒紫然Tz",
         originalText: "找不同[doge]",
-        translatedText: "다른 그림 찾기[doge]",
+        translatedText: "틀린 그림 찾기[doge]",
         images: ["images/2607-c21.jpg"],
         actorReply: {
           originalText: "图一的眼睛比图二大不少",
-          translatedText: "1번 그림 눈이 2번보다 훨씬 크네"
+          translatedText: "1번 사진 눈이 2번보다 훨씬 크네"
         }
       },
       {
         id: 26070122,
         username: "淇淇爱喝水qiiii",
         originalText: "来吃烤肉吗哥哥",
-        translatedText: "고기 구워 먹으러 올래 오빠",
+        translatedText: "고기 구워 먹으러 올래? 오빠",
         images: ["images/2607-c22.jpg"],
         actorReply: {
           originalText: "不吃 你吃吧",
@@ -257,18 +257,18 @@ const posts_202607 = [
         id: 26070123,
         username: "-七颗橙子",
         originalText: "潮汕好吃吗[干饭人]（你安利的这盘卤鹅是澄海狮头鹅，最灵魂的就是鹅头部分了，会吃！",
-        translatedText: "차오산 맛있었어?[干饭人] (오빠가 추천한 이 요리는 청하이 사자머리거위인데, 제일 핵심은 거위 머리 부분이야, 잘 먹네!",
+        translatedText: "차오산 맛있었어?[干饭人] (너한테 영업당한 거위 조림 요리는 청하이 사자머리 거위인데, 거위 머리 부분이 완전 킥이야, 먹을 줄 아네!",
         images: ["images/2607-c23.jpg"],
         actorReply: {
           originalText: "超级无敌好吃",
-          translatedText: "완전 엄청 맛있어"
+          translatedText: "진짜 킥이야 진짜 맛있더라"
         }
       },
       {
         id: 26070124,
         username: "咬一口脆皮年糕_",
         originalText: "这是在？",
-        translatedText: "이거 어디야?",
+        translatedText: "여기 어디야?",
         images: ["images/2607-c24.jpg"],
         actorReply: {
           originalText: "？",
@@ -278,34 +278,34 @@ const posts_202607 = [
       {
         id: 26070125,
         username: "Ran-瑶瑶瑶",
-        originalText: "@周翊然Tz 好看吗，然然这是我最喜欢的一张",
-        translatedText: "@周翊然Tz 예뻐? 익연아 이게 내가 제일 좋아하는 사진이야",
+        originalText: "好看吗，然然这是我最喜欢的一张",
+        translatedText: "예뻐? 익연아 이게 내가 제일 좋아하는 사진이야",
         images: ["images/2607-c25.jpg"],
         actorReply: {
           originalText: "好看",
-          translatedText: "예쁘네"
+          translatedText: "예쁘다"
         }
       },
       {
         id: 26070126,
         username: "甜栗猫耳",
-        originalText: "想不想跟我比一把拍照技术[不愧是你]顺便猜猜这是哪里！",
-        translatedText: "나랑 사진 실력 겨뤄볼래[不愧是你] 겸사겸사 여기 어딘지 맞혀봐!",
+        originalText: "想不想跟我比一把拍照技术 顺便猜猜这是哪里！",
+        translatedText: "나랑 사진 실력 겨루자 겸사겸사 여기 어딘지도 맞혀봐!",
         images: ["images/2607-c26.jpg"],
         actorReply: {
           originalText: "感觉有很多鱼",
-          translatedText: "물고기 엄청 많을 것 같은데"
+          translatedText: "뭔가 물고기 엄청 많을 것 같은데"
         }
       },
       {
         id: 26070127,
         username: "不高兴的小鹿啊",
         originalText: "哥哥，快看彩虹🌈，赛车🏎️一定要顺利呀！",
-        translatedText: "오빠, 무지개 빨리 봐🌈, 레이싱🏎️ 꼭 잘 되길!",
+        translatedText: "오빠, 무지개야🌈, 레이싱🏎️ 재밌게 하고 왕!",
         images: ["images/2607-c27.jpg"],
         actorReply: {
           originalText: "你的这个彩虹好完整诶 漂亮",
-          translatedText: "이 무지개 완전 완벽하네 예쁘다"
+          translatedText: "너가 찍은 무지개 완전 완벽한 무지개다 예뻐"
         }
       },
       {
@@ -316,15 +316,15 @@ const posts_202607 = [
         images: ["images/2607-c28.jpg"],
         actorReply: {
           originalText: "和我坐一桌",
-          translatedText: "나랑 같은 테이블에 앉자"
+          translatedText: "나랑 수준이 똑같은 것 같은데"
         }
       },
       {
         id: 26070129,
         username: "周翊然Tz",
         originalText: "",
-        translatedText: "",
-        note: "원문 음성 메시지 (텍스트 없음)",
+        translatedText: "几分之几",
+        note: "원문 노래 음성 메시지",
         images: [],
         actorReply: null
       },
@@ -332,7 +332,7 @@ const posts_202607 = [
         id: 26070130,
         username: "周翊然Tz",
         originalText: "最近发现 鹦鹉也很好玩 小太阳鹦鹉",
-        translatedText: "요즘 보니까 앵무새도 되게 재밌더라, 태양앵무",
+        translatedText: "요즘 보니까 앵무새도 되게 재밌더라, 썬코뉴어였나",
         images: [],
         actorReply: null
       },
@@ -340,41 +340,40 @@ const posts_202607 = [
         id: 26070131,
         username: "存放辣条",
         originalText: "能不能倒翻一下？错过了[裂开][裂开][裂开][裂开][裂开][裂开][裂开][裂开][裂开][裂开]",
-        translatedText: "다시 넘겨줄 수 있어? 놓쳤어[裂开]×10",
+        translatedText: "다시 위에서 부터 보면 안 돼? 놓쳤어[裂开][裂开][裂开][裂开][裂开][裂开][裂开][裂开][裂开][裂开]",
         images: ["images/2607-c31.jpg"],
         actorReply: {
           originalText: "我在看呢",
-          translatedText: "나 보고 있는데"
+          translatedText: "나 아직 보고 있엉"
         }
       },
       {
         id: 26070132,
         username: "-淞鸢书逢然",
-        originalText: "然然你在长大的过程中会很明显感觉自己的变化吗，这种感觉让我无所适从怎么办……以前总是不需要考虑很多。可现在总会为未来而担心，忧虑。这让我彻夜无眠，又带来的是记忆力下降，我陷入了一个恶性循环，可我不知该如何脱身。（困扰了我很久的问题或许今天能得到你的看法吗）[捂嘴哭]",
-        translatedText: "익연아 너는 커가면서 스스로의 변화를 확실히 느끼는 편이야? 이런 느낌 때문에 어떻게 해야 할지 모르겠어…… 예전엔 별로 많이 생각할 필요가 없었는데, 지금은 항상 미래에 대해 걱정하고 불안해해. 그것 때문에 밤새 잠도 못 자고, 거기다 기억력까지 떨어져서 악순환에 빠졌는데 어떻게 벗어나야 할지 모르겠어. (오래 고민했던 문제인데 오늘 네 생각을 들을 수 있을까)[捂嘴哭]",
+        originalText: "然然你在长大的过程中会很明显感觉自己的变化吗，这种感觉让我无所适从怎么办……以前总是不需要考虑很多。可现在总会为未来而担心，忧虑。这让我彻夜无眠，又带来的是记忆力下降，我陷入了一个恶性循环，可我不知该如何脱身。（困扰了我很久的问题或许今天能得到你的看法吗😢",
+        translatedText: "익연아 너는 네가 성장하는 과정에서 스스로의 변화를 분명하게 느끼는 편이야? 이런 기분 때문에 어떻게 해야 할지를 모르겠는데 어떻게 하면 좋을까......예전에는 이것저것 많이 생각할 필요가 없었는데 지금은 늘 미래 때문에 걱정되고 불안해이것 때문에 밤새 잠도 못 자고 그러다 보니 기억력까지 떨어진 것 같고 나는 계속 악순환에 빠져 있는데 어떻게 벗어나야 할지 모르겠어 (오랫동안 나를 괴롭혀온 문제인데 혹시 오늘 너의 생각을 들을 수 있을까?)😢",
         images: ["images/2607-c32.jpg"],
         actorReply: {
           originalText: "肯定会有很大的变化啊 这个世界一直都在变 你会遇到各种各样的人 对你好的人 伤害你的人 你也会经历很多事 但无论怎么样 你都是你自己 也不需要为了未来担心和焦虑 焦虑也不会改变事实 只需要活在当下 过好每一天就好 希望你的每一天都是充实而有意义的",
-          translatedText: "당연히 큰 변화가 있을 거야. 이 세상은 계속 변하니까. 너는 여러 종류의 사람을 만나게 될 거야, 너한테 잘해주는 사람도 있고 상처 주는 사람도 있고. 여러 일도 겪게 될 거고. 근데 어쨌든 너는 너 자신이야. 미래 때문에 걱정하고 불안해할 필요 없어. 불안해한다고 현실이 바뀌는 것도 아니고, 그냥 지금 이 순간에 충실하게 하루하루 잘 보내면 돼. 매일매일이 알차고 의미 있길 바라"
+          translatedText: "당연히 엄청 큰 변화가 있지 이 세상은 늘 변하고 있잖아. 너는 앞으로 정말 다양한 사람들을 만나게 될 거야 너에게 잘해주는 사람도 있고 상처를 주는 사람도 있을 거야. 그리고 수많은 일들도 겪게 될 거고 하지만 어떤 상황에서도 너는 그냥 너 자신이야. 미래 때문에 걱정하고 불안해할 필요도 없어. 불안해한다고 해서 사실이 바뀌지는 않거든. 그저 현재를 살아가며 하루하루를 잘 보내면 돼. 너의 매일매일이 보람차고 의미 있길 바랄게"
         }
       },
       {
         id: 26070133,
         username: "祈祈大王_",
         originalText: "你怎么知道我前两天见到余佳运老师啦",
-        translatedText: "너 내가 며칠 전에 위자윈 선생님 만난 거 어떻게 알았어",
-        note: "余佳运은 익연이의 동료 배우로 추정",
+        translatedText: "너 내가 며칠 전에 여가운 만난 거 어떻게 알았어",
         images: ["images/2607-c33.jpg"],
         actorReply: {
           originalText: "是不是瘦了很多 hhh",
-          translatedText: "많이 마른 거 아니야? ㅎㅎ"
+          translatedText: "살 엄청 빠지지 않았어? ㅋㅋㅋㅋ"
         }
       },
       {
         id: 26070134,
         username: "周翊然Tz",
         originalText: "我走了 马上要上飞机了 明天见[收到]",
-        translatedText: "나 갈게 곧 비행기 타야 해서 내일 봐[收到]",
+        translatedText: "나 갈게 곧 비행기 타야 해 내일 보자[收到]",
         images: [],
         actorReply: null
       }
