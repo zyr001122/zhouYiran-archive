7a88871589c2aea400995d08f8c11951f212817b
zyr001122 <vazseyo@gmail.com>
2026-07-30 21:39:03 +0900

    Update data-2026-06.js

--- data-2026-06.js
+++ data-2026-06.js
@@ -4,19 +4,19 @@ const posts_202606 = [
     date: "2026-06-22",
     weiboUrl: "https://weibo.com/5994946077/5312651345527123",
     authorName: "周翊然Tz",
-    originalText: "hello 🙋",
-    translatedText: "hello 🙋",
+    originalText: "hello[航天员]",
+    translatedText: "hello[航天员]",
     images: [],
     comments: [
       {
         id: 26060101,
         username: "祈川十里春",
         originalText: "哥哥 这是你的新宠吗！什么时候给我们介绍介绍哈哈哈",
-        translatedText: "오빠 이거 새로 키우는 애야? 언제 우리한테 소개해줄 거야 하하하",
+        translatedText: "오빠 이거 새로 키우는 애야?! 우리한테 언제 소개해 줄 거야ㅎㅎㅎ",
         images: ["images/2606-new-pet.jpg"],
         actorReply: {
           originalText: "不是我的 hhh",
-          translatedText: "내 거 아니야 ㅋㅋ"
+          translatedText: "내가 키우는 애 아니야ㅋㅋㅋ"
         }
       },
       {
@@ -27,19 +27,19 @@ const posts_202606 = [
         images: ["images/2606-gift-for-you.jpg"],
         actorReply: {
           originalText: "一直开心",
-          translatedText: "항상 행복해"
+          translatedText: "항상 행복하자"
         }
       },
       {
         id: 26060103,
         username: "只吃两碗饭-",
-        originalText: "hey 老公 看我钓的鱼！但是上钩的都太小了最后都放生了你最近钓鱼有啥收获不😢",
-        translatedText: "hey 자기야 내가 낚은 물고기 좀 봐! 근데 걸린 게 다 너무 작아서 결국 다 놓아줬어, 너 요즘 낚시해서 뭐 좀 건진 거 있어?😢",
-        note: "익연이 답글은 4초 음성 메시지 + 텍스트",
+        originalText: "hey 老公 看我钓的鱼！但是上钩的都太小了最后都放生了你最近钓鱼有啥收获不",
+        translatedText: "hey 자기야 내가 낚은 물고기 봐봐! 근데 걸린 게 다 너무 작아서 그냥 다 놓아줬어 넌 요즘 낚시해서 뭐 좀 건진 거 있어?",
+        note: "원문 음성 메시지",
         images: ["images/2606-fishing.jpg"],
         actorReply: {
           originalText: "你这个是抬调还是是露亚？看着像抬掉?",
-          translatedText: "이거 트롤링이야 아니면 루어야? 트롤링처럼 보이는데? (음성 4초)"
+          translatedText: "이거 트롤링이야 아니면 루어낚시야? 트롤링처럼 보이는데?"
         }
       },
       {
@@ -56,8 +56,8 @@ const posts_202606 = [
       {
         id: 26060105,
         username: "扁桃体发炎不发言-",
-        originalText: "哥哥请选择你的心动车嘉宾 小紫 小红 小黑 再乱入一个小粉🤔",
-        translatedText: "오빠 마음에 드는 차 골라봐 보라, 빨강, 검정 그리고 깜짝 분홍도 하나🤔",
+        originalText: "哥哥请选择你的心动车嘉宾 小紫 小红 小黑 再乱入一个小粉😆",
+        translatedText: "오빠 마음을 흔든 게스트 골라봐, 빨강, 검정 그리고 메기로 난입한 핑크도😆",
         images: ["images/2606-cars.jpg"],
         actorReply: {
           originalText: "？？厉害了",
@@ -68,19 +68,19 @@ const posts_202606 = [
         id: 26060106,
         username: "黄忆一个秋hyq",
         originalText: "快看我的猫",
-        translatedText: "내 고양이 좀 봐봐",
+        translatedText: "울 고양이 좀 봐봐",
         images: ["images/2606-cat-1.jpg"],
         actorReply: {
           originalText: "想rua",
-          translatedText: "만지고 싶다"
+          translatedText: "쓰담쓰담 하고 싶다"
         }
       },
       {
         id: 26060107,
         username: "周翊然Tz",
         originalText: "试一下这个新的功能，听说现在可以发语音",
-        translatedText: "이 새 기능 한번 써볼게, 이제 음성도 보낼 수 있대",
-        note: "5초 음성 메시지 첨부",
+        translatedText: "새 기능이 있다 그래서 한번 써보려고, 이제 음성도 보낼 수 있다며",
+        note: "원문 음성 메시지",
         images: [],
         actorReply: null
       },
@@ -89,53 +89,53 @@ const posts_202606 = [
         username: "是我的小土豆-Tz",
         originalText: "老公 我拍的日落怎么样",
         translatedText: "자기야 내가 찍은 노을 어때",
-        note: "익연이 답글은 2초 음성 메시지 + 텍스트",
+        note: "원문 음성 메시지",
         images: ["images/2606-sunset-photo-2.jpg"],
         actorReply: {
           originalText: "不错不错",
-          translatedText: "괜찮네 괜찮아 (음성 2초)"
+          translatedText: "괜찮다 좋은데"
         }
       },
       {
         id: 26060109,
         username: "只吃两碗饭-",
-        originalText: "跟你好朋友昨天这么同步 啥时候再还原一个同款表情🍅",
-        translatedText: "네 친한 친구랑 어제 이렇게 싱크로 됐던데, 언제 또 같은 표정 재현해줄 거야🍅",
-        note: "익연이 답글은 4초 음성 메시지",
+        originalText: "跟你好朋友昨天这么同步 啥时候再还原一个同款表情[钢铁侠]",
+        translatedText: "어제 절친 아니랄까봐 표정도 완전 똑같던데 언제 또 같은 표정 재현해줄 거야[钢铁侠]",
+        note: "원문 음성 메시지",
         images: ["images/2606-same-expression.jpg"],
         actorReply: {
-          originalText: "嗯哦哦哦哦哦哦哦",
-          translatedText: "음 오오오오오오오 (음성 4초)"
+          originalText: "",
+          translatedText: "음하하하하하하하하"
         }
       },
       {
         id: 26060110,
         username: "别烦针管",
         originalText: "哥哥说我回国第一顿吃什么！",
-        translatedText: "오빠 나 귀국하면 첫 끼로 뭐 먹을지 말해줘!",
+        translatedText: "오빠 나 귀국하면 첫 끼로 뭐 먹을지 정해줘!",
         images: [],
         actorReply: {
           originalText: "火锅啊",
-          translatedText: "훠궈지"
+          translatedText: "당연 훠궈지"
         }
       },
       {
         id: 26060111,
         username: "然应韬刊Angel",
         originalText: "为什么！为什么！为什么我发了一堆话换来的还是你的只言片语，你真的好冷漠 你的冷漠就像北极的冰川你的冷漠好像从喜马拉雅山上倒下来的一瓶水把山脚下热情的我浇的浑身湿透手脚发抖告诉我究竟怎么样才能让你对我重拾往日的热情我们难道真的已经回不去了吗！我的心就像浸泡在负一百度的冰水里一样麻木",
-        translatedText: "왜! 왜! 왜 나는 한참 이야기했는데 너는 딱 몇 마디뿐이야, 너 진짜 차갑다. 네 차가움은 마치 북극의 얼음 같아. 네 차가움은 히말라야산에서 쏟아지는 얼음물이 뜨거운 내 마음속 정열에 부어지는 것 같아, 온몸이 다 젖어서 손발까지 떨려. 대체 어떻게 해야 네가 예전처럼 다시 나한테 열정을 보여줄 수 있을지, 우리 진짜 이렇게 다 식어버린 거 아니지! 내 마음은 마치 영하 백도 물에 잠긴 것처럼 마비된 것 같아",
-        note: "익연이 답글은 2초 음성 메시지만",
+        translatedText: "왜! 왜! 왜 나는 한참 얘기해도 넌 왜 몇 마디만 보내는건데? 너 진짜 차갑다 너의 차가움은 마치 북극의 얼음 같아. 너의 차가움은 히말라야산에서 쏟아지는 얼음물이 뜨거운 내 마음속 열정에 부어지는 것 같아, 온몸이 다 젖어서 손발까지 떨려. 대체 어떻게 해야 네가 예전처럼 다시 나한테 열정을 보여줄 수 있는지 말해줘 우리 진짜 이대로 돌아갈 수 없는 거 아니지?! 내 마음은 마치 영하 100도 물에 잠긴 것처럼 마비된 것 같다고",
+        note: "원문 음성 메시지",
         images: ["images/2606-cold-drawing.jpg"],
         actorReply: {
           originalText: "",
-          translatedText: "(음성 2초만)"
+          translatedText: "허허허허"
         }
       },
       {
         id: 26060112,
         username: "悲伤甜甜圈圈",
         originalText: "喜欢我的人太多了你要去巴黎排队了",
-        translatedText: "나 좋아하는 사람이 너무 많아서 파리 갈 때 줄 서야겠어",
+        translatedText: "날 좋아하는 사람이 너무 많아서 너도 파리 와서 줄 서야해",
         images: [],
         actorReply: {
           originalText: "马上就去",
@@ -146,7 +146,7 @@ const posts_202606 = [
         id: 26060113,
         username: "薄荷_TvT",
         originalText: "hi 小然~首先谢谢你为大家呈现出这么优秀谢燕来呀 你成为谢燕来所做的努力我们也都有看到哦 你真的特别棒呀！不知道你看见现在越来越多人来爱你了 你的每个角色和进步我们都有看到 也谢谢你带给我的力量让我能够一直前行",
-        translatedText: "hi 소연아~ 먼저 네가 팬들한테 이렇게 멋진 셰옌라이를 보여줘서 고마워. 네가 셰옌라이가 되려고 노력한 거 우리도 다 지켜봤어. 너 진짜 특별히 대단해! 지금 얼마나 많은 사람들이 너 좋아하는지 몰라. 네가 맡은 모든 역할과 성장 우리 다 지켜봤어. 네가 나한테 준 힘 덕분에 계속 앞으로 나아갈 수 있었어, 고마워",
+        translatedText: "hi 익연아~ 먼저 우리한테 이렇게 멋진 사연래를 선물해줘서 고마워 네가 사연래가 되기 위해 노력한 거 우리도 다 지켜봤어 너 진짜 최고야! 지금 얼마나 많은 사람들이 널 좋아하는지 모르겠지 네가 연기한 캐릭터 하나하나와 멋진 성장을 다 지켜보고 있어 네가 나한테 준 힘 덕분에 계속 앞으로 나아갈 수 있었어 고마워",
         images: [],
         actorReply: {
           originalText: "❤️",
@@ -156,65 +156,65 @@ const posts_202606 = [
       {
         id: 26060114,
         username: "啵唧翊口耶啵哇",
-        originalText: "阿九不怕 左膀右臂来了🐶🐶🐶@周翊然Tz",
-        translatedText: "아지우 걱정 마 든든한 지원군 왔어🐶🐶🐶@周翊然Tz",
+        originalText: "阿九不怕 左膀右臂来了[送花花][送花花][送花花]",
+        translatedText: "아구 걱정 마 든든한 지원군 등장[送花花][送花花][送花花]",
         images: ["images/2606-support-photo.jpg"],
         actorReply: {
-          originalText: "礼拿图🙏",
-          translatedText: "선물 사진 잘 받았어🙏"
+          originalText: "礼拿图[航天员]",
+          translatedText: "사진 고마워[航天员]"
         }
       },
       {
         id: 26060115,
         username: "椰椰糕冷",
         originalText: "哥哥你现在知豆这个手势舞其实是捂嘴的不是捂鼻子的了吗 你跳错啦哈哈哈",
-        translatedText: "오빠 지금 지또우 이 손동작 춤 사실 입 가리는 거지 코 가리는 거 아닌 거 알아? 너 틀리게 췄어 ㅋㅋㅋ",
+        translatedText: "오빠 이거 챌린지 사실 코가 아니라 입을 가리는 거리는 건데 알아? 오빠 틀렸어ㅋㅋㅋ",
         images: ["images/2606-dance-move.jpg"],
         actorReply: {
-          originalText: "😌",
-          translatedText: "😌"
+          originalText: "[无聊]",
+          translatedText: "[无聊]"
         }
       },
       {
         id: 26060116,
         username: "粥牛牛不会流鼻涕",
         originalText: "周翊然你这个是咋做到的？有教程可学吗哈哈哈",
-        translatedText: "저우이란 이거 어떻게 한 거야? 배울 수 있는 강좌 있어 ㅋㅋㅋ",
-        note: "익연이 답글은 5초 음성 메시지 + 텍스트",
+        translatedText: "주익연 이거 어떻게 한 거야? 따라하게 강의 좀ㅋㅋㅋ",
+        note: "원문 음성 메세지",
         images: ["images/2606-how-to.jpg"],
         actorReply: {
           originalText: "这个就是这样做，然后那样做，然后再这样做就可以了",
-          translatedText: "이건 이렇게 하고, 그다음 저렇게 하고, 그다음 또 이렇게 하면 돼 (음성 5초)"
+          translatedText: "이건 그냥 이렇게 하고 저렇게 하고 그다음에 또 이렇게 하면 돼"
         }
       },
       {
         id: 26060117,
         username: "翊口韩式拌饭-Tz",
-        originalText: "哥哥快看 小狗对烧卖👅",
-        translatedText: "오빠 빨리 봐 강아지가 사오마이 보고👅",
-        note: "익연이 답글은 2초 음성 메시지만",
+        originalText: "哥哥快看 小狗对烧卖🐶👅",
+        translatedText: "오빠 봐봐 울 강아지가🐶👅",
+        note: "원문 음성 메세지",
         images: ["images/2606-dog-siomai.jpg"],
         actorReply: {
           originalText: "",
-          translatedText: "(음성 2초만)"
+          translatedText: "헤헤"
         }
       },
       {
         id: 26060118,
         username: "11_yyy-",
-        originalText: "耳洞最近养的如何了@周翊然Tz",
-        translatedText: "귀 뚫은 거 요즘 잘 관리되고 있어?@周翊然Tz",
+        originalText: "耳洞最近养的如何了",
+        translatedText: "귀 뚫은 곳은 어때 잘 관리하고 있어?",
         images: [],
         actorReply: {
           originalText: "我觉得已经完全好了",
-          translatedText: "이제 완전히 다 나은 것 같아"
+          translatedText: "내 생각엔 이제 완전히 다 아문 것 같아"
         }
       },
       {
         id: 26060119,
         username: "圣煮妻",
         originalText: "哥哥巴黎有没有手搓五老哥😭🤔",
-        translatedText: "오빠 파리에서 다섯 형님들 직접 만든 거 있어?😭🤔",
+        translatedText: "오빠 파리에서 파이브 가이즈 같이 수제버거라도 직접 만든 거 있어?😭🤔",
         note: "익연이 답글은 4초 음성 메시지 + 텍스트",
         images: [],
         actorReply: {
@@ -225,57 +225,57 @@ const posts_202606 = [
       {
         id: 26060120,
         username: "MangoBasque",
-        originalText: "评价一下我的画功🙏",
-        translatedText: "내 그림 실력 평가 좀 해줘🙏",
+        originalText: "评价一下我的画功[航天员]",
+        translatedText: "내 그림 실력 평가 좀 해줘[航天员]",
         images: ["images/2606-my-drawing.jpg"],
         actorReply: {
           originalText: "和我坐一桌",
-          translatedText: "나랑 같은 테이블에 앉자"
+          translatedText: "나랑 같은 반에 앉자"
         }
       },
       {
         id: 26060121,
         username: "小绘景Tz",
-        originalText: "哥哥去巴黎会不会拍一个新手势舞哈🥵",
-        translatedText: "오빠 파리 가면 새로운 손동작 춤 하나 찍어주지 않을까 하하🥵",
+        originalText: "哥哥去巴黎会不会拍一个新手势舞哈[加油]",
+        translatedText: "오빠 파리 가서 새로운 챌린지 하나 찍어주면 안대?ㅎ[加油]",
         images: [],
         actorReply: {
-          originalText: "我尽力而为🥹",
-          translatedText: "최선을 다해볼게🥹"
+          originalText: "我尽力而为[无聊]",
+          translatedText: "최선을 다해볼게[无聊]"
         }
       },
       {
         id: 26060122,
         username: "妹有心柿",
         originalText: "哥哥 在等三分钟",
-        translatedText: "오빠 3분 기다리는 중",
+        translatedText: "오빠 3분만 기다려",
         images: [],
         actorReply: {
           originalText: "什么三分钟",
-          translatedText: "무슨 3분?"
+          translatedText: "왜 3분이야?"
         }
       },
       {
         id: 26060123,
         username: "JuscHe",
-        originalText: "这里有你的胶片！@周翊然Tz",
-        translatedText: "여기 네 필름 사진 있어!@周翊然Tz",
+        originalText: "这里有你的胶片！",
+        translatedText: "여기 너 찍힌 필름 사진 있어!",
         images: ["images/2606-film-photo.jpg"],
         actorReply: {
           originalText: "洗出来看看呢",
-          translatedText: "인화해서 보여줘"
+          translatedText: "인화해서 보여주라"
         }
       },
       {
         id: 26060124,
         username: "拾月格格屋",
-        originalText: "老公这次vlog录个1小时的时长就行，别太辛苦了😢",
-        translatedText: "자기야 이번 브이로그는 1시간 정도 분량이면 돼, 너무 무리하지 마😢",
-        note: "익연이 답글은 2초 음성 메시지 + 텍스트",
+        originalText: "老公这次vlog录个1小时的时长就行，别太辛苦了[并不简单]",
+        translatedText: "자기야 이번 브이로그는 1시간 정도 분량이면 돼, 너무 무리하지 마[并不简单]",
+        note: "원문 음성 메세지",
         images: [],
         actorReply: {
           originalText: "好的好的，我尽力而为",
-          translatedText: "알겠어 알겠어, 최선을 다해볼게 (음성 2초)"
+          translatedText: "알겠어, 최선을 다해볼게"
         }
       },
       {
@@ -286,14 +286,14 @@ const posts_202606 = [
         images: ["images/2606-cat-2.jpg"],
         actorReply: {
           originalText: "一个月",
-          translatedText: "한 달"
+          translatedText: "1개월"
         }
       },
       {
         id: 26060126,
         username: "周翊然Tz",
-        originalText: "我走了 各位 准备赶飞机了 大家说我都努力而为🙋✈️✈️",
-        translatedText: "나 갈게 얘들아, 비행기 타러 가야 해서, 다들 말한 대로 최선을 다해볼게🙋✈️✈️",
+        originalText: "我走了 各位[航天员] 准备赶飞机了 大家说我都努力而为[航天员][航天员][航天员]",
+        translatedText: "갈게 얘들아[航天员] 비행기 타러 가야 해 너네한테 말한 대로 최선을 다해볼게[航天员][航天员][航天员] ",
         images: [],
         actorReply: null
       }
