27e7041764f23033da2081fcd7cb1d65f9016882
zyr001122 <vazseyo@gmail.com>
2026-08-05 20:42:19 +0900

    Add files via upload

--- data-schedule.js
+++ data-schedule.js
@@ -3,7 +3,7 @@
 // time은 없으면 빈 문자열로 두면 됨 (하루 종일 일정 등)
 const scheduleEvents = [
   { date: '2026-08-04', time: '', title: '비즈니스 촬영', category: 'shoot' },
-  { date: '2026-08-08', time: '', title: '브랜드 라이브방송', category: 'live' },
+  { date: '2026-08-08', time: '21:00', title: '브랜드 라이브방송 - APM Monaco <a href="https://pages-fast.m.taobao.com/wow/z/app/mtb/tblive-preview-h5/home?tabChannel=PREVIEW&fromLiveRoom=1&h5LiveId=1604753178024966&suid=7203B628-980D-4272-8EE9-AD75A23ADFC5&x-ssr=true&sp_tk=aDFiVmd5VXVqbjM%3D&isNeedHome=0&_afc_link=1&bxsign=scdaoiRmLx2eFd2hsHGuAuCWk-MEhfYEUEB3PS8u3cFJlQPI1cS5uz3jC5DWUP8VpRPwpG2QV0jEjHIgU-j1zVnZ06TqIUAzEq5TI4ARAAv2-9fpQGjClapGrsqE4iE-BCPTGQZtBrN54mcSqDZ9LsuKA&un=2d338b0a5cb31364a11ecd8b6bf592ec&disableNav=YES&id=1604753178024966&shareurl=true&share_crt_v=1&ut_sk=1.ZpY/TedjlxADACs/mi0Upkwe_21380790_1785916872369.Copy.zhibochuchuang&afcflow=unkown&cpp=1&h5AnchorId=RAzN8214YLDBs464Tm3t1t1cQHPTUMZaae1DLNH8A5huqeuLiXSyfHKFmmUzVSwvsfcMzJ3pN73imiso&pha_manifest=default&needLogin=false&surge_ssr=true&spm=a2141.live_waitou&async=false&un_site=0&sourceType=other&disableProgress=true&livesource=anchor_pc_share&short_name=h.8TZ5ZcX&utparamcnt=%7B%22_afc_link%22%3A%221%22%7D&tk=h1bVgyUujn3&app=apple_web_kit" target="_blank" rel="noopener" style="color:inherit;text-decoration:underline;">↗</a>', category: 'live' },
   { date: '2026-08-09', time: '', title: '웨이보 기우기 📍광저우', category: 'event' },
   { date: '2026-08-11', time: '', title: '예능 촬영 (1일차)', category: 'shoot' },
   { date: '2026-08-12', time: '', title: '예능 촬영 (2일차)', category: 'shoot' },
