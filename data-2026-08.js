b9f9b0b4acedbb80ba216cc4b52991a88c1218d3
zyr001122 <vazseyo@gmail.com>
2026-08-23 00:50:00 +0900

    Update data-2026-08.js

--- data-2026-08.js
+++ data-2026-08.js
@@ -24,7 +24,7 @@ const posts_202608 = [
         images: ["images/2608-drawing-1.jpg"],
         actorReply: {
           originalText: "我觉得你无敌了",
-          translatedText: "너한테 명함도 못 내밀겠다"
+          translatedText: "너한텐 명함도 못 내밀겠다"
         }
       },
       {
@@ -40,6 +40,17 @@ const posts_202608 = [
       },
       {
         id: 26080104,
+        username: "养了翊只卷毛狗",
+        originalText: "笑死我了哈哈哈哈哈要上火星了吗龙少～",
+        translatedText: "진짜 개웃겨 ㅋㅋㅋㅋㅋ 화성 가는 거야～",
+        images: [],
+        actorReply: {
+          originalText: "🙂",
+          translatedText: "🙂"
+        }
+      },
+      {
+        id: 26080105,
         username: "叛逆烤红薯-",
         originalText: "哥哥你看法拉利骑鬼火带茅台上街买奶茶了",
         translatedText: "오빠 이거 봐 페라리가 오토바이에 마오타이 태우고 밀크티 사러 나왔엉",
@@ -50,7 +61,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080105,
+        id: 26080106,
         username: "Baek_一顆濃糖",
         originalText: "请你看",
         translatedText: "이거 봐줘",
@@ -61,7 +72,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080106,
+        id: 26080107,
         username: "normi噜",
         originalText: "然然我今天过安检 上飞机 差点没有上去 安检一直在响 可是我身上没有金属制品 后来我才知道 原来是我铁了心的爱你！！！",
         translatedText: "익연아 나 오늘 비행기 타려고 보안검색대 지나가다 못 탈 뻔했어 검색대가 계속 삐삐 울리는데 몸에 금속이 하나도 없었거든? 나중에 알았는데 내가 철석같이 널 사랑해서 그랬던 거 였어!!!",
@@ -72,7 +83,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080107,
+        id: 26080108,
         username: "你要开心ooo",
         originalText: "敢不敢挑战这个🫠",
         translatedText: "이거 도전해 볼 자신 있어?",
@@ -83,7 +94,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080108,
+        id: 26080109,
         username: "R翊碗甜粥",
         originalText: "烛光晚餐",
         translatedText: "촛불 만찬",
@@ -94,7 +105,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080109,
+        id: 26080110,
         username: "XJJJXI",
         originalText: "hello帅哥",
         translatedText: "hello 잘생긴 오빠",
@@ -105,7 +116,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080110,
+        id: 26080111,
         username: "Yuuum7-",
         originalText: "画画进步了",
         translatedText: "그림 실력 늘었는데",
@@ -116,7 +127,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080111,
+        id: 26080112,
         username: "叫我曲露",
         originalText: "怎么说@微博智搜",
         translatedText: "어때?",
@@ -127,7 +138,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080112,
+        id: 26080113,
         username: "然然的大梦想家",
         originalText: "还记得吗",
         translatedText: "기억나?",
@@ -138,7 +149,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080113,
+        id: 26080114,
         username: "猫咪唱片__",
         originalText: "我第一次用相机你快夸夸我",
         translatedText: "나 카메라 처음 써보는데 빨리 칭찬해줘",
@@ -149,7 +160,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080114,
+        id: 26080115,
         username: "翊起追光到然处",
         originalText: "哥哥谁教你这么卖萌的",
         translatedText: "오빠 누가 이렇게 애교 부리는 거 가르쳐줬어",
@@ -160,18 +171,18 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080115,
+        id: 26080116,
         username: "青提奶芙卷Tz",
         originalText: "我有预感我今天会被翻",
         translatedText: "오늘 내 댓글에 답장 해줄 것 같은 예감이 들어",
         images: [],
         actorReply: {
           originalText: "那你预感很准啊",
-          translatedText: "그럼 너 예감이 되게 정확한 거네"
+          translatedText: "이러면 너 예감 되게 정확한 게 되는 거네"
         }
       },
       {
-        id: 26080116,
+        id: 26080117,
         username: "喵了个汪Tz",
         originalText: "猜猜什么材料做的",
         translatedText: "무슨 재료로 만든 건지 맞혀봐",
@@ -182,7 +193,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080117,
+        id: 26080118,
         username: "柚子苏打i",
         originalText: "请你看我洗出来的胶片",
         translatedText: "내가 인화한 필름 사진 봐봐",
@@ -193,18 +204,18 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080118,
+        id: 26080119,
         username: "只吃两碗饭-",
         originalText: "今天当寿星 把我的生日愿望分你一个 许个愿吧！",
         translatedText: "오늘 내가 생일 주인공이니까 내 생일 소원 하나 나눠줄게 소원 빌어!",
         images: ["images/2608-birthday-wish.jpg"],
         actorReply: {
           originalText: "那希望你的愿望都能实现",
-          translatedText: "그럼 네 소원들이 다 이뤄지길 바랄게"
+          translatedText: "그럼 너가 빈 소원들이 다 이뤄지길 바랄게"
         }
       },
       {
-        id: 26080119,
+        id: 26080120,
         username: "1amThur_",
         originalText: "三年从未被翻过💔",
         translatedText: "3년째 한 번도 답장 못 받아봄💔",
@@ -215,7 +226,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080120,
+        id: 26080121,
         username: "骤然然ran_Tz",
         originalText: "哥哥你看这俩在锤牛肉给你吃上班辛苦了",
         translatedText: "오빠 이거 봐 얘네 둘이 소고기 다지고 있어 오빠 먹으라고 출근하느라 고생했어",
@@ -226,7 +237,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080121,
+        id: 26080122,
         username: "桃气翊冉Tz",
         originalText: "然然我刚才在现场！刚离开你不到半小时就很想念，你今天又淋雨了注意身体好好补充营养素，不要又感冒加重了呀🥲",
         translatedText: "익연아 나 방금 현장에 있었어! 너랑 헤어진 지 30분도 안 됐는데 벌써 보고 싶어, 오늘 또 비 맞았던데 몸 잘 챙기고 영양제 잘 챙겨 먹어. 감기 또 심해지면 안 돼🥲",
@@ -237,18 +248,18 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080122,
+        id: 26080123,
         username: "羊啵纹文",
         originalText: "龙少说句泰语呗",
         translatedText: "태국어 한마디만 해줘",
         images: [],
         actorReply: {
-          originalText: "[微笑]",
-          translatedText: "[微笑]"
+          originalText: "🙂",
+          translatedText: "🙂"
         }
       },
       {
-        id: 26080123,
+        id: 26080124,
         username: "养只小狗叫拉瑞",
         originalText: "这个彩虹标准吗 哥哥",
         translatedText: "이 무지개 제대로 된 무지개 맞아? 오빠",
@@ -259,7 +270,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080124,
+        id: 26080125,
         username: "餐盒午",
         originalText: "然然能不能推荐一下自己最喜欢的耳钉 最近准备打耳洞 没看到好看的耳钉（os：什么时候复刻湿发造型哈哈哈）[航天员] [航天员] ",
         translatedText: "익연 너가 제일 좋아하는 귀걸이 추천해줄 수 있어? 요즘 귀 뚫고 싶은데 마음에 드는 귀걸이가 없어서 (os: 젖은 머리 스타일은 언제 또 해줄 거야 ㅋㅋㅋ)[航天员] [航天员] ",
@@ -270,7 +281,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080125,
+        id: 26080126,
         username: "甜粥章渝烧",
         originalText: "然然，泰国有没有吃到好吃的菜推荐[打call][打call]",
         translatedText: "익연아, 태국에서 먹은 것 중에 맛있었던 거 추천해줄 만한 거 있어?[打call][打call]",
@@ -281,7 +292,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080126,
+        id: 26080127,
         username: "纸飞飞er_",
         originalText: "我画的",
         translatedText: "내가 그린 거야",
@@ -292,7 +303,7 @@ const posts_202608 = [
         }
       },
       {
-        id: 26080127,
+        id: 26080128,
         username: "周翊然Tz",
         originalText: "我先走了 我要去看看 这段时间照片有没有攒够 拜拜各位",
         translatedText: "먼저 갈게 사진 좀 모였나 확인 해봐야겠다 갈게 빠빠이",
