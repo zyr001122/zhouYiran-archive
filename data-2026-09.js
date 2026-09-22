df3dac3f719d8ec3b727f382106d84abd902c136
zyr001122 <vazseyo@gmail.com>
2026-09-22 23:04:40 +0900

    Update data-2026-09.js

--- data-2026-09.js
+++ data-2026-09.js
@@ -33,7 +33,7 @@ const posts_202609 = [
         id: 26090103,
         username: "扁桃体发炎不发言-",
         originalText: "哥哥 透露一下专辑进度多少了🫢 有老余？",
-        translatedText: "오빠, 앨범이 얼마나 진행됐는지 살짝만 알려줘🫢 老余(라오위)도 같이 해?",
+        translatedText: "오빠, 앨범이 얼마나 진행됐는지 살짝만 스포해줘🫢 여가운님도 같이 작업해?",
         images: ["images/2609-album-laoyu-1.jpg"],
         actorReply: {
           originalText: "我们经常在一起创作[doge]",
@@ -44,7 +44,7 @@ const posts_202609 = [
         id: 26090104,
         username: "点点TZL",
         originalText: "听说开车哥要去比赛了 预祝哥哥顺利完赛 稳定发挥 同时也提前祝哥哥中秋快乐 一起吃月饼吧",
-        translatedText: "운전 오빠가 레이스 나간다며? 무사히 완주하고 실력 그대로 보여주길 미리 응원할게. 그리고 추석도 미리 축하해! 같이 월병 먹자",
+        translatedText: "레이서 오빠 레이스 나간다던데 무사 완주하고 실력 그대로 발휘하고 와 미리 응원할게. 그리고 추석도 미리 축하해! 같이 월병 먹장",
         images: ["images/2609-mooncake-1.jpg"],
         actorReply: {
           originalText: "？？？有点厉害",
@@ -55,7 +55,7 @@ const posts_202609 = [
         id: 26090105,
         username: "是我的小土豆-Tz",
         originalText: "然然 可以语音唱一下九月底不 超级想听！👂",
-        translatedText: "익연아, 〈九月底〉 음성으로 한 번 불러줄 수 있어? 완전 듣고 싶어!👂",
+        translatedText: "익연아, 〈九月底〉 음성으로 한 구절 불러줄 수 있어? 너무 듣고 싶어!👂",
         images: ["images/2609-jiuyuedi-1.jpg"],
         actorReply: {
           originalText: "还没到时间[doge]",
@@ -66,7 +66,7 @@ const posts_202609 = [
         id: 26090106,
         username: "悲伤甜甜圈圈",
         originalText: "老公中秋节是我的生日 如果得到你提前的祝福我会很高兴的🥺🥺[给你小心心][给你小心心]",
-        translatedText: "여보, 추석이 내 생일이야. 미리 축하해 주면 진짜 기쁠 것 같아🥺🥺 [给你小心心][给你小心心]",
+        translatedText: "여보, 추석이 내 생일이야 너가 미리 축하해 주면 진짜 기쁠 것 같아🥺🥺 [给你小心心][给你小心心]",
         images: [],
         actorReply: {
           originalText: "提前祝你生日快乐！",
@@ -99,7 +99,7 @@ const posts_202609 = [
         id: 26090109,
         username: "椰椰糕冷",
         originalText: "哥哥评价一下小法和茅台的开赛车🏎️技术！你就说能不能去参加比赛吧！[心]",
-        translatedText: "오빠, 샤오파랑 마오타이의 레이싱🏎️ 실력 좀 평가해 줘! 대회 나갈 수 있는지 없는지만 말해 봐! [心]",
+        translatedText: "오빠, 페라리랑 마오타이 레이싱🏎️ 실력 좀 평가해 줘! 대회 나갈 수 있는지 없는지만 말해 줘! [心]",
         images: ["images/2609-dogs-racing-1.jpg"],
         actorReply: {
           originalText: "没系安全带啊 ？？",
@@ -110,7 +110,7 @@ const posts_202609 = [
         id: 26090110,
         username: "JuscHe",
         originalText: "给你拍的胶片@周翊然Tz",
-        translatedText: "너 찍은 필름 사진이야 @周翊然Tz",
+        translatedText: "널 찍은 필름 사진이야 @周翊然Tz",
         images: ["images/2609-film-1.jpg", "images/2609-film-2.jpg"],
         actorReply: {
           originalText: "好看！近点就更好了",
@@ -121,7 +121,7 @@ const posts_202609 = [
         id: 26090111,
         username: "Yoomiu_曲奇冬饼",
         originalText: "又要回归赛车手身份了吧[干饭人]",
-        translatedText: "또 레이서로 복귀하는 거지? [干饭人]",
+        translatedText: "다시 레이서로 복귀하는 거지? [干饭人]",
         images: ["images/2609-racer-1.jpg"],
         actorReply: {
           originalText: "[收到]",
@@ -165,7 +165,7 @@ const posts_202609 = [
         id: 26090115,
         username: "香菜不断加加加",
         originalText: "携小法茅台预祝你赛车顺利呀[航天员]",
-        translatedText: "샤오파랑 마오타이랑 같이, 레이스 잘 치르길 미리 응원할게 [航天员]",
+        translatedText: "페라리랑 마오타이랑 같이 레이스 무사히 마치길 미리 응원할게 [航天员]",
         images: ["images/2609-dogs-cheer-1.jpg"],
         actorReply: {
           originalText: "谢谢你们啊[允悲]",
@@ -176,11 +176,11 @@ const posts_202609 = [
         id: 26090116,
         username: "脆皮灿包",
         originalText: "邀请你跟我一起看彩虹@周翊然Tz",
-        translatedText: "나랑 같이 무지개 보자 @周翊然Tz",
+        translatedText: "나랑 같이 무지개 보러가자 @周翊然Tz",
         images: ["images/2609-rainbow-1.jpg"],
         actorReply: {
           originalText: "藏在云里的彩虹 有点意思",
-          translatedText: "구름 속에 숨은 무지개라니, 좀 재밌네"
+          translatedText: "구름 속에 숨은 무지개라니, 느낌있네"
         }
       },
       {
@@ -198,7 +198,7 @@ const posts_202609 = [
         id: 26090118,
         username: "周翊然Tz",
         originalText: "最近的生活  每天早起 歌单有了很多新的歌 和几个朋友坐着一起爽吃 一起聊天  驾驶了新的赛车 研究新的驾驶方式 专辑ing 认真看剧本ing",
-        translatedText: "요즘 내 일상은 이래. 매일 일찍 일어나고, 플레이리스트에 새 노래도 많이 생겼어. 친구 몇 명이랑 둘러앉아서 실컷 먹고 수다도 떨었고, 새 레이싱카도 몰아 보면서 새로운 주행 방식을 연구하고 있어. 앨범 작업 중이고, 대본도 열심히 보는 중이야",
+        translatedText: "요즘 내 일상은 매일 일찍 일어나고 플레이리스트에 새 노래도 많이 생겼어 친구 몇 명이랑 둘러앉아서 실컷 먹고 수다도 떨고 새 레이싱카도 몰아 보면서 새로운 주행 방식을 연구하고 있어 앨범 작업ing 대본도 열심히ing 중이야",
         images: [],
         actorReply: null
       },
@@ -206,11 +206,11 @@ const posts_202609 = [
         id: 26090119,
         username: "小狗恋冬日",
         originalText: "哥哥你看像不像",
-        translatedText: "오빠 봐 봐, 닮았지?",
+        translatedText: "오빠 봐 봐 닮았지?",
         images: ["images/2609-lookalike-1.jpg"],
         actorReply: {
           originalText: "哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈",
-          translatedText: "하하하하하하하하하하하하하하하하하하"
+          translatedText: "ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ"
         }
       },
       {
@@ -221,14 +221,14 @@ const posts_202609 = [
         images: ["images/2609-rival-1.jpg"],
         actorReply: {
           originalText: "瑟瑟发抖了",
-          translatedText: "벌벌 떨고 있어"
+          translatedText: "벌벌 떨고 있다"
         }
       },
       {
         id: 26090121,
         username: "七十七小狗",
         originalText: "小女子携家夫@周翊然Tz 与爱犬在这里提前祝大家中秋快乐[给你小心心]",
-        translatedText: "소녀가 서방님 @周翊然Tz, 그리고 우리 강아지와 함께 여러분께 미리 추석 인사 올립니다 [给你小心心]",
+        translatedText: "소녀 서방님과 그리고 우리 강아지와 함께 여러분께 미리 추석 인사 올립니다 [给你小心心]",
         images: ["images/2609-mid-autumn-dog-1.jpg"],
         actorReply: {
           originalText: "好清秀的两只狗",
@@ -243,14 +243,14 @@ const posts_202609 = [
         images: ["images/2609-idea-1.jpg"],
         actorReply: {
           originalText: "有点超前了…..",
-          translatedText: "좀 시대를 앞서갔는데….."
+          translatedText: "시대를 너무 앞서갔는데….."
         }
       },
       {
         id: 26090123,
         username: "Xiaoenyr",
         originalText: "张嘴 喂你嘴里",
-        translatedText: "입 벌려, 먹여 줄게",
+        translatedText: "아 해봐 먹여 줄게",
         images: ["images/2609-feed-1.jpg"],
         actorReply: {
           originalText: "闻到味儿了",
@@ -261,18 +261,18 @@ const posts_202609 = [
         id: 26090124,
         username: "翊个海豹龙",
         originalText: "然然如果你回复我的话，我就给你吃，我今天刚做的月饼，是不是非常可爱？",
-        translatedText: "익연아, 답장해 주면 내가 오늘 만든 월병 너 줄게. 진짜 귀엽지?",
+        translatedText: "익연아, 답장해 주면 내가 오늘 만든 월병 너 줄게, 진짜 귀엽지?",
         images: ["images/2609-mooncake-2.jpg"],
         actorReply: {
           originalText: "哈哈哈哈哈 很可爱 而且你好厉害",
-          translatedText: "하하하하하 진짜 귀엽다. 그리고 너 진짜 대단하다"
+          translatedText: "ㅋㅋㅋㅋㅋ 진짜 귀엽다 근데 너 진짜 손재주 좋다"
         }
       },
       {
         id: 26090125,
         username: "周翊然Tz",
         originalText: "先走啦 大家早点休息 晚安[收到]",
-        translatedText: "나 먼저 갈게~ 다들 일찍 쉬어. 잘 자 [收到]",
+        translatedText: "먼저 갈게 다들 일찍 쉬고 잘 자 [收到]",
         images: [],
         actorReply: null
       }
